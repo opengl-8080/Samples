63869f774ae9f10c515c44563ff92b2bad999fbd
opengl8080 <tomcat.port.8080+github@gmail.com>
2016-01-02 11:29:29 +0900

    select の分割実行（複合キー）

--- java/mybatis/src/main/java/sample/mybatis/Bar.java
+++ java/mybatis/src/main/java/sample/mybatis/Bar.java
@@ -1,10 +1,11 @@
 package sample.mybatis;
 
 public class Bar {
-    private int id;
+    private int key1;
+    private String key2;
 
     @Override
     public String toString() {
-        return "Bar [id=" + id + "]";
+        return "Bar [key1=" + key1 + ", key2=" + key2 + "]";
     }
 }
