f24453e9691baa9d300c9d74164a6a122a517c9f
opengl8080 <tomcat.port.8080+github@gmail.com>
2016-01-03 12:44:15 +0900

    バッチ更新

--- java/mybatis/src/main/java/sample/mybatis/Main.java
+++ java/mybatis/src/main/java/sample/mybatis/Main.java
@@ -2,6 +2,7 @@ package sample.mybatis;
 
 import java.io.InputStream;
 
+import org.apache.ibatis.session.ExecutorType;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
@@ -12,11 +13,14 @@ public class Main {
         try (InputStream in = Main.class.getResourceAsStream("/mybatis-config.xml")) {
             SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(in);
             
-            try (SqlSession session = factory.openSession()) {
-                TestTableMapper mapper = session.getMapper(TestTableMapper.class);
-                mapper.selectTest();
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
+            try (SqlSession session = factory.openSession(ExecutorType.BATCH)) {
+                for (int i=0; i<3; i++) {
+                    TestTable t = new TestTable("value-" + i);
+                    int updateCount = session.insert("sample.mybatis.insertTest", t);
+                    System.out.println("updateCount = " + updateCount);
+                }
+                
+                session.commit();
             }
         }
     }
--- java/mybatis/src/main/java/sample/mybatis/TestTable.java
+++ java/mybatis/src/main/java/sample/mybatis/TestTable.java
@@ -2,9 +2,14 @@ package sample.mybatis;
 
 public class TestTable {
     private int id;
+    private String value;
+    
+    public TestTable(String value) {
+        this.value = value;
+    }
 
     @Override
     public String toString() {
-        return "TestTable [id=" + id + "]";
+        return "TestTable [id=" + id + ", value=" + value + "]";
     }
 }
