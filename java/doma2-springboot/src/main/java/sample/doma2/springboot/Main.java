c08b82a5d04a2b7f14c6873905447711532136c0
opengl8080 <tomcat.port.8080+github@gmail.com>
2016-01-16 00:25:47 +0900

    @EnableTransactionManagement がなくてもいいっぽい？

--- java/doma2-springboot/src/main/java/sample/doma2/springboot/Main.java
+++ java/doma2-springboot/src/main/java/sample/doma2/springboot/Main.java
@@ -4,10 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
-@EnableTransactionManagement
 public class Main {
     
     @Autowired
