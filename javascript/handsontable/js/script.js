9c779c3387ce5045f09f5730b5cef935488198bf
opengl8080 <tomcat.port.8080+github@gmail.com>
2016-01-16 21:04:42 +0900

    ネストされたオブジェクトのマッピング

--- javascript/handsontable/js/script.js
+++ javascript/handsontable/js/script.js
@@ -1,9 +1,16 @@
 var data = [
-  {name: '佐藤', age: 25, a: 'A'},
-  {name: '鈴木', age: 11, b: 'B'},
-  {name: '田中', age: 21}
+  {name: {first: '佐藤', last: '一郎'}, age: 25},
+  {name: {first: '鈴木', last: '二郎'}, age: 11},
+  {name: {first: '田中', last: '三郎'}, age: 21}
 ];
 
 var grid = document.getElementById('grid');
 
-new Handsontable(grid, {data: data});
+new Handsontable(grid, {
+    data: data,
+    column: [
+        {data: 'age'},
+        {data: 'name.first'},
+        {data: 'name.last'}
+    ]
+});
