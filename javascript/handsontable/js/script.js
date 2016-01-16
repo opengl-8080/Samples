e9b50c566de4c0a237f5a0cf0682eb2f1a4d42b6
opengl8080 <tomcat.port.8080+github@gmail.com>
2016-01-16 21:09:29 +0900

    配列を使用した最も基本的なデータソース

--- javascript/handsontable/js/script.js
+++ javascript/handsontable/js/script.js
@@ -1,16 +1,9 @@
 var data = [
-  {name: {first: '佐藤', last: '一郎'}, age: 25},
-  {name: {first: '鈴木', last: '二郎'}, age: 11},
-  {name: {first: '田中', last: '三郎'}, age: 21}
+  ['佐藤', 28],
+  ['鈴木', 19],
+  ['田中', 25]
 ];
 
 var grid = document.getElementById('grid');
 
-new Handsontable(grid, {
-    data: data,
-    column: [
-        {data: 'age'},
-        {data: 'name.first'},
-        {data: 'name.last'}
-    ]
-});
+new Handsontable(grid, {data: data});
