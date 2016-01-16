c80a9ac59c104f06e923b2c7ee0fbbd1dba0c4ab
opengl8080 <tomcat.port.8080+github@gmail.com>
2016-01-16 23:40:05 +0900

    任意のオブジェクトをデータオースにする

--- javascript/handsontable/js/script.js
+++ javascript/handsontable/js/script.js
@@ -1,9 +1,68 @@
 var data = [
-  ['佐藤', 28],
-  ['鈴木', 19],
-  ['田中', 25]
+    new MyModel('佐藤', 22),
+    new MyModel('鈴木', 31),
+    new MyModel('田中', 19)
 ];
 
 var grid = document.getElementById('grid');
 
-new Handsontable(grid, {data: data});
+new Handsontable(grid, {
+    data: data,
+    dataSchema: new MyModel(), // ★空のインスタンスを渡す
+    columns: [
+        {
+            // ★セルの値が参照・変更されたときにコールバックされるので、
+            //   第二引数に値が渡されたかどうかでモデルから値を取得するか、
+            //   値を設定するかを決める。
+            data: function(myModel, name) {
+                if (name) {
+                    myModel.setName(name);
+                } else {
+                    return myModel.getName();
+                }
+            }
+        },
+        {
+            data: function(myModel, age) {
+                if (age) {
+                    myModel.setAge(age);
+                } else {
+                    return myModel.getAge();
+                }
+            }
+        }
+    ]
+});
+
+// ボタンをクリックしたら、現在の data の内容を表示する
+document.getElementById('button').addEventListener('click', function() {
+    data.forEach(function(myModel) {
+        console.log(myModel.toString());
+    });
+});
+
+// 独自クラス
+function MyModel(name, age) {
+    var _name = name,
+        _age = age;
+
+    this.getName = function() {
+        return _name;
+    };
+
+    this.setName = function(name) {
+        _name = name;
+    };
+
+    this.getAge = function() {
+        return _age;
+    };
+
+    this.setAge = function(age) {
+        _age = age;
+    };
+
+    this.toString = function() {
+        return 'MyModel{name=' + _name + ', age=' + _age + '}';
+    };
+}
