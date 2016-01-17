1137f7565df00061cc618c910c18778a78759452
opengl8080 <tomcat.port.8080+github@gmail.com>
2016-01-17 10:10:19 +0900

    データを読み込む

--- javascript/handsontable/js/script.js
+++ javascript/handsontable/js/script.js
@@ -1,68 +1,14 @@
 var data = [
-    new MyModel('佐藤', 22),
-    new MyModel('鈴木', 31),
-    new MyModel('田中', 19)
+  {name: '佐藤', age: 25},
+  {name: '鈴木', age: 11},
+  {name: '田中', age: 21}
 ];
 
 var grid = document.getElementById('grid');
 
-new Handsontable(grid, {
-    data: data,
-    dataSchema: new MyModel(), // ★空のインスタンスを渡す
-    columns: [
-        {
-            // ★セルの値が参照・変更されたときにコールバックされるので、
-            //   第二引数に値が渡されたかどうかでモデルから値を取得するか、
-            //   値を設定するかを決める。
-            data: function(myModel, name) {
-                if (name) {
-                    myModel.setName(name);
-                } else {
-                    return myModel.getName();
-                }
-            }
-        },
-        {
-            data: function(myModel, age) {
-                if (age) {
-                    myModel.setAge(age);
-                } else {
-                    return myModel.getAge();
-                }
-            }
-        }
-    ]
-});
+var table = new Handsontable(grid, {data: data});
 
-// ボタンをクリックしたら、現在の data の内容を表示する
-document.getElementById('button').addEventListener('click', function() {
-    data.forEach(function(myModel) {
-        console.log(myModel.toString());
-    });
-});
-
-// 独自クラス
-function MyModel(name, age) {
-    var _name = name,
-        _age = age;
-
-    this.getName = function() {
-        return _name;
-    };
-
-    this.setName = function(name) {
-        _name = name;
-    };
-
-    this.getAge = function() {
-        return _age;
-    };
-
-    this.setAge = function(age) {
-        _age = age;
-    };
-
-    this.toString = function() {
-        return 'MyModel{name=' + _name + ', age=' + _age + '}';
-    };
-}
+table.loadData([
+  {name: '御手洗', age: 31},
+  {name: '東海林', age: 22}
+]);
