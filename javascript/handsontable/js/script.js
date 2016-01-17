d2cb2cbced6f105e0bfa867c5506027dff740668
opengl8080 <tomcat.port.8080+github@gmail.com>
2016-01-17 11:58:00 +0900

    autoColumnSize

--- javascript/handsontable/js/script.js
+++ javascript/handsontable/js/script.js
@@ -1,11 +1,9 @@
 var grid = document.getElementById('grid');
 
-var table = new Handsontable(grid, {data: [
-  {name: '佐藤', age: 25},
-  {name: '鈴木', age: 11},
-  {name: '田中', age: 21}
-]});
-
-table.getData().forEach(function(row) {
-    console.log(row);
+new Handsontable(grid, {
+    data: [
+        ['あいうえお', 'かきくけこさしすせそ'],
+        ['やゆよ', 'たちつてと']
+    ],
+    autoColumnSize: true
 });
