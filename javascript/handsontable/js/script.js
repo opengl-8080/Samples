fe9faf88ad9adbfccd5f25cbc3fec7047cffd289
opengl8080 <tomcat.port.8080+github@gmail.com>
2016-01-17 10:13:18 +0900

    データを取得する

--- javascript/handsontable/js/script.js
+++ javascript/handsontable/js/script.js
@@ -1,14 +1,11 @@
-var data = [
+var grid = document.getElementById('grid');
+
+var table = new Handsontable(grid, {data: [
   {name: '佐藤', age: 25},
   {name: '鈴木', age: 11},
   {name: '田中', age: 21}
-];
+]});
 
-var grid = document.getElementById('grid');
-
-var table = new Handsontable(grid, {data: data});
-
-table.loadData([
-  {name: '御手洗', age: 31},
-  {name: '東海林', age: 22}
-]);
+table.getData().forEach(function(row) {
+    console.log(row);
+});
