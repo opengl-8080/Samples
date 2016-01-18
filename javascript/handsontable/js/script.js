7acf9caad813685361dcf742409fe8acc7454739
opengl8080 <tomcat.port.8080+github@gmail.com>
2016-01-19 00:05:20 +0900

    メニューのカスタマイズ

--- javascript/handsontable/js/script.js
+++ javascript/handsontable/js/script.js
@@ -5,5 +5,7 @@ new Handsontable(grid, {
         ['a', 'd'],
         ['b', 'c']
     ],
-    contextMenu: true
+    comments: true,
+    customBorders: true,
+    contextMenu: ['row_below', '---------', 'col_left', 'col_right', 'remove_row', 'remove_col', 'undo', 'redo', 'make_read_only', 'alignment', 'borders', 'commentsAddEdit', 'commentsRemove']
 });
