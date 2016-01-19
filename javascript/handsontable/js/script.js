5abd1150417eca0b158552912ac43e110e824bc8
opengl8080 <tomcat.port.8080+github@gmail.com>
2016-01-19 23:38:29 +0900

    メニューのテキストをカスタマイズ

--- javascript/handsontable/js/script.js
+++ javascript/handsontable/js/script.js
@@ -1,11 +1,18 @@
 var grid = document.getElementById('grid');
 
-new Handsontable(grid, {
+var table = new Handsontable(grid, {
     data: [
         ['a', 'd'],
         ['b', 'c']
-    ],
-    comments: true,
-    customBorders: true,
-    contextMenu: ['row_below', '---------', 'col_left', 'col_right', 'remove_row', 'remove_col', 'undo', 'redo', 'make_read_only', 'alignment', 'borders', 'commentsAddEdit', 'commentsRemove']
+    ]
+});
+
+table.updateSettings({
+    contextMenu: {
+        items: {
+            row_above: {
+                name: '行を挿入'
+            }
+        }
+    }
 });
