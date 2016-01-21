3992cb3b932faee880046a71151d9d46b47a287a
opengl8080 <tomcat.port.8080+github@gmail.com>
2016-01-21 23:34:41 +0900

    コールバック・items を配列に修正

--- javascript/handsontable/js/script.js
+++ javascript/handsontable/js/script.js
@@ -7,27 +7,14 @@ var table = new Handsontable(grid, {
     ]
 });
 
-var selected = 1;
-
 table.updateSettings({
     contextMenu: {
         items: [
             {
-                key: 'item1',
-                name: function() {
-                    return selected === 1 ? '●item1' : 'item1';
-                },
+                key: 'myItem',
+                name: 'メニュ～',
                 callback: function() {
-                    selected *= -1;
-                }
-            },
-            {
-                key: 'item2',
-                name: function() {
-                    return selected === -1 ? '●item2' : 'item2';
-                },
-                callback: function() {
-                    selected *= -1;
+                    console.log(arguments);
                 }
             }
         ]
