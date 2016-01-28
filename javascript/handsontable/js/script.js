a10c66bb179a89f2e764270779ecb937d573b4c6
opengl8080 <tomcat.port.8080+github@gmail.com>
2016-01-28 21:04:35 +0900

    コピペ無効

--- javascript/handsontable/js/script.js
+++ javascript/handsontable/js/script.js
@@ -1,28 +1,9 @@
 var grid = document.getElementById('grid');
 
-var table = new Handsontable(grid, {
+new Handsontable(grid, {
     data: [
         ['a', 'd'],
         ['b', 'c']
-    ]
-});
-
-var flag = true;
-
-table.updateSettings({
-    contextMenu: {
-        items: [
-            {
-                name: 'item1',
-                disabled: true
-            },
-            {
-                name: 'item2',
-                disabled: function() {
-                    flag = !flag;
-                    return flag;
-                }
-            }
-        ]
-    }
+    ],
+    copyPaste: false
 });
