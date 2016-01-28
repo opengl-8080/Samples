bc495bb07167a1da042169248d903bc182642e49
opengl8080 <tomcat.port.8080+github@gmail.com>
2016-01-28 21:20:24 +0900

    任意の場所に罫線を引く

--- javascript/handsontable/js/script.js
+++ javascript/handsontable/js/script.js
@@ -6,6 +6,18 @@ new Handsontable(grid, {
         [1, 2, 3],
         [1, 2, 3]
     ],
-    customBorders: true,
-    contextMenu: ['borders']
+    customBorders: [
+        {
+            row: 1,
+            col: 1,
+            top: {
+                width: 3,
+                color: 'red'
+            },
+            right: {
+                width: 2,
+                color: '#1199ff'
+            }
+        }
+    ]
 });
