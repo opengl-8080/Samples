4f5ece7f594df3f64c95361c0e8392bc80f1880e
opengl8080 <tomcat.port.8080+github@gmail.com>
2016-01-29 21:44:47 +0900

    Enter で次の行

--- javascript/handsontable/js/script.js
+++ javascript/handsontable/js/script.js
@@ -6,20 +6,5 @@ new Handsontable(grid, {
         [1, 2, 3],
         [1, 2, 3]
     ],
-    customBorders: [
-        {
-            range: {
-                from: {col: 0, row: 0},
-                to: {col: 1, row: 2}
-            },
-            top: {
-                width: 3,
-                color: 'red'
-            },
-            right: {
-                width: 2,
-                color: '#1199ff'
-            }
-        }
-    ]
+    enterBeginsEditing: false
 });
