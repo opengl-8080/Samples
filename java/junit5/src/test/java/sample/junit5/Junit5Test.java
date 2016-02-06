7cc4016ed2a3b9bfc095e5efc11a9b627c9c2afe
opengl8080 <tomcat.port.8080+github@gmail.com>
2016-02-06 15:30:50 +0900

    @DisplayName

--- java/junit5/src/test/java/sample/junit5/Junit5Test.java
+++ java/junit5/src/test/java/sample/junit5/Junit5Test.java
@@ -2,31 +2,19 @@ package sample.junit5;
 
 import static org.junit.gen5.api.Assertions.*;
 
+import org.junit.gen5.api.DisplayName;
 import org.junit.gen5.api.Test;
 
 public class Junit5Test {
 
-    public void test0() {
-        assertEquals("hoge", "test0");
-    }
-
     @Test
+    @DisplayName("１つ目のテストやで！")
     public void test1() {
         assertEquals("hoge", "test1");
     }
-
+    
     @Test
-    protected void test2() {
+    public void test2() {
         assertEquals("hoge", "test2");
-    }
-
-    @Test
-    void test3() {
-        assertEquals("hoge", "test3");
-    }
-
-    @Test
-    private void test4() {
-        assertEquals("hoge", "test4");
     }
 }
