67a793a4ac66ef6226db86d4f41f4ea471d91e6b
opengl8080 <tomcat.port.8080+github@gmail.com>
2016-02-06 15:55:45 +0900

    @Nested と @DisplayName の組み合わせ

--- java/junit5/src/test/java/sample/junit5/Junit5Test.java
+++ java/junit5/src/test/java/sample/junit5/Junit5Test.java
@@ -1,58 +1,27 @@
 package sample.junit5;
 
-import org.junit.gen5.api.AfterEach;
-import org.junit.gen5.api.BeforeEach;
+import static org.junit.gen5.api.Assertions.*;
+
+import org.junit.gen5.api.DisplayName;
+import org.junit.gen5.api.Nested;
 import org.junit.gen5.api.Test;
 
 public class Junit5Test {
-
-    @BeforeEach
-    public void before1() {
-        System.out.println("before1");
-    }
-
-    @BeforeEach
-    protected void before2() {
-        System.out.println("before2");
-    }
-
-    @BeforeEach
-    void before3() {
-        System.out.println("before3");
-    }
-
-    @BeforeEach
-    private void before4() {
-        System.out.println("before4");
-    }
     
-    @Test
-    public void test1() {
-        System.out.println("  test1");
-    }
-    
-    @Test
-    public void test2() {
-        System.out.println("  test2");
-    }
-
-    @AfterEach
-    public void after1() {
-        System.out.println("after1");
-    }
-
-    @AfterEach
-    protected void after2() {
-        System.out.println("after2");
-    }
-
-    @AfterEach
-    void after3() {
-        System.out.println("after3");
-    }
-
-    @AfterEach
-    private void after4() {
-        System.out.println("after4");
+    @Nested
+    @DisplayName("◯◯の場合")
+    public class NestedTest {
+        
+        @Test
+        @DisplayName("テスト１")
+        public void test1() {
+            assertEquals("foo", "bar");
+        }
+        
+        @Test
+        @DisplayName("テスト２")
+        public void test2() {
+            assertEquals("foo", "bar");
+        }
     }
 }
