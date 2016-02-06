4cbb347e54409799188d4c353030bfe1060d93ec
opengl8080 <tomcat.port.8080+github@gmail.com>
2016-02-06 15:46:47 +0900

    @BeforeEach @AfterEach

--- java/junit5/src/test/java/sample/junit5/Junit5Test.java
+++ java/junit5/src/test/java/sample/junit5/Junit5Test.java
@@ -1,70 +1,58 @@
 package sample.junit5;
 
-import org.junit.gen5.api.AfterAll;
 import org.junit.gen5.api.AfterEach;
-import org.junit.gen5.api.BeforeAll;
 import org.junit.gen5.api.BeforeEach;
 import org.junit.gen5.api.Test;
 
 public class Junit5Test {
 
-    @BeforeAll
-    public static void beforeAll1() {
-        System.out.println("beforeAll1");
-    }
-
-    @BeforeAll
-    protected static void beforeAll2() {
-        System.out.println("beforeAll2");
-    }
-
-    @BeforeAll
-    static void beforeAll3() {
-        System.out.println("beforeAll3");
-    }
-
-    @BeforeAll
-    private static void beforeAll4() {
-        System.out.println("beforeAll4");
+    @BeforeEach
+    public void before1() {
+        System.out.println("before1");
     }
 
     @BeforeEach
-    public void before() {
-        System.out.println("  before");
+    protected void before2() {
+        System.out.println("before2");
+    }
+
+    @BeforeEach
+    void before3() {
+        System.out.println("before3");
+    }
+
+    @BeforeEach
+    private void before4() {
+        System.out.println("before4");
     }
     
     @Test
     public void test1() {
-        System.out.println("    test1");
+        System.out.println("  test1");
     }
     
     @Test
     public void test2() {
-        System.out.println("    test2");
+        System.out.println("  test2");
     }
 
     @AfterEach
-    public void after() {
-        System.out.println("  after");
+    public void after1() {
+        System.out.println("after1");
     }
 
-    @AfterAll
-    public static void afterAll1() {
-        System.out.println("afterAll1");
+    @AfterEach
+    protected void after2() {
+        System.out.println("after2");
     }
 
-    @AfterAll
-    protected static void afterAll2() {
-        System.out.println("afterAll2");
+    @AfterEach
+    void after3() {
+        System.out.println("after3");
     }
 
-    @AfterAll
-    static void afterAll3() {
-        System.out.println("afterAll3");
-    }
-
-    @AfterAll
-    private static void afterAll4() {
-        System.out.println("afterAll4");
+    @AfterEach
+    private void after4() {
+        System.out.println("after4");
     }
 }
