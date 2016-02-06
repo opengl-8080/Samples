47e779077ae33545dc5bfa4a3d285a0a3d386244
opengl8080 <tomcat.port.8080+github@gmail.com>
2016-02-06 15:28:22 +0900

    @Test アノテーション

--- java/junit5/src/test/java/sample/junit5/Junit5Test.java
+++ java/junit5/src/test/java/sample/junit5/Junit5Test.java
@@ -6,13 +6,27 @@ import org.junit.gen5.api.Test;
 
 public class Junit5Test {
 
-    @Test
-    public void test1() {
-        assertEquals("hoge", "hoge");
+    public void test0() {
+        assertEquals("hoge", "test0");
     }
 
     @Test
-    public void test2() {
-        assertEquals("hoge", "fuga");
+    public void test1() {
+        assertEquals("hoge", "test1");
+    }
+
+    @Test
+    protected void test2() {
+        assertEquals("hoge", "test2");
+    }
+
+    @Test
+    void test3() {
+        assertEquals("hoge", "test3");
+    }
+
+    @Test
+    private void test4() {
+        assertEquals("hoge", "test4");
     }
 }
