2c594ebcaa88820973be70549a6053a63390e282
opengl8080 <tomcat.port.8080+github@gmail.com>
2016-02-06 19:13:10 +0900

    @Tag

--- java/junit5/src/test/java/sample/junit5/Junit5Test.java
+++ java/junit5/src/test/java/sample/junit5/Junit5Test.java
@@ -1,73 +1,31 @@
 package sample.junit5;
 
-import org.junit.gen5.api.AfterAll;
-import org.junit.gen5.api.AfterEach;
-import org.junit.gen5.api.BeforeAll;
-import org.junit.gen5.api.BeforeEach;
-import org.junit.gen5.api.Nested;
+import org.junit.gen5.api.Tag;
 import org.junit.gen5.api.Test;
 
 public class Junit5Test {
     
-    @BeforeAll
-    public static void beforeAll1() {
-        System.out.println("beforeAll1");
+    @Test
+    @Tag("hoge")
+    public void test1() {
+        System.out.println("[hoge] test1");
     }
 
-    @AfterAll
-    public static void afterAll1() {
-        System.out.println("afterAll1");
-    }
-    
-    @BeforeEach
-    public void before1() {
-        System.out.println("    before1");
+    @Test
+    @Tag("fuga")
+    public void test2() {
+        System.out.println("[fuga] test2");
     }
 
-    @AfterEach
-    public void after1() {
-        System.out.println("    after1");
+    @Test
+    @Tag("hoge") @Tag("fuga")
+    public void test3() {
+        System.out.println("[hoge, fuga] test3");
     }
-    
-    public static class Base {
-        
-        @BeforeAll
-        public static void beforeAll2() {
-            System.out.println("  beforeAll2");
-        }
 
-        @AfterAll
-        public static void afterAll2() {
-            System.out.println("  afterAll2");
-        }
-        
-        @BeforeEach
-        public void before2() {
-            System.out.println("      before2");
-        }
-
-        @AfterEach
-        public void after2() {
-            System.out.println("      after2");
-        }
-    }
-    
-    @Nested
-    public class NestedTest extends Base {
-
-        @BeforeEach
-        public void before3() {
-            System.out.println("        before3");
-        }
-        
-        @Test
-        public void test() {
-            System.out.println("          test");
-        }
-
-        @AfterEach
-        public void after3() {
-            System.out.println("        after3");
-        }
+    @Test
+    @Tag("hoge") @Tag("piyo")
+    public void test4() {
+        System.out.println("[hoge, piyo] test4");
     }
 }
