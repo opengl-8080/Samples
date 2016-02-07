25c2433d629415e9ae845c46a0d3d4ef84c03f3a
opengl8080 <tomcat.port.8080+github@gmail.com>
2016-02-07 11:23:46 +0900

    インスタンスのライフサイクル

--- java/junit5/src/test/java/sample/junit5/Junit5Test.java
+++ java/junit5/src/test/java/sample/junit5/Junit5Test.java
@@ -1,30 +1,16 @@
 package sample.junit5;
 
-import org.junit.gen5.api.BeforeAll;
-import org.junit.gen5.api.BeforeEach;
 import org.junit.gen5.api.Test;
-import org.junit.gen5.api.extension.ExtendWith;
 
-@ExtendWith(MyExtend.class)
 public class Junit5Test {
-    
-    @BeforeAll
-    public static void beforeAll() {
-        System.out.println("beforeAll");
-    }
-    
-    @BeforeEach
-    public void before() {
-        System.out.println("before");
-    }
     
     @Test
     public void test1() {
-        System.out.println("test1");
+        System.out.println("test1 : " + this.hashCode());
     }
     
     @Test
     public void test2() {
-        System.out.println("test2");
+        System.out.println("test2 : " + this.hashCode());
     }
 }
