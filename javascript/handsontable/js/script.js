bf15a25965b4f2fbccf406f9a9b7d427d0a27eef
opengl8080 <tomcat.port.8080+github@gmail.com>
2016-02-08 23:12:14 +0900

    maxRows, maxCols

--- javascript/handsontable/js/script.js
+++ javascript/handsontable/js/script.js
@@ -2,12 +2,12 @@ var grid = document.getElementById('grid');
 
 var data = [
     [1, 2, 3],
-    [1, 2, 3],
-    [1, 2, 3]
+    [2, 3, 4],
+    [3, 4, 5]
 ];
 
 new Handsontable(grid, {
     data: data,
-    colHeaders: true,
-    manualColumnResize: true
+    maxCols: 4,
+    maxRows: 7
 });
