c0b308bf9f5ac42c228ca2b92768a2e6834091e7
opengl8080 <tomcat.port.8080+github@gmail.com>
2016-02-10 23:12:35 +0900

    startCols, startRows

--- javascript/handsontable/js/script.js
+++ javascript/handsontable/js/script.js
@@ -1,12 +1,6 @@
 var grid = document.getElementById('grid');
 
-var data = [
-    [1, 2, 3],
-    [2, 3, 4],
-    [3, 4, 5]
-];
-
 var table = new Handsontable(grid, {
-    data: data,
-    placeholder: '( ^ω^)'
+    startCols: 2,
+    startRows: 3
 });
