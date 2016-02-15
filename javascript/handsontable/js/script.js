ed7b724a3c87a15c8a9daa74ff5788748c8f874b
opengl8080 <tomcat.port.8080+github@gmail.com>
2016-02-15 23:10:28 +0900

    stretchH last

--- javascript/handsontable/js/script.js
+++ javascript/handsontable/js/script.js
@@ -1,6 +1,10 @@
 var grid = document.getElementById('grid');
 
 var table = new Handsontable(grid, {
-    startCols: 2,
-    startRows: 3
+    data: [
+        [1, 2, 3],
+        [1, 2, 3],
+        [1, 2, 3]
+    ],
+    stretchH: 'last'
 });
