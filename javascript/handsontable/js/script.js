99aa5c2e5a5731dc80b20920c72cb9416dcaa598
opengl8080 <tomcat.port.8080+github@gmail.com>
2016-02-17 23:46:08 +0900

    width, height

--- javascript/handsontable/js/script.js
+++ javascript/handsontable/js/script.js
@@ -2,15 +2,16 @@ var grid = document.getElementById('grid');
 
 var table = new Handsontable(grid, {
     data: [
-        [1, 2, 3],
-        [1, 2, 3],
-        [1, 2, 3]
+        [1, 2, 3, 4, 5, 6, 7, 8, 9, 0],
+        [2, 3, 4],
+        [3, 4, 5],
+        [4, 5, 6],
+        [5, 6, 7],
+        [6, 7, 8],
+        [7, 8, 9],
+        [8, 9, 0],
+        [9, 0, 1],
+        [0, 1, 2]
     ],
-    tabMoves: function() {
-        console.log(arguments);
-        return {
-            col: 1,
-            row: 0
-        };
-    }
+    height: 100
 });
