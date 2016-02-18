aea6b5512660b8d8e417b900335090c83196033f
opengl8080 <tomcat.port.8080+github@gmail.com>
2016-02-18 22:17:02 +0900

    columns type numeric

--- javascript/handsontable/js/script.js
+++ javascript/handsontable/js/script.js
@@ -3,7 +3,7 @@ var grid = document.getElementById('grid');
 var table = new Handsontable(grid, {
     columns: [
         {
-            type: 'text'
+            type: 'numeric'
         }
     ]
 });
