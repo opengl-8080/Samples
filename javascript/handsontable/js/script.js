1347b93abfa72c13d07c39b66a5450e2cd3625d3
opengl8080 <tomcat.port.8080+github@gmail.com>
2016-02-20 00:00:06 +0900

    autocomplete

--- javascript/handsontable/js/script.js
+++ javascript/handsontable/js/script.js
@@ -1,18 +1,10 @@
 var grid = document.getElementById('grid');
 
 new Handsontable(grid, {
-    data: [
-        [false],
-        [true],
-        [false]
-    ],
     columns: [
         {
-            type: 'checkbox',
-            label: {
-                value: 'チェックボックスやで',
-                position: 'before'
-            }
+            type: 'autocomplete',
+            source: ['one', 'two', 'three', 'four', 'five']
         }
     ]
 });
