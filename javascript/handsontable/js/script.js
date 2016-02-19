eb1fc7a848cd426e82213b411cbc12f15044eb2f
opengl8080 <tomcat.port.8080+github@gmail.com>
2016-02-20 08:58:16 +0900

    password

--- javascript/handsontable/js/script.js
+++ javascript/handsontable/js/script.js
@@ -3,9 +3,7 @@ var grid = document.getElementById('grid');
 new Handsontable(grid, {
     columns: [
         {
-            type: 'autocomplete',
-            source: ['hoge', 'fuga', 'piyo'],
-            filter: false
+            type: 'password'
         }
     ]
 });
