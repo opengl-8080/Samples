cb1b457f022b17af9fa2836bffb2f9c9a152f0a1
opengl8080 <tomcat.port.8080+github@gmail.com>
2016-02-20 09:33:40 +0900

    date minDate

--- javascript/handsontable/js/script.js
+++ javascript/handsontable/js/script.js
@@ -5,10 +5,7 @@ new Handsontable(grid, {
         {
             type: 'date',
             datePickerConfig: {
-                disableDayFn: function(date) {
-                    var minDate = new Date(2016, 1, 15);
-                    return date < minDate;
-                }
+                minDate: new Date()
             }
         }
     ]
