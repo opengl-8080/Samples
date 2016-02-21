2035b12f2f8b78da737c53dfdb62a8c577263270
opengl8080 <tomcat.port.8080+github@gmail.com>
2016-02-21 09:55:54 +0900

    renderer function

--- javascript/handsontable/js/script.js
+++ javascript/handsontable/js/script.js
@@ -8,7 +8,24 @@ new Handsontable(grid, {
     ],
     columns: [
         {
-            renderer: 'html'
+            renderer: function(instance, td, row, col, prop, value, cellProperties) {
+                var escaped = Handsontable.helper.stringify(value);
+                escaped = strip_tags(escaped, '<u><a>');
+                td.innerHTML = escaped;
+            }
         }
     ]
 });
+
+// original by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+function strip_tags(input, allowed) {
+    var tags = /<\/?([a-z][a-z0-9]*)\b[^>]*>/gi,
+        commentsAndPhpTags = /<!--[\s\S]*?-->|<\?(?:php)?[\s\S]*?\?>/gi;
+
+    // making sure the allowed arg is a string containing only tags in lowercase (<a><b><c>)
+    allowed = (((allowed || "") + "").toLowerCase().match(/<[a-z][a-z0-9]*>/g) || []).join('');
+
+    return input.replace(commentsAndPhpTags, '').replace(tags, function ($0, $1) {
+        return allowed.indexOf('<' + $1.toLowerCase() + '>') > -1 ? $0 : '';
+    });
+}
