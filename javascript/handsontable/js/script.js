ecb1e679a99ca077d0ea7d6106d2835543969e8c
opengl8080 <tomcat.port.8080+github@gmail.com>
2016-02-23 22:51:11 +0900

    validator

--- javascript/handsontable/js/script.js
+++ javascript/handsontable/js/script.js
@@ -3,8 +3,10 @@ var grid = document.getElementById('grid');
 new Handsontable(grid, {
     columns: [
         {
-            type: 'numeric',
-            allowInvalid: false
+            validator: function(value, callback) {
+                console.log(value);
+                callback(value === 'hoge');
+            }
         }
     ]
 });
