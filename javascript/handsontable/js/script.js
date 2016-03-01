a921d03f71c3b4a66ce5f069d023cfbc56e6d49a
opengl8080 <tomcat.port.8080+github@gmail.com>
2016-03-01 22:21:06 +0900

    count**Rows とかの動作検証

--- javascript/handsontable/js/script.js
+++ javascript/handsontable/js/script.js
@@ -1,5 +1,25 @@
 var grid = document.getElementById('grid');
 
-var table = new Handsontable(grid);
+var data = [];
 
-table.destroy();
+for (var i=0; i<100; i++) {
+    data.push([i]);
+}
+
+var table = new Handsontable(grid, {
+    data: data,
+    height: 100,
+    width: 100,
+    contextMenu: ['row_above']
+});
+
+document
+    .querySelector('#button')
+    .addEventListener('click', function() {
+        console.log({
+            countRows: table.countRows(),
+            sourceRows: table.countSourceRows(),
+            visibleRows: table.countVisibleRows(),
+            renderedRows: table.countRenderedRows()
+        });
+    });
