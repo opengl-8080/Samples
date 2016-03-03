9072b27aa81b8d6d4f2ce31bf97588dbf6b28c79
opengl8080 <tomcat.port.8080+github@gmail.com>
2016-03-03 23:31:01 +0900

    getCellMeta

--- javascript/handsontable/js/script.js
+++ javascript/handsontable/js/script.js
@@ -1,25 +1,14 @@
 var grid = document.getElementById('grid');
 
-var data = [];
-
-for (var i=0; i<100; i++) {
-    data.push([i]);
-}
-
 var table = new Handsontable(grid, {
-    data: data,
-    height: 100,
-    width: 100,
-    contextMenu: ['row_above']
+    cell: [
+        {
+            col: 0,
+            row: 0,
+            type: 'numeric'
+        }
+    ]
 });
 
-document
-    .querySelector('#button')
-    .addEventListener('click', function() {
-        console.log({
-            countRows: table.countRows(),
-            sourceRows: table.countSourceRows(),
-            visibleRows: table.countVisibleRows(),
-            renderedRows: table.countRenderedRows()
-        });
-    });
+console.log(table.getCellMeta(0, 0));
+console.log(table.getCellMeta(0, 1));
