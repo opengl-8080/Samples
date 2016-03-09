ec8005f7a162dbd2e6157e191a72bd2411f4bb84
opengl8080 <tomcat.port.8080+github@gmail.com>
2016-03-09 23:49:06 +0900

    getDataAtProp

--- javascript/handsontable/js/script.js
+++ javascript/handsontable/js/script.js
@@ -2,10 +2,19 @@ var grid = document.getElementById('grid');
 
 var table = new Handsontable(grid, {
     data: [
-        [1, 2, 3],
-        [true, false, true],
-        ['hoge', 'fuga', 'piyo']
+        {
+            name: 'sato',
+            age: 13
+        },
+        {
+            name: 'suzuki',
+            age: 15
+        },
+        {
+            name: 'tanaka',
+            age: 18
+        }
     ]
 });
 
-console.log(table.getDataAtRow(2));
+console.log(table.getDataAtProp('name'));
