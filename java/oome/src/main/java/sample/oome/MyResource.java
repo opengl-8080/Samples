5efbf86115eec17fc442c65359c41be978a38ced
opengl8080 <tomcat.port.8080+github@gmail.com>
2016-03-13 14:14:14 +0900

    OOME 調査色々勉強

--- java/oome/src/main/java/sample/oome/MyResource.java
+++ java/oome/src/main/java/sample/oome/MyResource.java
@@ -11,20 +11,22 @@ import javax.ws.rs.QueryParam;
 @Path("")
 @RequestScoped
 public class MyResource {
-
-    private List<BigObject> list = new ArrayList<>();
+    
+//    private static List<BigObject> list = new ArrayList<>();
+    private BigObject bigObject;
     
     @GET
     public void execute(@QueryParam("size") int size) {
-        this.list.add(new BigObject(size));
-        System.out.println("done (" + size + "MB)");
+        this.bigObject = new BigObject(size);
+//        list.add(new BigObject(size));
+        System.out.println("done (" + size + ")");
     }
     
     public static class BigObject {
-        public byte[][][] data;
+        public byte[][] data;
         
         public BigObject(int size) {
-            this.data = new byte[1024][1024][size];
+            this.data = new byte[1024][size];
         }
     }
 }
