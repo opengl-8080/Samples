a0155f55180ba7763d0fbf1025f0e9adade433ee
opengl8080 <tomcat.port.8080+github@gmail.com>
2016-03-14 23:15:39 +0900

    selectCellByProp

--- javascript/handsontable/js/script.js
+++ javascript/handsontable/js/script.js
@@ -1,9 +1,11 @@
 var grid = document.getElementById('grid');
 
-var table = new Handsontable(grid);
+var table = new Handsontable(grid, {
+    data: [
+        {name: 'aaa', age: 12},
+        {name: 'bbb', age: 15},
+        {name: 'ccc', age: 18}
+    ]
+});
 
-table.selectCell(0, 1, 2, 2);
-
-var selected = table.getSelected();
-
-console.log(selected);
+table.selectCellByProp(1, 'age');
