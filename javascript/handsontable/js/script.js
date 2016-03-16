94c1a30fdca3c4ae5cadf07d9e47de5f15e54b5f
opengl8080 <tomcat.port.8080+github@gmail.com>
2016-03-16 22:51:26 +0900

    populateFromArray 繰り返し

--- javascript/handsontable/js/script.js
+++ javascript/handsontable/js/script.js
@@ -2,8 +2,8 @@ var grid = document.getElementById('grid');
 
 var table = new Handsontable(grid);
 
-table.populateFromArray(1, 1, [
+table.populateFromArray(0, 0, [
     [1, 2, 3],
     [4, 5, 6],
     [7, 8, 9]
-], 2, 3);
+], 4, 4);
