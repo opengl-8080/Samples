185e5a75a175a244574dcdd93d117f719ad28bbc
opengl8080 <tomcat.port.8080+github@gmail.com>
2016-03-16 23:12:42 +0900

    populateFromArray 基本やりお直し

--- javascript/handsontable/js/script.js
+++ javascript/handsontable/js/script.js
@@ -1,9 +1,15 @@
 var grid = document.getElementById('grid');
 
-var table = new Handsontable(grid);
+var table = new Handsontable(grid, {
+    data: [
+        ['a', 'b', 'c', 'd'],
+        ['e', 'f', 'g', 'h'],
+        ['i', 'j', 'k', 'l'],
+        ['m', 'n', 'o', 'p'],
+    ]
+});
 
-table.populateFromArray(0, 0, [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-], 4, 4);
+table.populateFromArray(0, 1, [
+    [1, 2],
+    [3, 4]
+]);
