c291615baedbb3439dfebe9ddfeeb249bb2001c6
opengl8080 <tomcat.port.8080+github@gmail.com>
2016-03-18 22:35:20 +0900

    setCellMetaObject

--- javascript/handsontable/js/script.js
+++ javascript/handsontable/js/script.js
@@ -2,5 +2,7 @@ var grid = document.getElementById('grid');
 
 var table = new Handsontable(grid);
 
-table.setCellMeta(1, 1, 'type', 'autocomplete');
-table.setCellMeta(1, 1, 'source', ['one', 'two', 'three']);
+table.setCellMetaObject(1, 1, {
+    type: 'autocomplete',
+    source: ['one', 'two', 'three']
+});
