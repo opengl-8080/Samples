7251553ec1031c17bd22859e64513c28172cd263
opengl8080 <tomcat.port.8080+github@gmail.com>
2016-03-18 22:32:38 +0900

    setCellMeta

--- javascript/handsontable/js/script.js
+++ javascript/handsontable/js/script.js
@@ -1,11 +1,6 @@
 var grid = document.getElementById('grid');
 
-var table = new Handsontable(grid, {
-    data: [
-        {name: 'Sato', age: 14, sex: 'male'},
-        {name: 'Suzuki', age: 16, sex: 'female'},
-        {name: 'Tanaka', age: 18, sex: 'male'}
-    ]
-});
+var table = new Handsontable(grid);
 
-console.log('age = ' + table.propToCol('age'));
+table.setCellMeta(1, 1, 'type', 'autocomplete');
+table.setCellMeta(1, 1, 'source', ['one', 'two', 'three']);
