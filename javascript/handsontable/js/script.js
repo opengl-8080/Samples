6dd6d839ccb108332425fb8c8d73c5ac941180c7
opengl8080 <tomcat.port.8080+github@gmail.com>
2016-03-28 23:54:33 +0900

    cells

--- javascript/handsontable/js/script.js
+++ javascript/handsontable/js/script.js
@@ -1,27 +1,7 @@
 var grid = document.getElementById('grid');
 
 new Handsontable(grid, {
-    data: [
-        [
-            'text', 'text', 'text',
-            1, 2, 3,
-            '2015/01/01', '2015/01/01', '2015/01/01',
-            'foo', 'bar', 'foo'
-        ]
-    ],
-    allowEmpty: false,
-    columns: [
-        {type: 'text'},
-        {allowEmpty: true, type: 'text'},
-        {allowEmpty: false, type: 'text'},
-        {type: 'numeric'},
-        {allowEmpty: true, type: 'numeric'},
-        {allowEmpty: false, type: 'numeric'},
-        {type: 'date'},
-        {allowEmpty: true, type: 'date'},
-        {allowEmpty: false, type: 'date'},
-        {type: 'autocomplete', source: ['foo', 'bar']},
-        {allowEmpty: true, type: 'autocomplete', source: ['foo', 'bar']},
-        {allowEmpty: false, type: 'autocomplete', source: ['foo', 'bar']},
-    ]
+    cells: function(row, cols, prop) {
+        return row % 2 === 0 ? {type: 'date'} : {type: 'numeric'};
+    }
 });
