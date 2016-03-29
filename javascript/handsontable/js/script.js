64e06ca294d0a4f6dbd01e100b796ed40a86a326
opengl8080 <tomcat.port.8080+github@gmail.com>
2016-03-29 22:49:47 +0900

    search 基本

--- javascript/handsontable/js/script.js
+++ javascript/handsontable/js/script.js
@@ -1,5 +1,13 @@
 var grid = document.getElementById('grid');
 
-new Handsontable(grid, {
-    disableVisualSelection: 'area'
+var table = new Handsontable(grid, {
+    data: [
+        ['one', 'two', 'three'],
+        ['four', 'five', 'six'],
+        ['seven', 'eight', 'nine']
+    ],
+    search: true
 });
+
+var result = table.search.query('T');
+console.dir(result);
