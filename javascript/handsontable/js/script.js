7979b53157c19a73a0b3238917c3b6fbc7820523
opengl8080 <tomcat.port.8080+github@gmail.com>
2016-03-31 23:03:54 +0900

    selectOptions

--- javascript/handsontable/js/script.js
+++ javascript/handsontable/js/script.js
@@ -1,22 +1,10 @@
 var grid = document.getElementById('grid');
 
-var table = new Handsontable(grid, {
-    data: [
-        ['one', 'two', 'three'],
-        ['four', 'five', 'six'],
-        ['seven', 'eight', 'nine']
-    ],
-    search: true
+new Handsontable(grid, {
+    columns: [
+        {
+            editor: 'select',
+            selectOptions: ['hoge', 'fuga', 'piyo']
+        }
+    ]
 });
-
-Handsontable.Search.global.setDefaultQueryMethod(function (query, value) {
-    console.dir(arguments);
-  
-    if (typeof query == 'undefined' || query == null || !query.toLowerCase || query.length === 0) {
-        return false;
-    }
-    
-    return value.toString().toLowerCase().indexOf(query.toLowerCase()) !== -1;
-});
-
-table.search.query('T');
