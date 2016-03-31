76fdec57e10303c0ad2411ede8e5101bf070ecb0
opengl8080 <tomcat.port.8080+github@gmail.com>
2016-03-31 23:29:42 +0900

    undo

--- javascript/handsontable/js/script.js
+++ javascript/handsontable/js/script.js
@@ -1,5 +1,13 @@
 var grid = document.getElementById('grid');
 
-new Handsontable(grid, {
-    trimWhitespace: false
+var table = new Handsontable(grid, {
+    undo: true
+});
+
+document.querySelector('#undo').addEventListener('click', function() {
+    table.undo();
+});
+
+document.querySelector('#redo').addEventListener('click', function() {
+    table.redo();
 });
