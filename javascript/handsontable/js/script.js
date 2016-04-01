c31833c9ab2922a77c7a78ed6b590586cc41775b
opengl8080 <tomcat.port.8080+github@gmail.com>
2016-04-01 22:38:40 +0900

    title

--- javascript/handsontable/js/script.js
+++ javascript/handsontable/js/script.js
@@ -1,13 +1,8 @@
 var grid = document.getElementById('grid');
 
-var table = new Handsontable(grid, {
-    undo: true
-});
-
-document.querySelector('#undo').addEventListener('click', function() {
-    table.undo();
-});
-
-document.querySelector('#redo').addEventListener('click', function() {
-    table.redo();
+new Handsontable(grid, {
+    columns: [
+        {title: 'foo'},
+        {}
+    ]
 });
