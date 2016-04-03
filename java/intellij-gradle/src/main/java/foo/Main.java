5cb8197d5a1a55b7c073c4a30d3a775a17a1b724
opengl8080 <tomcat.port.8080+github@gmail.com>
2016-04-03 20:41:38 +0900

    intellij で Gradle のプロジェクトを取り込んでみたら簡単だった

---
+++ java/intellij-gradle/src/main/java/foo/Main.java
@@ -0,0 +1,10 @@
+package foo;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class Main {
+    public static void main(String... args) {
+        StringUtils.isEmpty("");
+        System.out.println("Hello Gradle!!");
+    }
+}
