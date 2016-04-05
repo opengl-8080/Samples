1cf3aab32ab34aa4b362dbef43746e9cbf9319aa
opengl8080 <tomcat.port.8080+github@gmail.com>
2016-04-05 23:05:42 +0900

    add context

--- javascript/handsontable/js/script.js
+++ javascript/handsontable/js/script.js
@@ -1,9 +1,9 @@
-var grid = document.getElementById('grid');
+var grid1 = document.getElementById('grid1');
+var grid2 = document.getElementById('grid2');
 
-var table = new Handsontable(grid, {
-    comments: true
-});
+var table1 = new Handsontable(grid1);
+var table2 = new Handsontable(grid2);
 
-table.add('afterSelection', function() {
+Handsontable.hooks.add('afterSelection', function() {
     console.log(arguments);
-});
+}, table1);
