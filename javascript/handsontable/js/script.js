7b9d39814b62b1493da8cfc8f6beb264db6f9c68
opengl8080 <tomcat.port.8080+github@gmail.com>
2016-04-08 23:24:46 +0900

    Hooks run arguments

--- javascript/handsontable/js/script.js
+++ javascript/handsontable/js/script.js
@@ -2,15 +2,12 @@ var grid = document.getElementById('grid');
 
 var table = new Handsontable(grid);
 
-// 独自イベントの追加
 Handsontable.hooks.register('onButtonClick');
 
-// イベントハンドラの登録
 Handsontable.hooks.add('onButtonClick', function () {
-    console.log('click!!');
+    console.log(arguments);
 });
 
 document.getElementById('button').addEventListener('click', function() {
-    // 独自イベントを発火
-    Handsontable.hooks.run(table, 'onButtonClick');
+    Handsontable.hooks.run(table, 'onButtonClick', 1, 2, 3, 4, 5, 6, 7);
 });
