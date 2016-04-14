3e4eee741d83af51b186e81e1825b72733cfcfff
opengl8080 <tomcat.port.8080+github@gmail.com>
2016-04-15 00:15:25 +0900

    webpack loader pipe

--- javascript/webpack/entry.js
+++ javascript/webpack/entry.js
@@ -1,2 +1,7 @@
-var message = require('raw!./message.txt');
-console.log(message);
+var json = require('json!./sample.json');
+console.log(typeof json);
+console.log(json);
+
+var tostring = require('to-string!json!./sample.json');
+console.log(typeof tostring);
+console.log(tostring);
