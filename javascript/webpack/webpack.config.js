dfd862af75a4c9d6fedbb707bdca23dec629c7b3
opengl8080 <tomcat.port.8080+github@gmail.com>
2016-04-16 20:35:37 +0900

    webpack config path.resolve

--- javascript/webpack/webpack.config.js
+++ javascript/webpack/webpack.config.js
@@ -1,14 +1,11 @@
 var path = require('path');
 
+console.log(__dirname);
+console.log(path.resolve(__dirname, './foo/bar'));
+
 module.exports = {
     entry: './entry.js',
     output: {
         filename: 'bundle.js'
-    },
-    resolve: {
-        alias: {
-            absolutePath: path.resolve(__dirname, './value.js'),
-            relativePath: './value.js'
-        }
     }
 };
