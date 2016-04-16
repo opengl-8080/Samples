3130b7be72554d64631b29a1aee2cf50babf5752
opengl8080 <tomcat.port.8080+github@gmail.com>
2016-04-16 19:50:40 +0900

    webpack config output.path

--- javascript/webpack/entry.js
+++ javascript/webpack/entry.js
@@ -1,7 +1,1 @@
-var foo = require('./foo/foo.json');
-console.log('typeof foo = ' + (typeof foo));
-console.log('foo = ' + foo);
-
-var bar = require('./bar/sub/bar.json');
-console.log('typeof bar = ' + (typeof bar));
-console.log('bar = ' + bar);
+console.log('Hello World!!');
--- javascript/webpack/webpack.config.js
+++ javascript/webpack/webpack.config.js
@@ -1,26 +1,7 @@
-var path = require('path');
-
 module.exports = {
     entry: './entry.js',
     output: {
-        filename: './bundle.js'
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.json$/,
-                include: [
-                    path.resolve(__dirname, 'foo')
-                ],
-                loader: 'raw'
-            },
-            {
-                test: /\.json$/,
-                include: [
-                    /bar/
-                ],
-                loader: 'json'
-            },
-        ]
+        path: './output/foo/bar',
+        filename: 'bundle.js'
     }
 };
