5fb4a65a387f8c7fe7876f4d67159cabb21eb26b
opengl-8080 <tomcat.port.8080+github@gmail.com>
2016-04-28 23:43:10 +0900

    kotlin オブジェクトの比較方法

--- kotlin/hello/src/main/java/Foo.java
+++ kotlin/hello/src/main/java/Foo.java
@@ -1,5 +1,5 @@
 public class Foo {
 
-    public static void method(Runnable runnable) {
+    public static void main(String[] args) {
     }
 }
--- kotlin/hello/src/main/kotlin/hello.kt
+++ kotlin/hello/src/main/kotlin/hello.kt
@@ -1,6 +1,20 @@
 fun main(args: Array<String>) {
-    val f : Runnable = Runnable {println("Hello")}
+
+    val a = Hoge("a")
+    val b = Hoge("a")
+
+    println(a === b)
+    println(a == b)
 }
 
-fun function(runnable: Runnable) {
+class Hoge(val name: String) {
+
+    override fun equals(o: Any?): Boolean {
+        if (this === o) return true
+        if (!(o is Hoge)) return false
+
+        println("equals()!!")
+
+        return this.name == o.name
+    }
 }
