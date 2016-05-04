992636a650435a396aff29e54334e7c8d189dd2c
opengl-8080 <tomcat.port.8080+github@gmail.com>
2016-05-04 23:35:13 +0900

    kotlin @Deprecated

--- kotlin/hello/src/main/kotlin/hello.kt
+++ kotlin/hello/src/main/kotlin/hello.kt
@@ -1,18 +1,7 @@
 fun main(args: Array<String>) {
-    val integerHoge = Hoge<Int>(10)
-    val numberBase: Base<Int> = integerHoge
-
-    println(numberBase.get())
+    hoge()
 }
 
-interface Base<out T> {
-
-    fun get(): T
-}
-
-class Hoge<U>(private val u: U): Base<U> {
-
-    override fun get(): U {
-        return this.u
-    }
+@Deprecated("使うな！！")
+fun hoge() {
 }
