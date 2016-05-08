8a4b40ced93831244b42f4f69d23abc202658120
opengl-8080 <tomcat.port.8080+github@gmail.com>
2016-05-08 12:45:46 +0900

    Kotlin Collection

--- kotlin/hello/src/main/java/Main.java
+++ kotlin/hello/src/main/java/Main.java
@@ -1,3 +1,5 @@
+import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -8,5 +10,33 @@ public class Main {
         List<? extends Number> numberList = integerList;
 
         numberList.forEach(n -> System.out.println(n));
+
+        List<Integer> intList = new ArrayList<>();
+        List<? super Number> stringList = new ArrayList<>();
+
+        stringList.add(new Integer(20));
+        stringList.add(new Double(100));
+        stringList.add(new Long(100));
+        stringList.add(new BigDecimal(200));
+
+        Object s = stringList.get(0);
+
+        hoge(new ArrayList<Object>());
+        hoge(new ArrayList<Number>());
+//        hoge(new ArrayList<Integer>());
+//
+//        fuga(new ArrayList<Object>());
+        fuga(new ArrayList<Number>());
+        fuga(new ArrayList<Integer>());
+    }
+
+    public static void hoge(List<? super Number> list) {
+        Object object = list.get(0);
+        list.add(10);
+    }
+
+    public static void fuga(List<? extends Number> list) {
+        Number number = list.get(0);
+//        list.add(10);
     }
 }
--- kotlin/hello/src/main/kotlin/hello.kt
+++ kotlin/hello/src/main/kotlin/hello.kt
@@ -1,13 +1,2 @@
 fun main(args: Array<String>) {
-    val map = mapOf(Pair("one", "ONE"), Pair("two", "TWO"), Pair("three", "THREE"))
-    val keys = listOf("one", "three")
-
-    val list: List<String> =
-                            map
-                                .filterKeys { keys.contains(it) }
-                                .values
-                                .toList()
-
-    println(list)
 }
-
