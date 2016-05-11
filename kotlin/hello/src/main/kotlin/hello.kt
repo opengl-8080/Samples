811a14d5b14eef2f5063817479620261ada027f9
opengl-8080 <tomcat.port.8080+github@gmail.com>
2016-05-11 23:02:16 +0900

    Kotlin override parent field getter

--- kotlin/hello/src/main/kotlin/hello.kt
+++ kotlin/hello/src/main/kotlin/hello.kt
@@ -1,2 +1,12 @@
 fun main(args: Array<String>) {
 }
+
+open class Parent {
+    open fun getValue() = "parent"
+}
+
+class Child: Parent() {
+    private val value = "child"
+
+    override fun getValue() = this.value
+}
