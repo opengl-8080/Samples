58b7d646add7e60105a05c56b8f0f1f655c76df2
opengl-8080 <tomcat.port.8080+github@gmail.com>
2016-05-29 12:09:42 +0900

    Kotlin Collection associateBy()

--- kotlin/hello/src/main/kotlin/hello.kt
+++ kotlin/hello/src/main/kotlin/hello.kt
@@ -1,7 +1,7 @@
 fun main(args: Array<String>) {
     val iterable: Iterable<Int> = listOf(1, 2, 3)
 
-    val map = iterable.associate { it to it*10 }
+    val map = iterable.associateBy { it*10 }
 
     println(map)
 }
