3164d5e2fd1d3e42f5b9f129d297ee014e2b4801
opengl-8080 <tomcat.port.8080+github@gmail.com>
2016-05-29 21:35:10 +0900

    Kotlin collections elementAtOrElse()

--- kotlin/hello/src/main/kotlin/hello.kt
+++ kotlin/hello/src/main/kotlin/hello.kt
@@ -1,5 +1,6 @@
 fun main(args: Array<String>) {
-    val iterable: Iterable<Int> = listOf(1, 2, 3, 4, 5)
+    val iterable: Iterable<Int> = listOf(1, 2, 3)
 
-    println(iterable.elementAt(2))
+    println(iterable.elementAtOrElse(0, { it * 10 }))
+    println(iterable.elementAtOrElse(5, { it * 10 }))
 }
