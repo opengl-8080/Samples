16c9931b11ce4b0370a1c4d771a81ff09f627f84
opengl-8080 <tomcat.port.8080+github@gmail.com>
2016-05-29 22:30:15 +0900

    Kotlin collections flatten()

--- kotlin/hello/src/main/kotlin/hello.kt
+++ kotlin/hello/src/main/kotlin/hello.kt
@@ -1,10 +1,9 @@
 fun main(args: Array<String>) {
-    val result = mutableListOf(999)
     val iterable: Iterable<Iterable<Int>> = listOf(
         listOf(1, 2, 3),
         listOf(4, 5, 6),
         listOf(7, 8, 9)
     )
 
-    println(iterable.flatMapTo(result) { it })
+    println(iterable.flatten())
 }
