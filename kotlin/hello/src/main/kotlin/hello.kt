f1e1ab930a10708c488f10ac4b19ff7e638bf75f
opengl-8080 <tomcat.port.8080+github@gmail.com>
2016-05-29 21:36:52 +0900

    Kotlin collections elementAtOrNull()

--- kotlin/hello/src/main/kotlin/hello.kt
+++ kotlin/hello/src/main/kotlin/hello.kt
@@ -1,6 +1,6 @@
 fun main(args: Array<String>) {
     val iterable: Iterable<Int> = listOf(1, 2, 3)
 
-    println(iterable.elementAtOrElse(0, { it * 10 }))
-    println(iterable.elementAtOrElse(5, { it * 10 }))
+    println(iterable.elementAtOrNull(0))
+    println(iterable.elementAtOrNull(5))
 }
