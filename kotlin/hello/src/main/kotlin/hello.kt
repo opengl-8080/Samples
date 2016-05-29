6f5cf38d6d8d35099fa8dbc59cb571fd7f7b2908
opengl-8080 <tomcat.port.8080+github@gmail.com>
2016-05-29 21:43:37 +0900

    Kotlin collections filterIndexedTo()

--- kotlin/hello/src/main/kotlin/hello.kt
+++ kotlin/hello/src/main/kotlin/hello.kt
@@ -1,7 +1,10 @@
 fun main(args: Array<String>) {
+    val result = mutableListOf(9);
     val iterable: Iterable<Int> = listOf(1, 2, 3)
 
-    println(iterable.filterIndexed { index, value ->
+    iterable.filterIndexedTo(result) { index, value ->
         index == 0 || value == 3
-    })
+    }
+
+    println(result)
 }
