5e3ce163a44e7e261fe7e0b98d835f73381e679f
opengl-8080 <tomcat.port.8080+github@gmail.com>
2016-05-29 22:11:14 +0900

    Kotlin collections find()

--- kotlin/hello/src/main/kotlin/hello.kt
+++ kotlin/hello/src/main/kotlin/hello.kt
@@ -1,6 +1,5 @@
 fun main(args: Array<String>) {
-    val result = mutableListOf(9)
     val iterable: Iterable<Int> = listOf(1, 2, 3, 4,  5)
 
-    println(iterable.filterTo(result, { it % 2 == 0 }))
+    println(iterable.find { it % 2 == 0 })
 }
