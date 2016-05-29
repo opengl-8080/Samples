089e6e327e71618fc734c314ee8762601ef311b5
opengl-8080 <tomcat.port.8080+github@gmail.com>
2016-05-29 12:21:47 +0900

    Kotlin Collection contains()

--- kotlin/hello/src/main/kotlin/hello.kt
+++ kotlin/hello/src/main/kotlin/hello.kt
@@ -1,7 +1,6 @@
 fun main(args: Array<String>) {
     val iterable: Iterable<Int> = listOf(1, 2, 3)
 
-    val average = iterable.average()
-
-    println(average)
+    println(iterable.contains(2))
+    println(iterable.contains(4))
 }
