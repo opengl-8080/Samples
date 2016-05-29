9b7feedce6097571e2944747035ebf11ed09c3b1
opengl-8080 <tomcat.port.8080+github@gmail.com>
2016-05-29 21:38:16 +0900

    Kotlin collections filter()

--- kotlin/hello/src/main/kotlin/hello.kt
+++ kotlin/hello/src/main/kotlin/hello.kt
@@ -1,6 +1,5 @@
 fun main(args: Array<String>) {
     val iterable: Iterable<Int> = listOf(1, 2, 3)
 
-    println(iterable.elementAtOrNull(0))
-    println(iterable.elementAtOrNull(5))
+    println(iterable.filter { it % 2 != 0 })
 }
