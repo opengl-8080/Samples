e247dbb8d37ab1d47dd5a20e8d281d35aaeae364
opengl-8080 <tomcat.port.8080+github@gmail.com>
2016-05-29 22:04:26 +0900

    Kotlin collections filterNotNull()

--- kotlin/hello/src/main/kotlin/hello.kt
+++ kotlin/hello/src/main/kotlin/hello.kt
@@ -1,5 +1,5 @@
 fun main(args: Array<String>) {
-    val iterable: Iterable<Int> = listOf(1, 2, 3, 4, 5)
+    val iterable: Iterable<Int?> = listOf(1, null, 3, null, 5)
 
-    println(iterable.filterNot { it % 2 == 0 })
+    println(iterable.filterNotNull())
 }
