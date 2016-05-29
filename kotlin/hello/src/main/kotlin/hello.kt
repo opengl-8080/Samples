5f1739cfb2e84ed6f3dc49cc893d0a7acfdf58ee
opengl-8080 <tomcat.port.8080+github@gmail.com>
2016-05-29 22:10:16 +0900

    Kotlin collections filterTo()

--- kotlin/hello/src/main/kotlin/hello.kt
+++ kotlin/hello/src/main/kotlin/hello.kt
@@ -2,5 +2,5 @@ fun main(args: Array<String>) {
     val result = mutableListOf(9)
     val iterable: Iterable<Int> = listOf(1, 2, 3, 4,  5)
 
-    println(iterable.filterNotTo(result, { it % 2 == 0 }))
+    println(iterable.filterTo(result, { it % 2 == 0 }))
 }
