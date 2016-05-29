e0b8a3608b98b25b7c1b71a571c25bafb0dfc068
opengl-8080 <tomcat.port.8080+github@gmail.com>
2016-05-29 12:16:01 +0900

    Kotlin Collection associateByTo() valueSelector

--- kotlin/hello/src/main/kotlin/hello.kt
+++ kotlin/hello/src/main/kotlin/hello.kt
@@ -2,7 +2,7 @@ fun main(args: Array<String>) {
     val resultMap = mutableMapOf(9 to 99)
     val iterable: Iterable<Int> = listOf(1, 2, 3)
 
-    iterable.associateByTo(resultMap) { it*10 }
+    iterable.associateByTo(resultMap, { it*10 }, { it*100 })
 
     println(resultMap)
 }
