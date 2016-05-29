79943141d4ad46a077bfd8c58574f9edf3cf13d8
opengl-8080 <tomcat.port.8080+github@gmail.com>
2016-05-29 11:59:34 +0900

    Kotlin Collection any()

--- kotlin/hello/src/main/kotlin/hello.kt
+++ kotlin/hello/src/main/kotlin/hello.kt
@@ -1,6 +1,6 @@
 fun main(args: Array<String>) {
     val iterable: Iterable<Int> = listOf(1, 2, 3)
 
-    println(iterable.all { it < 4 })
-    println(iterable.all { it < 3 })
+    println(iterable.any { it < 4 })
+    println(iterable.any { it < 0 })
 }
