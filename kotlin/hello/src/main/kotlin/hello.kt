5db1334a677759299552f3857b8a05c25c638aa9
opengl-8080 <tomcat.port.8080+github@gmail.com>
2016-05-29 12:20:45 +0900

    Kotlin Collection average()

--- kotlin/hello/src/main/kotlin/hello.kt
+++ kotlin/hello/src/main/kotlin/hello.kt
@@ -1,8 +1,7 @@
 fun main(args: Array<String>) {
-    val resultMap = mutableMapOf(9 to 99)
     val iterable: Iterable<Int> = listOf(1, 2, 3)
 
-    iterable.associateTo(resultMap) { it to it*100 }
+    val average = iterable.average()
 
-    println(resultMap)
+    println(average)
 }
