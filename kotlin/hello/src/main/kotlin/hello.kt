da3a74de7d4b1b607475a462b9f4548dae726a08
opengl-8080 <tomcat.port.8080+github@gmail.com>
2016-05-29 23:10:24 +0900

    Kotlin collections  forEachIndexed()

--- kotlin/hello/src/main/kotlin/hello.kt
+++ kotlin/hello/src/main/kotlin/hello.kt
@@ -1,5 +1,5 @@
 fun main(args: Array<String>) {
     val iterable: Iterable<Int> = listOf(1, 2, 3)
 
-    iterable.forEach { println(it) }
+    iterable.forEachIndexed { index, value -> println("$index = $value") }
 }
