bf2b19675978ad068ebdaa873cf1f1c1a21af08b
opengl-8080 <tomcat.port.8080+github@gmail.com>
2016-05-29 22:02:09 +0900

    Kotlin collections filterNot()

--- kotlin/hello/src/main/kotlin/hello.kt
+++ kotlin/hello/src/main/kotlin/hello.kt
@@ -1,8 +1,5 @@
 fun main(args: Array<String>) {
-    val result = mutableListOf<Number>(9)
-    val iterable: Iterable<*> = listOf(1, "foo", 2.4, false)
+    val iterable: Iterable<Int> = listOf(1, 2, 3, 4, 5)
 
-    iterable.filterIsInstanceTo(result, Integer::class.java)
-
-    println(result)
+    println(iterable.filterNot { it % 2 == 0 })
 }
