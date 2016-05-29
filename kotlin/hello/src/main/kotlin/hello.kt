2e479554760929ad85943d631092a5602426908c
opengl-8080 <tomcat.port.8080+github@gmail.com>
2016-05-29 21:57:42 +0900

    Kotlin collections filterIsInstanceTo()

--- kotlin/hello/src/main/kotlin/hello.kt
+++ kotlin/hello/src/main/kotlin/hello.kt
@@ -1,7 +1,8 @@
 fun main(args: Array<String>) {
+    val result = mutableListOf<Number>(9)
     val iterable: Iterable<*> = listOf(1, "foo", 2.4, false)
 
-    val result = iterable.filterIsInstance(String::class.java)
+    iterable.filterIsInstanceTo(result, Integer::class.java)
 
     println(result)
 }
