cc4cd168c65cda1cd3a355a39e87489231e53df9
opengl-8080 <tomcat.port.8080+github@gmail.com>
2016-05-29 12:22:50 +0900

    Kotlin Collection count()

--- kotlin/hello/src/main/kotlin/hello.kt
+++ kotlin/hello/src/main/kotlin/hello.kt
@@ -1,6 +1,5 @@
 fun main(args: Array<String>) {
     val iterable: Iterable<Int> = listOf(1, 2, 3)
 
-    println(iterable.contains(2))
-    println(iterable.contains(4))
+    println(iterable.count())
 }
