921084b4e1200120a9019af649c99de5c5bdb778
opengl-8080 <tomcat.port.8080+github@gmail.com>
2016-05-29 22:16:17 +0900

    Kotlin collections firstOrNull()

--- kotlin/hello/src/main/kotlin/hello.kt
+++ kotlin/hello/src/main/kotlin/hello.kt
@@ -1,7 +1,7 @@
 fun main(args: Array<String>) {
     val iterable: Iterable<Int> = listOf(1, 2, 3, 4, 5)
 
-    println(iterable.first())
-    println(iterable.first { it % 2 == 0 })
-    println(iterable.first { it % 6 == 0 })
+    println(iterable.firstOrNull())
+    println(iterable.firstOrNull { it % 2 == 0 })
+    println(iterable.firstOrNull { it % 6 == 0 })
 }
