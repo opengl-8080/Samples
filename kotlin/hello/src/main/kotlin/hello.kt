c6b419d0910a0324bac039a40062d2a79fadc18c
opengl-8080 <tomcat.port.8080+github@gmail.com>
2016-05-30 23:34:35 +0900

    Kotlin Collections last()

--- kotlin/hello/src/main/kotlin/hello.kt
+++ kotlin/hello/src/main/kotlin/hello.kt
@@ -1,5 +1,7 @@
 fun main(args: Array<String>) {
     val iterable: Iterable<Int> = listOf(1, 2, 3, 4, 5)
 
-    println(iterable.joinToString())
+    println(iterable.last())
+    println(iterable.last { it % 2 == 0 })
+    println(iterable.last { it % 6 == 0 })
 }
