5bc80d8f4d16f0bca3ed46f75175b70d00db7dc2
opengl-8080 <tomcat.port.8080+github@gmail.com>
2016-05-30 23:37:17 +0900

    Kotlin Collections map()

--- kotlin/hello/src/main/kotlin/hello.kt
+++ kotlin/hello/src/main/kotlin/hello.kt
@@ -1,7 +1,5 @@
 fun main(args: Array<String>) {
     val iterable: Iterable<Int> = listOf(1, 2, 3, 4, 5)
 
-    println(iterable.lastOrNull())
-    println(iterable.lastOrNull { it % 2 == 0 })
-    println(iterable.lastOrNull { it % 6 == 0 })
+    println(iterable.map { it * 10 })
 }
