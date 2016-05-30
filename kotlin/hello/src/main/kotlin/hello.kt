74feed3d85be74cf9d8cdcd2c9058e8396b94ac3
opengl-8080 <tomcat.port.8080+github@gmail.com>
2016-05-30 23:11:23 +0900

    Kotlin Collections groupByTo()

--- kotlin/hello/src/main/kotlin/hello.kt
+++ kotlin/hello/src/main/kotlin/hello.kt
@@ -1,17 +1,17 @@
 fun main(args: Array<String>) {
     val iterable: Iterable<Int> = listOf(1, 2, 3, 4, 5, 6, 7)
 
-    val result1: Map<String, List<Int>> =
-        iterable.groupBy { if (it % 2 == 0) "偶数" else "奇数" }
+    val result1: MutableMap<String, MutableList<Int>> = mutableMapOf("foo" to mutableListOf(9))
 
+    iterable.groupByTo(result1) { if (it % 2 == 0) "偶数" else "奇数" }
     println(result1)
 
+    val result2: MutableMap<String, MutableList<String>> = mutableMapOf("bar" to mutableListOf("BAR"))
 
-    val result2: Map<String, List<String>> =
-        iterable.groupBy(
-            { if (it % 2 == 0) "偶数" else "奇数" },
-            { "<$it>"}
-        )
-
+    iterable.groupByTo(
+        result2,
+        { if (it % 2 == 0) "偶数" else "奇数" },
+        { "<$it>"}
+    )
     println(result2)
 }
