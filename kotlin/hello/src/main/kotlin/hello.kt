158e2c18982e68e7349cbc260567847553af270b
opengl-8080 <tomcat.port.8080+github@gmail.com>
2016-05-30 23:43:20 +0900

    Kotlin Collections mapIndexedNotNullTo()

--- kotlin/hello/src/main/kotlin/hello.kt
+++ kotlin/hello/src/main/kotlin/hello.kt
@@ -1,5 +1,10 @@
 fun main(args: Array<String>) {
     val iterable: Iterable<Int> = listOf(1, 2, 3, 4, 5)
+    val result = mutableListOf("Z")
 
-    println(iterable.mapIndexed { index, value -> "$index>$value" })
+    iterable.mapIndexedNotNullTo(result) { index, value ->
+        if (value % 2 == 0) "$index($value)" else null
+    }
+
+    println(result)
 }
