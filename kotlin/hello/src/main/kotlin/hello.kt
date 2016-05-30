2ca03cc4b64da18979d72f28e881ccf2ec8235c1
opengl-8080 <tomcat.port.8080+github@gmail.com>
2016-05-30 23:17:35 +0900

    Kotlin Collections intersect()

--- kotlin/hello/src/main/kotlin/hello.kt
+++ kotlin/hello/src/main/kotlin/hello.kt
@@ -1,5 +1,8 @@
 fun main(args: Array<String>) {
     val iterable: Iterable<Int> = listOf(1, 2, 3, 4, 5)
+    val other: Iterable<Int> = listOf(2, 3, 5, 2)
 
-    println(iterable.indexOfLast { it % 2 == 0 })
+    val result: Set<Int> = iterable.intersect(other)
+
+    println(result)
 }
