dd9bc824534fe7695a44b30595135953998b6bac
opengl-8080 <tomcat.port.8080+github@gmail.com>
2016-05-30 23:35:54 +0900

    Kotlin Collections lastOrNull()

--- kotlin/hello/src/main/kotlin/hello.kt
+++ kotlin/hello/src/main/kotlin/hello.kt
@@ -1,7 +1,7 @@
 fun main(args: Array<String>) {
     val iterable: Iterable<Int> = listOf(1, 2, 3, 4, 5)
 
-    println(iterable.last())
-    println(iterable.last { it % 2 == 0 })
-    println(iterable.last { it % 6 == 0 })
+    println(iterable.lastOrNull())
+    println(iterable.lastOrNull { it % 2 == 0 })
+    println(iterable.lastOrNull { it % 6 == 0 })
 }
