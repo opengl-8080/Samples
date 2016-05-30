9df4219c2951a54f43c6ed090277bc14899cb3bf
opengl-8080 <tomcat.port.8080+github@gmail.com>
2016-05-30 23:06:38 +0900

    Kotlin Collections groupBy()

--- kotlin/hello/src/main/kotlin/hello.kt
+++ kotlin/hello/src/main/kotlin/hello.kt
@@ -1,5 +1,17 @@
 fun main(args: Array<String>) {
-    val iterable: Iterable<Int> = listOf(1, 2, 3)
+    val iterable: Iterable<Int> = listOf(1, 2, 3, 4, 5, 6, 7)
 
-    iterable.forEachIndexed { index, value -> println("$index = $value") }
+    val result1: Map<String, List<Int>> =
+        iterable.groupBy { if (it % 2 == 0) "偶数" else "奇数" }
+
+    println(result1)
+
+
+    val result2: Map<String, List<String>> =
+        iterable.groupBy(
+            { if (it % 2 == 0) "偶数" else "奇数" },
+            { "<$it>"}
+        )
+
+    println(result2)
 }
