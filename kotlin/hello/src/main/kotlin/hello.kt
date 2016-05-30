d3ff81b3e2d553c121b15a1d96b17f7ce031f0b0
opengl-8080 <tomcat.port.8080+github@gmail.com>
2016-05-30 23:13:44 +0900

    Kotlin Collections indexOfFirst()

--- kotlin/hello/src/main/kotlin/hello.kt
+++ kotlin/hello/src/main/kotlin/hello.kt
@@ -1,17 +1,5 @@
 fun main(args: Array<String>) {
-    val iterable: Iterable<Int> = listOf(1, 2, 3, 4, 5, 6, 7)
+    val iterable: Iterable<Int> = listOf(1, 2, 3, 4, 5)
 
-    val result1: MutableMap<String, MutableList<Int>> = mutableMapOf("foo" to mutableListOf(9))
-
-    iterable.groupByTo(result1) { if (it % 2 == 0) "偶数" else "奇数" }
-    println(result1)
-
-    val result2: MutableMap<String, MutableList<String>> = mutableMapOf("bar" to mutableListOf("BAR"))
-
-    iterable.groupByTo(
-        result2,
-        { if (it % 2 == 0) "偶数" else "奇数" },
-        { "<$it>"}
-    )
-    println(result2)
+    println(iterable.indexOfFirst { it % 2 == 0 })
 }
