65ac81b2a751ceda1e9f4b01639e24a54c07081b
opengl-8080 <tomcat.port.8080+github@gmail.com>
2016-05-30 23:28:15 +0900

    Kotlin Collections joinTo() parameters

--- kotlin/hello/src/main/kotlin/hello.kt
+++ kotlin/hello/src/main/kotlin/hello.kt
@@ -2,7 +2,15 @@ fun main(args: Array<String>) {
     val iterable: Iterable<Int> = listOf(1, 2, 3, 4, 5)
     val buffer: Appendable = StringBuilder()
 
-    iterable.joinTo(buffer)
+    iterable.joinTo(
+        buffer = buffer,
+        separator = " - ",
+        prefix = "{",
+        postfix = "}",
+        limit = 3,
+        truncated = "(ry",
+        transform = { "<$it>" }
+    )
 
     println(buffer)
 }
