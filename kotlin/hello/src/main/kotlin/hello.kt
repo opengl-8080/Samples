1b2e8137b98d677be9890738a99a5ea8fc728270
opengl-8080 <tomcat.port.8080+github@gmail.com>
2016-05-31 22:46:44 +0900

    Kotlin Collections mapNotNull()

--- kotlin/hello/src/main/kotlin/hello.kt
+++ kotlin/hello/src/main/kotlin/hello.kt
@@ -1,10 +1,5 @@
 fun main(args: Array<String>) {
     val iterable: Iterable<Int> = listOf(1, 2, 3, 4, 5)
-    val result = mutableListOf("Z")
 
-    iterable.mapIndexedNotNullTo(result) { index, value ->
-        if (value % 2 == 0) "$index($value)" else null
-    }
-
-    println(result)
+    println(iterable.mapNotNull { if (it % 2 == 0) it else null })
 }
