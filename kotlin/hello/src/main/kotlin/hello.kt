70d0996d9412753be8aeb8f1f9d8e2f6eaf3c17d
opengl-8080 <tomcat.port.8080+github@gmail.com>
2016-05-31 23:16:13 +0900

    Kotlin Collections max()

--- kotlin/hello/src/main/kotlin/hello.kt
+++ kotlin/hello/src/main/kotlin/hello.kt
@@ -1,5 +1,5 @@
 fun main(args: Array<String>) {
     val iterable: Iterable<Int> = listOf(1, 2, 3, 4, 5)
 
-    iterable.forEachIndexed { index, value -> println("[$index] = $value") }
+    println(iterable.max())
 }
