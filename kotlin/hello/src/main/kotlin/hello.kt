ede261382ac65eb516f52afcf7758b3c6dd7adf4
opengl-8080 <tomcat.port.8080+github@gmail.com>
2016-05-31 23:09:06 +0900

    Kotlin Collections Indexed

--- kotlin/hello/src/main/kotlin/hello.kt
+++ kotlin/hello/src/main/kotlin/hello.kt
@@ -1,8 +1,5 @@
 fun main(args: Array<String>) {
     val iterable: Iterable<Int> = listOf(1, 2, 3, 4, 5)
 
-    val result = mutableListOf("<9>")
-    iterable.mapTo(result) { "<$it>" }
-
-    println(result)
+    iterable.forEachIndexed { index, value -> println("[$index] = $value") }
 }
