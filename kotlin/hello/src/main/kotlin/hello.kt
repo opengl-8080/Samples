a88fdf5be6eab0bd2abba885da860851253c108f
opengl-8080 <tomcat.port.8080+github@gmail.com>
2016-06-01 23:38:01 +0900

    Kotlin Collections singleOrNull()

--- kotlin/hello/src/main/kotlin/hello.kt
+++ kotlin/hello/src/main/kotlin/hello.kt
@@ -1,5 +1,10 @@
 fun main(args: Array<String>) {
-    val iterable = listOf(1, 2, 3, 4)
+    val iterable1 = listOf(9)
+    val iterable2 = listOf(1, 2, 3, 4)
 
-    println(iterable.single())
+
+    println(iterable1.singleOrNull())
+
+    println(iterable2.singleOrNull {it == 4})
+    println(iterable2.singleOrNull())
 }
