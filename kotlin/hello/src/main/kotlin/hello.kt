562be36f46cfff85e0b5d03eaced2609b78872e2
opengl-8080 <tomcat.port.8080+github@gmail.com>
2016-06-01 23:49:21 +0900

    Kotlin Collections sum()

--- kotlin/hello/src/main/kotlin/hello.kt
+++ kotlin/hello/src/main/kotlin/hello.kt
@@ -1,6 +1,5 @@
 fun main(args: Array<String>) {
-    val iterable1 = listOf(1, 1, 2, 2, 3, 4, 5)
-    val iterable2 = listOf(2, 3, 4)
+    val iterable = listOf(1, 2, 3)
 
-    println(iterable1.subtract(iterable2))
+    println(iterable.sum())
 }
