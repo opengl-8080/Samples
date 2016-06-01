b646fe235da724ef45e646a990786d778c15cc4b
opengl-8080 <tomcat.port.8080+github@gmail.com>
2016-06-01 23:28:08 +0900

    Kotlin Collections reduce()

--- kotlin/hello/src/main/kotlin/hello.kt
+++ kotlin/hello/src/main/kotlin/hello.kt
@@ -1,8 +1,8 @@
 fun main(args: Array<String>) {
-    val iterable1 = listOf(1, 2, 3)
-    val iterable2 = listOf(3, 5)
+    val iterable = listOf("a", "b", "c")
 
-    println(iterable1.plus(6))
-    println(iterable1.plus(iterable2))
-    println(iterable1 + iterable2)
+    println(iterable.reduce {tmp, value ->
+        println("tmp=$tmp, value=$value")
+        tmp + ":" + value
+    })
 }
