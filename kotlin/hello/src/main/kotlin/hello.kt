5c39e77467ce93c6533b18ab8fede53828ce6272
opengl-8080 <tomcat.port.8080+github@gmail.com>
2016-06-01 23:39:03 +0900

    Kotlin Collections sorted()

--- kotlin/hello/src/main/kotlin/hello.kt
+++ kotlin/hello/src/main/kotlin/hello.kt
@@ -1,10 +1,5 @@
 fun main(args: Array<String>) {
-    val iterable1 = listOf(9)
-    val iterable2 = listOf(1, 2, 3, 4)
+    val iterable = listOf(3, 1, 2, 4)
 
-
-    println(iterable1.singleOrNull())
-
-    println(iterable2.singleOrNull {it == 4})
-    println(iterable2.singleOrNull())
+    println(iterable.sorted())
 }
