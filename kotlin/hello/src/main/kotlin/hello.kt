ee686216d501aa16de7cf67d009d955275dcbd95
opengl-8080 <tomcat.port.8080+github@gmail.com>
2016-06-01 23:43:38 +0900

    Kotlin Collections sortedWith()

--- kotlin/hello/src/main/kotlin/hello.kt
+++ kotlin/hello/src/main/kotlin/hello.kt
@@ -1,5 +1,7 @@
+import java.util.*
+
 fun main(args: Array<String>) {
     val iterable = listOf(3, 1, 2, 4)
 
-    println(iterable.sortedBy { it * -1 })
+    println(iterable.sortedWith(Comparator { left, right -> right - left }))
 }
