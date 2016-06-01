4c1d0bbee8c399b963bf06616790abd00a198c9b
opengl-8080 <tomcat.port.8080+github@gmail.com>
2016-06-01 23:21:08 +0900

    Kotlin Collections plus()

--- kotlin/hello/src/main/kotlin/hello.kt
+++ kotlin/hello/src/main/kotlin/hello.kt
@@ -1,10 +1,8 @@
 fun main(args: Array<String>) {
     val iterable1 = listOf(1, 2, 3)
-    val iterable2 = listOf<Int>();
+    val iterable2 = listOf(3, 5)
 
-    println(iterable1.none())
-    println(iterable2.none())
-
-    println(iterable1.none {it < 2})
-    println(iterable1.none {it < 1})
+    println(iterable1.plus(6))
+    println(iterable1.plus(iterable2))
+    println(iterable1 + iterable2)
 }
