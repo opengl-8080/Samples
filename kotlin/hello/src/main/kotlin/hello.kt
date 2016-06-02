5a163ce574d90b92f61c7194c0f6f7d7cfc19973
opengl-8080 <tomcat.port.8080+github@gmail.com>
2016-06-02 22:25:01 +0900

    Kotlin Collections takeWhile()

--- kotlin/hello/src/main/kotlin/hello.kt
+++ kotlin/hello/src/main/kotlin/hello.kt
@@ -1,5 +1,5 @@
 fun main(args: Array<String>) {
-    val iterable = listOf(1, 2, 3)
+    val iterable = listOf(1, 2, 3, 4, 5)
 
-    println(iterable.sumByDouble { it * 1.5 })
+    println(iterable.takeWhile { it < 5 })
 }
