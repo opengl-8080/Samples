c84085922aef3eff5a0ee9d53cfe1d256c003447
opengl-8080 <tomcat.port.8080+github@gmail.com>
2016-06-02 22:52:45 +0900

    Kotlin Collection union()

--- kotlin/hello/src/main/kotlin/hello.kt
+++ kotlin/hello/src/main/kotlin/hello.kt
@@ -1,9 +1,8 @@
-import java.util.*
-
 fun main(args: Array<String>) {
-    val iterable = listOf(3, 1, 2, 3, 1, 4)
+    val iterable1 = listOf(1, 2, 3, 4)
+    val iterable2 = listOf(2, 4, 5, 6)
 
-    val result: SortedSet<Int> = iterable.toSortedSet()
+    val result: Set<Int> = iterable1.union(iterable2)
 
     println(result)
 }
