9d1c11438e6fd0ad768a52bda1a0bf3ff6699855
opengl-8080 <tomcat.port.8080+github@gmail.com>
2016-06-02 22:45:34 +0900

    Kotlin Collection toSet()

--- kotlin/hello/src/main/kotlin/hello.kt
+++ kotlin/hello/src/main/kotlin/hello.kt
@@ -1,7 +1,7 @@
 fun main(args: Array<String>) {
     val iterable = listOf(1, 1, 2, 3, 3)
 
-    val result: MutableSet<Int> = iterable.toMutableSet()
+    val result: Set<Int> = iterable.toSet()
 
     println(result)
 }
