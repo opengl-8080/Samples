a8d7ff3ed0a779bd3bcd84bc4484a8774636ea46
opengl-8080 <tomcat.port.8080+github@gmail.com>
2016-06-03 23:20:53 +0900

    Kotlin Collections isEmpty()

--- kotlin/hello/src/main/kotlin/hello.kt
+++ kotlin/hello/src/main/kotlin/hello.kt
@@ -1,8 +1,7 @@
 fun main(args: Array<String>) {
     val iterable1 = listOf(1, 2, 3, 4)
-    val iterable2 = listOf(2, 3)
-    val iterable3 = listOf(4, 5)
+    val iterable2 = listOf<Int>()
 
-    println(iterable1.containsAll(iterable2))
-    println(iterable1.containsAll(iterable3))
+    println(iterable1.isEmpty())
+    println(iterable2.isEmpty())
 }
