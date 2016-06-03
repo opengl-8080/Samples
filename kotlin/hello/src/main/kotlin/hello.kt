21ebae09096e9c3754f70198f5a209ab382b28c9
opengl-8080 <tomcat.port.8080+github@gmail.com>
2016-06-04 08:55:33 +0900

    Kotlin Collections Map flatMap()

--- kotlin/hello/src/main/kotlin/hello.kt
+++ kotlin/hello/src/main/kotlin/hello.kt
@@ -1,5 +1,5 @@
 fun main(args: Array<String>) {
     val map = mapOf("one" to "ONE", "two" to "TWO", "three" to "THREE")
 
-    println(map.filterKeys { it.contains("o") })
+    println(map.flatMap { listOf(it.key, it.value) })
 }
