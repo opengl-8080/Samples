0a99b5335113f0f6a8bcc40f99237a9474edd67e
opengl-8080 <tomcat.port.8080+github@gmail.com>
2016-06-04 07:21:10 +0900

    Kotlin Collections binarySearch()

--- kotlin/hello/src/main/kotlin/hello.kt
+++ kotlin/hello/src/main/kotlin/hello.kt
@@ -1,5 +1,10 @@
+import java.util.*
+
 fun main(args: Array<String>) {
-    val list = mutableListOf(1, 2, 3)
+    val list = listOf(1, 2, 2, 3, 3, 3)
 
     println(list.binarySearch(3))
+    println(list.binarySearch(9))
+    println(list.binarySearch(element = 3, fromIndex = 0, toIndex = 2))
+    println(list.binarySearch(element = 3, comparator = Comparator { a, b -> a - b }))
 }
