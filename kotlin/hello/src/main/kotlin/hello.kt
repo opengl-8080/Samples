93c07352c262e072e56dc47161b8a95408c55467
opengl-8080 <tomcat.port.8080+github@gmail.com>
2016-06-04 08:08:55 +0900

    Kotlin Collections retainAll()

--- kotlin/hello/src/main/kotlin/hello.kt
+++ kotlin/hello/src/main/kotlin/hello.kt
@@ -1,7 +1,7 @@
 fun main(args: Array<String>) {
     val mutableIterable = mutableListOf(1, 2, 3, 1, 2, 3)
 
-    mutableIterable.removeAll { it == 3 }
+    mutableIterable.retainAll { it == 3 }
 
     println(mutableIterable)
 }
