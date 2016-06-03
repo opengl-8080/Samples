d6f434e61cb465360740e5a06f83e6b65583a6f8
opengl-8080 <tomcat.port.8080+github@gmail.com>
2016-06-03 23:19:39 +0900

    Kotlin Collections containsAll()

--- kotlin/hello/src/main/kotlin/hello.kt
+++ kotlin/hello/src/main/kotlin/hello.kt
@@ -1,11 +1,8 @@
 fun main(args: Array<String>) {
-    val iterable1 = listOf(1, 2, 3)
+    val iterable1 = listOf(1, 2, 3, 4)
+    val iterable2 = listOf(2, 3)
+    val iterable3 = listOf(4, 5)
 
-    val iterable2 = listOf("one", "two", "three", "four")
-    val result1: List<String> = iterable1.zip(iterable2, {a, b -> "$b($a)"})
-    println(result1)
-
-    val array = arrayOf(1.1, 2.2)
-    val result2: List<Int> = iterable1.zip(array, {a, b -> (a + b*10).toInt()})
-    println(result2)
+    println(iterable1.containsAll(iterable2))
+    println(iterable1.containsAll(iterable3))
 }
