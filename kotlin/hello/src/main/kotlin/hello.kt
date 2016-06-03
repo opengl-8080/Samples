ca3792319e416b275835a2af514d2cdc785d0e6c
opengl-8080 <tomcat.port.8080+github@gmail.com>
2016-06-04 08:56:50 +0900

    Kotlin Collections Map forEach()

--- kotlin/hello/src/main/kotlin/hello.kt
+++ kotlin/hello/src/main/kotlin/hello.kt
@@ -1,5 +1,5 @@
 fun main(args: Array<String>) {
     val map = mapOf("one" to "ONE", "two" to "TWO", "three" to "THREE")
 
-    println(map.flatMap { listOf(it.key, it.value) })
+    map.forEach { println("key=${it.key}, value=${it.value}") }
 }
