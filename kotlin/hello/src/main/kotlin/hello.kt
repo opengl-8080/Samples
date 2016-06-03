6929ee509026c222e209e177df642cdbf2127b8f
opengl-8080 <tomcat.port.8080+github@gmail.com>
2016-06-04 08:20:08 +0900

    Kotlin Collections remove()

--- kotlin/hello/src/main/kotlin/hello.kt
+++ kotlin/hello/src/main/kotlin/hello.kt
@@ -1,10 +1,7 @@
 fun main(args: Array<String>) {
     val mutableCollection = mutableListOf(1, 2, 3)
-    val iterable = listOf(7, 8)
 
-    mutableCollection.plusAssign(iterable)
-    println(mutableCollection)
+    mutableCollection.remove(2)
 
-    mutableCollection += 9
     println(mutableCollection)
 }
