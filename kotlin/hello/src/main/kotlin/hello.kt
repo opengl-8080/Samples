e4d3099711d38044cabd3c1b82246e8b52ddb50e
opengl-8080 <tomcat.port.8080+github@gmail.com>
2016-06-04 08:50:19 +0900

    Kotlin Collections Map filterKeys()

--- kotlin/hello/src/main/kotlin/hello.kt
+++ kotlin/hello/src/main/kotlin/hello.kt
@@ -1,5 +1,5 @@
 fun main(args: Array<String>) {
     val map = mapOf("one" to "ONE", "two" to "TWO", "three" to "THREE")
 
-    println(map.filter { it.key.contains("t") })
+    println(map.filterKeys { it.contains("o") })
 }
