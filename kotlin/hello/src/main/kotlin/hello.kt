478d4ef014949b389d0fb5d049c96f7100eeccbe
opengl-8080 <tomcat.port.8080+github@gmail.com>
2016-06-04 08:02:32 +0900

    Kotlin Collections takeLast()

--- kotlin/hello/src/main/kotlin/hello.kt
+++ kotlin/hello/src/main/kotlin/hello.kt
@@ -1,7 +1,5 @@
 fun main(args: Array<String>) {
-    val list = listOf("0:one", "1:two", "2:three", "3:four", "4:five")
-    println(list.slice(2..4))
+    val list = listOf(1, 2, 3, 4, 5)
 
-    val indices = listOf(1, 3, 4)
-    println(list.slice(indices))
+    println(list.takeLast(2))
 }
