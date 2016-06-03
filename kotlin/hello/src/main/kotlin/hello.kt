61e08981f4b003cbd438800d6d0ac53b6288031b
opengl-8080 <tomcat.port.8080+github@gmail.com>
2016-06-03 23:46:30 +0900

    Kotlin Collections binarySearch()

--- kotlin/hello/src/main/kotlin/hello.kt
+++ kotlin/hello/src/main/kotlin/hello.kt
@@ -1,16 +1,5 @@
 fun main(args: Array<String>) {
     val list = mutableListOf(1, 2, 3)
 
-    println("[before] list = $list")
-
-    val asReversed = list.asReversed()
-
-    println("[after asReversed] list = $list")
-    println("[after asReversed] asReversed = $asReversed")
-
-    list += 9
-    asReversed += 99
-
-    println("[after modify] list = $list")
-    println("[after modify] asReversed = $asReversed")
+    println(list.binarySearch(3))
 }
