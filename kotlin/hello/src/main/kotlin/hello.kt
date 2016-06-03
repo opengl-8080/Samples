47e461b45f55db52b8682705e6a13d23f93fc3f3
opengl-8080 <tomcat.port.8080+github@gmail.com>
2016-06-04 08:23:57 +0900

    Kotlin Collections sort()

--- kotlin/hello/src/main/kotlin/hello.kt
+++ kotlin/hello/src/main/kotlin/hello.kt
@@ -1,7 +1,7 @@
 fun main(args: Array<String>) {
-    val mutableList = mutableListOf(1, 2, 3)
+    val mutableList = mutableListOf(3, 1, 2)
 
-    mutableList.reverse()
+    mutableList.sort()
 
     println(mutableList)
 }
