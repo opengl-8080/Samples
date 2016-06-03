9faa71fb825ebaefabe8e71c660e764811326d38
opengl-8080 <tomcat.port.8080+github@gmail.com>
2016-06-04 08:47:51 +0900

    Kotlin Collections Map count()

--- kotlin/hello/src/main/kotlin/hello.kt
+++ kotlin/hello/src/main/kotlin/hello.kt
@@ -1,6 +1,6 @@
 fun main(args: Array<String>) {
     val map = mapOf("one" to "ONE", "two" to "TWO", "three" to "THREE")
 
-    println(map.containsValue("one"))
-    println(map.containsValue("ONE"))
+    println(map.count())
+    println(map.count { it.key.contains("t") })
 }
