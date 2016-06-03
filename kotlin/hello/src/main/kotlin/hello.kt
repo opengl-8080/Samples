2a98c09fe30c57460d6b1ceb43c0b004ea065d49
opengl-8080 <tomcat.port.8080+github@gmail.com>
2016-06-04 08:46:34 +0900

    Kotlin Collections Map containsValue()

--- kotlin/hello/src/main/kotlin/hello.kt
+++ kotlin/hello/src/main/kotlin/hello.kt
@@ -1,6 +1,6 @@
 fun main(args: Array<String>) {
     val map = mapOf("one" to "ONE", "two" to "TWO", "three" to "THREE")
 
-    println(map.any { it.key.contains("o") })
-    println(map.any { it.key.isEmpty() })
+    println(map.containsValue("one"))
+    println(map.containsValue("ONE"))
 }
