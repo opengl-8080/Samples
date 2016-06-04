d5f9c5f5e75d799d5502fd06c103ee82eeff4140
opengl-8080 <tomcat.port.8080+github@gmail.com>
2016-06-04 09:29:02 +0900

    Kotlin Collections Map mapNotNull ()

--- kotlin/hello/src/main/kotlin/hello.kt
+++ kotlin/hello/src/main/kotlin/hello.kt
@@ -1,5 +1,6 @@
 fun main(args: Array<String>) {
     val map = mapOf("one" to "ONE", "two" to "TWO", "three" to "THREE")
 
-    println(map.mapKeys { "<${it.key}>" })
+    println(map.map { if (it.key == "two") null else "${it.key}=${it.value}" })
+    println(map.mapNotNull { if (it.key == "two") null else "${it.key}=${it.value}" })
 }
