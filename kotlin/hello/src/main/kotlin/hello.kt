306cf9d70f5cd47668605240fc12a2ac66126428
opengl-8080 <tomcat.port.8080+github@gmail.com>
2016-06-04 09:26:30 +0900

    Kotlin Collections Map mapKeys()

--- kotlin/hello/src/main/kotlin/hello.kt
+++ kotlin/hello/src/main/kotlin/hello.kt
@@ -1,5 +1,5 @@
 fun main(args: Array<String>) {
     val map = mapOf("one" to "ONE", "two" to "TWO", "three" to "THREE")
 
-    println(map.map { "${it.key}:${it.value}" })
+    println(map.mapKeys { "<${it.key}>" })
 }
