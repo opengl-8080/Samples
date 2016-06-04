e7c9b2ccb44d727f7c14b24bc50ddad1042311e4
opengl-8080 <tomcat.port.8080+github@gmail.com>
2016-06-04 09:37:55 +0900

    Kotlin Collections Map maxWith() minWith()

--- kotlin/hello/src/main/kotlin/hello.kt
+++ kotlin/hello/src/main/kotlin/hello.kt
@@ -1,6 +1,10 @@
+import java.util.*
+
 fun main(args: Array<String>) {
     val map = mapOf("one" to "ONE", "two" to "TWO", "three" to "THREE")
 
-    println(map.maxBy { it.key })
-    println(map.minBy { it.key })
+    val comparator = Comparator<Map.Entry<String, String>> { a, b -> a.key.compareTo(b.key) }
+
+    println(map.maxWith(comparator))
+    println(map.minWith(comparator))
 }
