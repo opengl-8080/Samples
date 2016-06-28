8a1067147c4ea41baaf09a7506e831076caf733c
opengl-8080 <tomcat.port.8080+github@gmail.com>
2016-06-28 23:18:23 +0900

    jline accept

--- java/jline/src/main/java/sample/jline/Main.java
+++ java/jline/src/main/java/sample/jline/Main.java
@@ -2,19 +2,13 @@ package sample.jline;
 
 import jline.console.ConsoleReader;
 
-import java.io.IOException;
-
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-
+    public static void main(String[] args) throws Exception {
         ConsoleReader console = new ConsoleReader();
 
-        console.println("hoge");
-        console.print("fuga");
-        console.println("piyo");
-        console.flush();
-
-        console.println("foo");
+        System.out.println("1");
+        console.accept();
+        System.out.println("2");
     }
 }
