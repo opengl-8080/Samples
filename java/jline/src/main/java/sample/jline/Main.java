ed689968a0592afb78353dc906859c380c572a07
opengl-8080 <tomcat.port.8080+github@gmail.com>
2016-07-02 17:34:16 +0900

    jline killLine

--- java/jline/src/main/java/sample/jline/Main.java
+++ java/jline/src/main/java/sample/jline/Main.java
@@ -6,7 +6,11 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         ConsoleReader console = new ConsoleReader();
-        console.clearScreen();
+        console.putString("12345");
+        console.flush();
+
+        console.moveCursor(-3);
+        console.killLine();
         console.flush();
     }
 }
