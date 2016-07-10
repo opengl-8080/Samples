edbfe0fb123d8cd9ec3a3ad52f0c161da6476ded
opengl-8080 <tomcat.port.8080+github@gmail.com>
2016-07-10 21:35:52 +0900

    checkstyle ArrayTypeStyle

--- java/checkstyle/src/main/java/sample/checkstyle/Main.java
+++ java/checkstyle/src/main/java/sample/checkstyle/Main.java
@@ -2,17 +2,6 @@ package sample.checkstyle;
 
 public class Main {
 
-    int[] a = {1, 2, 3};
-
-    int[] b = {
-        1,
-        2,
-        3
-    };
-
-    int[] c = {
-        1,
-        2,
-        3,
-    };
+    int[] a;
+    int b[];
 }
