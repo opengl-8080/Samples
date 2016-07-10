660453181ea13ad2cf78b438428b8aad1e15bcef
opengl-8080 <tomcat.port.8080+github@gmail.com>
2016-07-10 21:33:17 +0900

    checkstyle ArrayTrailingComma

--- java/checkstyle/src/main/java/sample/checkstyle/Main.java
+++ java/checkstyle/src/main/java/sample/checkstyle/Main.java
@@ -2,27 +2,17 @@ package sample.checkstyle;
 
 public class Main {
 
-    public static void main(String[] args) {
-        new Object() {//1
-            //2
-            //3
-            //4
-            //5
-            //6
-            //7
-            //8
-            //9
-            //10
-            //11
-            //12
-            //13
-            //14
-            //15
-            //16
-            //17
-            //18
-            //19
-            //20
-        };//21
-    }
+    int[] a = {1, 2, 3};
+
+    int[] b = {
+        1,
+        2,
+        3
+    };
+
+    int[] c = {
+        1,
+        2,
+        3,
+    };
 }
