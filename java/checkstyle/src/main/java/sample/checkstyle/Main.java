40005e538a9b83aa135d49c0ccab6d00e7694232
opengl-8080 <tomcat.port.8080+github@gmail.com>
2016-07-10 20:42:03 +0900

    checkstyle AnnotationLocation

--- java/checkstyle/src/main/java/sample/checkstyle/Main.java
+++ java/checkstyle/src/main/java/sample/checkstyle/Main.java
@@ -1,9 +1,17 @@
 package sample.checkstyle;
 
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+
 public class Main {
 
-    public static class AbstractHoge {}
-    public static abstract class AbstractFuga {}
-    public static abstract class Piyo {}
+    @XmlElement
+    @XmlAttribute
+    int a;
 
+    @XmlElement int b;
+
+    @SuppressWarnings("xxx") int c;
+
+    @XmlElement @XmlAttribute int d;
 }
