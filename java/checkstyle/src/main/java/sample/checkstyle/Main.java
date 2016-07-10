16d941ed5d21f4a3a5ff73efcc1ed4cc0aeebead
opengl-8080 <tomcat.port.8080+github@gmail.com>
2016-07-10 21:20:24 +0900

    checkstyle AnnotationUseStyle

--- java/checkstyle/src/main/java/sample/checkstyle/Main.java
+++ java/checkstyle/src/main/java/sample/checkstyle/Main.java
@@ -1,17 +1,27 @@
 package sample.checkstyle;
 
-import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
 
 public class Main {
 
     @XmlElement
-    @XmlAttribute
     int a;
 
-    @XmlElement int b;
+    @XmlElement() // アノテーションは閉じ括弧を持つことはできません。
+    int b;
 
-    @SuppressWarnings("xxx") int c;
+    @SuppressWarnings("xxx")
+    int c;
 
-    @XmlElement @XmlAttribute int d;
+    @SuppressWarnings(value="xxx")
+    int d;
+
+    @SuppressWarnings({"xxx"}) // アノテーションの書式は 'COMPACT_NO_ARRAY' でなければなりません 。
+    int e;
+
+    @SuppressWarnings({"xxx", "yyy"})
+    int f;
+
+    @SuppressWarnings({"xxx", "yyy", }) // アノテーションの配列値の末尾をカンマにすることはできません。
+    int g;
 }
