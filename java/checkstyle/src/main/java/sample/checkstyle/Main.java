deee6bbb967dbe555b80c969e70c9be438f9ee1c
opengl-8080 <tomcat.port.8080+github@gmail.com>
2016-07-17 00:19:02 +0900

    checkstyle SuppressWithNearbyCommentFilter

--- java/checkstyle/src/main/java/sample/checkstyle/Main.java
+++ java/checkstyle/src/main/java/sample/checkstyle/Main.java
@@ -1,40 +1,10 @@
 package sample.checkstyle;
 
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.parser.Parser;
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStream;
-import java.util.List;
-
-import static java.util.stream.Collectors.*;
-
 public class Main {
-
-    public static void main(String[] args) throws Exception {
-        try (InputStream in = new FileInputStream(new File("./google_checks.xml"))) {
-            Document document = Jsoup.parse(in, "UTF-8", ".", Parser.xmlParser());
-
-            document.select("module").forEach(module -> {
-                String moduleName = module.attr("name");
-
-
-                List<String> list =
-                module.select("> property")
-                        .stream()
-                        .map(p -> moduleName + "\t" + p.attr("name") + "\t" + p.attr("value"))
-                        .collect(toList());
-
-                if (list.isEmpty()) {
-                    System.out.println(moduleName);
-                } else {
-                    list.forEach(System.out::println);
-                }
-            });
-        }
-
-
-    }
+    // CHECKSTYLE:OFF ConstantName
+    int Aaa;
+    static final int aaa = 1;
+    // CHECKSTYLE:ON ConstantName
+    int Bbb;
+    static final int bbb = 1;
 }
