bdfa2994c9ab5b8f4d1cb42f18d7f47d349b0074
opengl-8080 <tomcat.port.8080+github@gmail.com>
2016-08-08 23:33:03 +0900

    FindBugs html report

--- java/findbugs/src/main/java/sample/findbugs/Main.java
+++ java/findbugs/src/main/java/sample/findbugs/Main.java
@@ -1,22 +1,9 @@
 package sample.findbugs;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        method();
-    }
-
-    public static void method() {
-        InputStream in = null;
-        try {
-            in = new FileInputStream(new File("a"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        String text = null;
+        System.out.println("text.length=" + text.length());
     }
 }
