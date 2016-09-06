61832cf440399645207e0e0ede9c5bc9b4052400
opengl-8080 <tomcat.port.8080+github@gmail.com>
2016-09-07 00:11:56 +0900

    check jsf viewparam

--- java/jsf/src/main/java/sample/jsf/SampleBean.java
+++ java/jsf/src/main/java/sample/jsf/SampleBean.java
@@ -4,41 +4,20 @@ import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 import java.io.Serializable;
 
-@Named("sampleBean")
+@Named
 @ViewScoped
 public class SampleBean implements Serializable {
     private int id;
-    private boolean flag;
 
-    public void init() {
-        this.flag = (this.id % 2 == 0);
+    public int getId() {
+        return id;
     }
 
     public void setId(int id) {
         this.id = id;
     }
 
-    public int getId() {
-        return id;
-    }
-
-    public String gusu() {
-        return "next.xhtml";
-    }
-
-    public String kisu() {
-        return "next.xhtml";
-    }
-
-    @Override
-    public String toString() {
-        return "SampleBean{" +
-                "id=" + id +
-                ", flag=" + flag +
-                '}';
-    }
-
-    public boolean isFlag() {
-        return flag;
+    public void init() {
+        System.out.println("id=" + id);
     }
 }
