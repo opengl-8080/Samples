48e54fe806021fb4347de4d64ff76d23d51ec73e
opengl-8080 <tomcat.port.8080+github@gmail.com>
2016-10-18 22:30:22 +0900

    jxls

--- java/jxls/src/main/java/sample/jxls/Main.java
+++ java/jxls/src/main/java/sample/jxls/Main.java
@@ -1,14 +1,13 @@
 package sample.jxls;
 
+import org.jxls.common.Context;
+import org.jxls.util.JxlsHelper;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
-
-import org.jxls.builder.xls.XlsCommentAreaBuilder;
-import org.jxls.common.Context;
-import org.jxls.util.JxlsHelper;
 
 public class Main {
 
@@ -16,11 +15,9 @@ public class Main {
         try (InputStream in = new FileInputStream(new File("./excel/template.xlsx"));
              OutputStream out = new FileOutputStream(new File("./excel/out.xlsx"))) {
             
-            XlsCommentAreaBuilder.addCommandMapping("myCommand", MyCommand.class);
             Context context = new Context();
+
             JxlsHelper.getInstance().processTemplate(in, out, context);
         }
-        
-        System.out.println("end");
     }
 }
