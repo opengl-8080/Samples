a95c8a93736b219cb0fcfc4ac03f250e807e64ee
opengl-8080 <tomcat.port.8080+github@gmail.com>
2017-02-01 00:11:17 +0900

    Spring Security @PreAuthorize が動いた模様

--- java/spring-security/src/main/java/sample/spring/mvc/HelloController.java
+++ java/spring-security/src/main/java/sample/spring/mvc/HelloController.java
@@ -1,14 +1,30 @@
 package sample.spring.mvc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import sample.spring.service.HelloService;
 
 @Controller
 public class HelloController {
     
+    private HelloService service;
+
+    @Autowired
+    public HelloController(HelloService service) {
+        this.service = service;
+    }
+
     @GetMapping("/hello")
-    public String hello() {
-        System.out.println("Hello World!!");
+    public String hello(@RequestParam("p") String p) {
+        System.out.println("p=" + p);
+        if ("user".equals(p)) {
+            this.service.user();
+        } else {
+            this.service.admin();
+        }
+        
         return "hello";
     }
 }
