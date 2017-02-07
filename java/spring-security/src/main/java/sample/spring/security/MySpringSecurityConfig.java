1c4b9e89affa340aa20cb652aec3f295e66230ba
opengl-8080 <tomcat.port.8080+github@gmail.com>
2017-02-07 22:38:32 +0900

    Spring Security の JavaConfig の初期化フロー解読

--- java/spring-security/src/main/java/sample/spring/security/MySpringSecurityConfig.java
+++ java/spring-security/src/main/java/sample/spring/security/MySpringSecurityConfig.java
@@ -1,21 +1,41 @@
 package sample.spring.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
-public class MySpringSecurityConfig extends WebSecurityConfigurerAdapter {
+public class MySpringSecurityConfig {
 
-    @Override
-    public void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/login").permitAll()
-                .anyRequest().authenticated()
-            .and()
-                .formLogin();
+    @Configuration
+    @Order(1)
+    public static class ForRest extends WebSecurityConfigurerAdapter {
+
+        @Override
+        public void configure(HttpSecurity http) throws Exception {
+            http.antMatcher("/api/**").authorizeRequests()
+                    .antMatchers("/login").permitAll()
+                    .anyRequest().authenticated()
+                    .and()
+                    .formLogin();
+        }
+    }
+    
+    @Configuration
+    public static class ForWeb extends WebSecurityConfigurerAdapter {
+
+        @Override
+        public void configure(HttpSecurity http) throws Exception {
+            http.authorizeRequests()
+                    .antMatchers("/login").permitAll()
+                    .anyRequest().authenticated()
+                    .and()
+                    .formLogin();
+        }
     }
 
     @Autowired
