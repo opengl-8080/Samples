11ddf0d670dad7a26044ee10cc51cec221241a35
opengl-8080 <tomcat.port.8080+github@gmail.com>
2017-02-09 23:18:21 +0900

    Spring Security Servlet3.0 Initializer

---
+++ java/spring-security/src/main/java/sample/spring/security/MySpringSecurityInitializer.java
@@ -0,0 +1,9 @@
+package sample.spring.security;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class MySpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+    public MySpringSecurityInitializer() {
+        super(MySpringSecurityConfig.class);
+    }
+}
