3a603ad0a2564913436a73f2d20f529aed00872b
opengl-8080 <tomcat.port.8080+github@gmail.com>
2017-02-19 12:14:43 +0900

    Spring Security namespace と java-config を分離して検証しやすくした

---
+++ java/spring-security/java-config/src/main/java/sample/spring/security/MySpringSecurityConfig.java
@@ -0,0 +1,26 @@
+package sample.spring.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class MySpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/login").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin();
+    }
+
+    @Autowired
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("hoge").password("HOGE").roles("USER");
+    }
+}
--- java/spring-security/namespace/src/main/webapp/index.jsp
+++ java/spring-security/namespace/src/main/webapp/index.jsp
@@ -1,17 +1,18 @@
 <%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
 <!doctype html>
 <html>
     <head>
         <meta charset="utf-8" />
-        <title>/secure/logout.jsp</title>
+        <title>Hello Spring Security!!</title>
     </head>
     <body>
-        <h1>/secure/logout.jsp</h1>
+        <h1>Hello Spring Security!!</h1>
         
         <c:url var="logoutUrl" value="/logout" />
         <form action="${logoutUrl}" method="post">
+            <input type="submit" value="logout" />
             <input type="hidden" name="${_csrf.parameterName}" value="${_csrf.token}" />
-            <input type="submit" value="Logout" />
         </form>
     </body>
 </html>
