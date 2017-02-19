92bdbc47d17f603a42f6b8ceb5f68fe71c626be2
opengl-8080 <tomcat.port.8080+github@gmail.com>
2017-02-19 17:57:07 +0900

    Spring Security UserDetailsService を自作

--- java/spring-security/common/src/main/java/sample/spring/security/MyUserDetailsService.java
+++ java/spring-security/common/src/main/java/sample/spring/security/MyUserDetailsService.java
@@ -1,12 +1,21 @@
 package sample.spring.security;
 
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
 
+@Component
 public class MyUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        System.out.println("username = " + username);
+        if ("hoge".equals(username)) {
+            return new User(username, "HOGE", AuthorityUtils.createAuthorityList("USER"));
+        }
+        
+        throw new UsernameNotFoundException("not found : " + username);
     }
 }
--- java/spring-security/java-config/src/main/java/sample/spring/security/MySpringSecurityConfig.java
+++ java/spring-security/java-config/src/main/java/sample/spring/security/MySpringSecurityConfig.java
@@ -1,17 +1,15 @@
 package sample.spring.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-import javax.sql.DataSource;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 @EnableWebSecurity
+@ComponentScan
 public class MySpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
@@ -22,21 +20,9 @@ public class MySpringSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .formLogin();
     }
-    
-    @Bean
-    public DataSource dataSource() {
-        return new EmbeddedDatabaseBuilder()
-                .generateUniqueName(true)
-                .setType(EmbeddedDatabaseType.H2)
-                .setScriptEncoding("UTF-8")
-                .addScript("/sql/create_database.sql")
-                .build();
-    }
 
     @Autowired
-    public void configure(AuthenticationManagerBuilder auth, DataSource dataSource) throws Exception {
-        auth.jdbcAuthentication().dataSource(dataSource)
-        .usersByUsernameQuery("SELECT LOGIN_ID, PASSWORD, ENABLED FROM USERS WHERE LOGIN_ID=?")
-        .authoritiesByUsernameQuery("SELECT LOGIN_ID, ROLE FROM AUTHORITIES WHERE LOGIN_ID=?");
+    public void configure(AuthenticationManagerBuilder auth, UserDetailsService userDetailsService) throws Exception {
+        auth.userDetailsService(userDetailsService);
     }
 }
