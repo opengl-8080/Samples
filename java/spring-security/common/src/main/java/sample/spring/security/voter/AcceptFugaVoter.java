35926d06decc55dbfe38ed8a29db49eda5947ca5
opengl-8080 <tomcat.port.8080+github@gmail.com>
2017-03-07 22:47:19 +0900

    Spring Security Voter のカスタマイズを検証

---
+++ java/spring-security/common/src/main/java/sample/spring/security/voter/AcceptFugaVoter.java
@@ -0,0 +1,37 @@
+package sample.spring.security.voter;
+
+import org.springframework.security.access.AccessDecisionVoter;
+import org.springframework.security.access.ConfigAttribute;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+public class AcceptFugaVoter implements AccessDecisionVoter<Object> {
+    @Override
+    public boolean supports(ConfigAttribute attribute) {
+        return "acceptFuga".equals(attribute.getAttribute());
+    }
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return true;
+    }
+
+    @Override
+    public int vote(Authentication authentication, Object object, Collection<ConfigAttribute> attributes) {
+        Object principal = authentication.getPrincipal();
+        if (!(principal instanceof UserDetails)) {
+            return ACCESS_ABSTAIN;
+        }
+        
+        for (ConfigAttribute attribute : attributes) {
+            if (this.supports(attribute)) {
+                String username = ((UserDetails)principal).getUsername();
+                return "fuga".equals(username) ? ACCESS_GRANTED : ACCESS_DENIED;
+            }
+        }
+        
+        return ACCESS_ABSTAIN;
+    }
+}
