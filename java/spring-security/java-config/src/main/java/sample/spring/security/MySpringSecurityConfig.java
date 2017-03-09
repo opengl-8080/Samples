213d8bce11f75897e09768812676f7668ac0d72e
opengl-8080 <tomcat.port.8080+github@gmail.com>
2017-03-09 22:36:32 +0900

    Spring Security JavaConfig で自作 Voter 差し替え

--- java/spring-security/java-config/src/main/java/sample/spring/security/MySpringSecurityConfig.java
+++ java/spring-security/java-config/src/main/java/sample/spring/security/MySpringSecurityConfig.java
@@ -2,11 +2,15 @@ package sample.spring.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.access.vote.AffirmativeBased;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.access.expression.WebExpressionVoter;
+import sample.spring.security.voter.AcceptFugaVoter;
 
+import java.util.Arrays;
 import java.util.Collections;
 
 @EnableWebSecurity
@@ -15,9 +19,14 @@ public class MySpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
+        AcceptFugaVoter acceptFugaVoter = new AcceptFugaVoter();
+        WebExpressionVoter webExpressionVoter = new WebExpressionVoter();
+        AffirmativeBased accessDecisionManager = new AffirmativeBased(Arrays.asList(acceptFugaVoter, webExpressionVoter));
+
         http.authorizeRequests()
+                .accessDecisionManager(accessDecisionManager)
                 .antMatchers("/login").permitAll()
-                .anyRequest().access("isAuthenticated()")
+                .anyRequest().hasRole("USER")
                 .and()
                 .formLogin()
                 .and()
@@ -29,6 +38,14 @@ public class MySpringSecurityConfig extends WebSecurityConfigurerAdapter {
         auth.inMemoryAuthentication()
             .withUser("hoge")
             .password("hoge")
-            .authorities(Collections.emptyList());
+            .roles("USER")
+        .and()
+            .withUser("fuga")
+            .password("fuga")
+            .roles("FUGA")
+        .and()
+            .withUser("piyo")
+            .password("piyo")
+            .roles("PIYO");
     }
 }
