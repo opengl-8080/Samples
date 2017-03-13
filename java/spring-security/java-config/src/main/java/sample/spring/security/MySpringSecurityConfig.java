c5c83fa57cb1a32915177b8629c288ae115beb28
opengl-8080 <tomcat.port.8080+github@gmail.com>
2017-03-13 22:29:46 +0900

    Spring Security RoleHierarchy を Java Config で

--- java/spring-security/java-config/src/main/java/sample/spring/security/MySpringSecurityConfig.java
+++ java/spring-security/java-config/src/main/java/sample/spring/security/MySpringSecurityConfig.java
@@ -2,16 +2,14 @@ package sample.spring.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.security.access.vote.AffirmativeBased;
+import org.springframework.security.access.expression.SecurityExpressionHandler;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.access.expression.WebExpressionVoter;
-import sample.spring.security.voter.AcceptFugaVoter;
-
-import java.util.Arrays;
-import java.util.Collections;
+import org.springframework.security.web.FilterInvocation;
+import org.springframework.security.web.access.expression.DefaultWebSecurityExpressionHandler;
 
 @EnableWebSecurity
 @ComponentScan
@@ -19,33 +17,37 @@ public class MySpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
-        AcceptFugaVoter acceptFugaVoter = new AcceptFugaVoter();
-        WebExpressionVoter webExpressionVoter = new WebExpressionVoter();
-        AffirmativeBased accessDecisionManager = new AffirmativeBased(Arrays.asList(acceptFugaVoter, webExpressionVoter));
-
         http.authorizeRequests()
-                .accessDecisionManager(accessDecisionManager)
+                .expressionHandler(this.createSecurityExpressionHandler())
                 .antMatchers("/login").permitAll()
-                .anyRequest().hasRole("USER")
+                .antMatchers("/hierarchy/user").hasRole("USER")
+                .antMatchers("/hierarchy/admin").hasRole("ADMIN")
+                .anyRequest().authenticated()
                 .and()
                 .formLogin()
                 .and()
                 .rememberMe();
     }
     
+    private SecurityExpressionHandler<FilterInvocation> createSecurityExpressionHandler() {
+        RoleHierarchyImpl roleHierarchy = new RoleHierarchyImpl();
+        roleHierarchy.setHierarchy("ROLE_ADMIN > ROLE_USER");
+
+        DefaultWebSecurityExpressionHandler expressionHandler = new DefaultWebSecurityExpressionHandler();
+        expressionHandler.setRoleHierarchy(roleHierarchy);
+        
+        return expressionHandler;
+    }
+    
     @Autowired
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
-            .withUser("hoge")
-            .password("hoge")
+            .withUser("user")
+            .password("user")
             .roles("USER")
         .and()
-            .withUser("fuga")
-            .password("fuga")
-            .roles("FUGA")
-        .and()
-            .withUser("piyo")
-            .password("piyo")
-            .roles("PIYO");
+            .withUser("admin")
+            .password("admin")
+            .roles("ADMIN");
     }
 }
