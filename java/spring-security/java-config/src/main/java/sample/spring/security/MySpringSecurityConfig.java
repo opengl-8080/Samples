31fabb9df6fe4f34aef674dcb1170af3b348b869
opengl-8080 <tomcat.port.8080+github@gmail.com>
2017-04-01 23:04:10 +0900

    Spring Security JavaConfig で RememberMe の Secure Cookie

--- java/spring-security/java-config/src/main/java/sample/spring/security/MySpringSecurityConfig.java
+++ java/spring-security/java-config/src/main/java/sample/spring/security/MySpringSecurityConfig.java
@@ -3,14 +3,12 @@ package sample.spring.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 import java.util.Collections;
 
-@EnableGlobalMethodSecurity(prePostEnabled=true)
 @EnableWebSecurity
 @ComponentScan
 public class MySpringSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -19,9 +17,12 @@ public class MySpringSecurityConfig extends WebSecurityConfigurerAdapter {
     public void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/login").permitAll()
-                .anyRequest().access("@myExpression.check(authentication)")
+                .anyRequest().authenticated()
                 .and()
-                .formLogin();
+                .formLogin()
+                .and()
+                .rememberMe()
+                .useSecureCookie(true);
     }
     
     @Autowired
