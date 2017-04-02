77fe0e95fd158062a81bfcd27f030660015ad441
opengl-8080 <tomcat.port.8080+github@gmail.com>
2017-04-02 18:22:31 +0900

    Spring Security RememberMe トークンの保存に DB を使用

--- java/spring-security/java-config/src/main/java/sample/spring/security/MySpringSecurityConfig.java
+++ java/spring-security/java-config/src/main/java/sample/spring/security/MySpringSecurityConfig.java
@@ -2,11 +2,16 @@ package sample.spring.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 
+import javax.sql.DataSource;
 import java.util.Collections;
 
 @EnableWebSecurity
@@ -22,7 +27,22 @@ public class MySpringSecurityConfig extends WebSecurityConfigurerAdapter {
                 .formLogin()
                 .and()
                 .rememberMe()
-                .useSecureCookie(true);
+                .tokenRepository(this.createTokenRepository());
+    }
+    
+    public PersistentTokenRepository createTokenRepository() {
+        DataSource dataSource =
+                new EmbeddedDatabaseBuilder()
+                    .generateUniqueName(true)
+                    .setType(EmbeddedDatabaseType.H2)
+                    .setScriptEncoding("UTF-8")
+                    .addScript("/sql/create_remember-me_tables.sql")
+                    .build();
+
+        JdbcTokenRepositoryImpl tokenRepository = new JdbcTokenRepositoryImpl();
+        tokenRepository.setDataSource(dataSource);
+        
+        return tokenRepository;
     }
     
     @Autowired
