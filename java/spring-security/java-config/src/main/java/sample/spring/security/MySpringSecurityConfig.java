519ca215d702ef54ca217adf11a92ff1961b2f64
opengl-8080 <tomcat.port.8080+github@gmail.com>
2017-04-18 00:13:30 +0900

    Spring Security Referrer-Policy を試そうとしたが、サポートしているブラウザが少なかったのでやめた

--- java/spring-security/java-config/src/main/java/sample/spring/security/MySpringSecurityConfig.java
+++ java/spring-security/java-config/src/main/java/sample/spring/security/MySpringSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.header.writers.ReferrerPolicyHeaderWriter;
 
 import java.util.Collections;
 
@@ -22,7 +23,7 @@ public class MySpringSecurityConfig extends WebSecurityConfigurerAdapter {
                 .formLogin()
                 .and()
                 .headers()
-                    .frameOptions().sameOrigin();
+                    .referrerPolicy(ReferrerPolicyHeaderWriter.ReferrerPolicy.SAME_ORIGIN);
     }
     
     @Autowired
