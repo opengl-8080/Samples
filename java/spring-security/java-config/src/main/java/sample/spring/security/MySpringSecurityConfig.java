a12f21a5cde6993bea7fa5dd96e8ef8d94f24b6b
opengl-8080 <tomcat.port.8080+github@gmail.com>
2017-04-25 00:15:10 +0900

    Spring Security UserDetailsService の自作を ComponentScan なしで

--- java/spring-security/java-config/src/main/java/sample/spring/security/MySpringSecurityConfig.java
+++ java/spring-security/java-config/src/main/java/sample/spring/security/MySpringSecurityConfig.java
@@ -1,15 +1,10 @@
 package sample.spring.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-import java.util.Collections;
 
 @EnableWebSecurity
 public class MySpringSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -22,17 +17,9 @@ public class MySpringSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .formLogin();
     }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
     
     @Autowired
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-            .withUser("hoge")
-            .password("$2a$08$CekzJRYhb5bzp5mx/eZmX.grG92fRXo267QVVyRs0IE.V.zeCIw8S")
-            .authorities(Collections.emptyList());
+        auth.userDetailsService(new MyUserDetailsService());
     }
 }
