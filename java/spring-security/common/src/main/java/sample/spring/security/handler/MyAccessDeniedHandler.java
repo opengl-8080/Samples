ebd4780f5b9f18a7a5d2c16bad9a62cd2dfc0314
opengl-8080 <tomcat.port.8080+github@gmail.com>
2017-04-27 23:56:06 +0900

    Spring Security 認可エラー時の任意実装

--- java/spring-security/common/src/main/java/sample/spring/security/handler/MyAccessDeniedHandler.java
+++ java/spring-security/common/src/main/java/sample/spring/security/handler/MyAccessDeniedHandler.java
@@ -1,10 +1,9 @@
 package sample.spring.security.handler;
 
 import org.springframework.security.access.AccessDeniedException;
-import org.springframework.security.web.WebAttributes;
+import org.springframework.security.web.DefaultRedirectStrategy;
 import org.springframework.security.web.access.AccessDeniedHandler;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -14,12 +13,13 @@ public class MyAccessDeniedHandler implements AccessDeniedHandler {
     
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
-        if (!response.isCommitted()) {
-            request.setAttribute(WebAttributes.ACCESS_DENIED_403, accessDeniedException);
-            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
-
-            RequestDispatcher dispatcher = request.getRequestDispatcher("/access-denied.html");
-            dispatcher.forward(request, response);
+        if (response.isCommitted()) {
+            return;
         }
+        
+        response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+
+        DefaultRedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+        redirectStrategy.sendRedirect(request, response, "/access-denied.html");
     }
 }
