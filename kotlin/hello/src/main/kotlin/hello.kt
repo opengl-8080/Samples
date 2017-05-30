97e24c0ab5f326ba56791b9798720c1a2742fb52
opengl-8080 <tomcat.port.8080+github@gmail.com>
2017-05-30 23:09:04 +0900

    Kotlin プロジェクトを InteliJ 用に変更

--- kotlin/hello/src/main/kotlin/hello.kt
+++ kotlin/hello/src/main/kotlin/hello.kt
@@ -1,11 +1,3 @@
 fun main(args: Array<String>) {
-    val mutableMap = mutableMapOf("one" to "ONE", "two" to "TWO", "three" to "THREE")
-
-    mutableMap.plusAssign(mapOf("four" to "FOUR"))
-    mutableMap.plusAssign("five" to "FIVE")
-    mutableMap.plusAssign(listOf("six" to "SIX"))
-
-    mutableMap += "seven" to "SEVEN"
-
-    println(mutableMap)
+    
 }
