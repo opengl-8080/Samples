47fed7cf16bb25bea2d47af8144563787c0b44fa
opengl-8080 <tomcat.port.8080+github@gmail.com>
2017-07-23 21:39:48 +0900

    Spring Security Test MVC result matchers

--- java/spring-security/namespace/src/test/java/sample/spring/security/test/MyMvcControllerTest.java
+++ java/spring-security/namespace/src/test/java/sample/spring/security/test/MyMvcControllerTest.java
@@ -4,16 +4,19 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import java.util.List;
+
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.*;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.*;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -34,18 +37,42 @@ public class MyMvcControllerTest {
     }
 
     @Test
-    public void test() throws Exception {
-        MvcResult result = this.mvc.perform(logout()).andReturn();
-        this.printResponse("test", result);
+    public void test_unauthenticated() throws Exception {
+        this.mvc.perform(formLogin().user("foo").password("invalid"))
+                .andExpect(unauthenticated());
     }
 
-    private void printResponse(String method, MvcResult result) throws Exception {
-        MockHttpServletResponse response = result.getResponse();
-        int status = response.getStatus();
-        String location = response.getHeader("Location");
+    @Test
+    public void test_authenticated() throws Exception {
+        this.mvc.perform(formLogin().user("foo").password("foo"))
+                .andExpect(authenticated());
+    }
 
-        System.out.println("[" + method + "]\n" +
-                "status : " + status + "\n" +
-                "location : " + location);
+    @Test
+    public void test_withUsername() throws Exception {
+        this.mvc.perform(formLogin().user("foo").password("foo"))
+                .andExpect(authenticated().withUsername("foo"));
+    }
+
+    @Test
+    public void test_withAuthorities() throws Exception {
+        List<GrantedAuthority> authorities = AuthorityUtils.createAuthorityList("FOO", "BAR");
+        
+        this.mvc.perform(formLogin().user("foo").password("foo"))
+                .andExpect(authenticated().withAuthorities(authorities));
+    }
+
+    @Test
+    public void test_combine() throws Exception {
+        List<GrantedAuthority> authorities = AuthorityUtils.createAuthorityList("FOO", "BAR");
+        
+        this.mvc.perform(formLogin().user("foo").password("foo"))
+                .andExpect(authenticated().withUsername("foo").withAuthorities(authorities));
+    }
+
+    @Test
+    public void test_withRoles() throws Exception {
+        this.mvc.perform(formLogin().user("fizz").password("fizz"))
+                .andExpect(authenticated().withRoles("FIZZ", "BUZZ"));
     }
 }
