27a16dd009639dacece15e2ba844479e32055c9b
opengl-8080 <tomcat.port.8080+github@gmail.com>
2017-07-25 23:18:08 +0900

    Spring MVC Hello World

---
+++ java/spring-mvc/src/main/java/sample/spring/mvc/MyWebApplicationInitializer.java
@@ -0,0 +1,21 @@
+package sample.spring.mvc;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MyWebApplicationInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[0];
+    }
+}
