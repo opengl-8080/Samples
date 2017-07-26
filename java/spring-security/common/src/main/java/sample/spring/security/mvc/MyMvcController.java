23bf9a7c289dd113cfe091467d1dfdac0af26730
opengl-8080 <tomcat.port.8080+github@gmail.com>
2017-07-26 22:56:09 +0900

    Spring Security MVC Integration MvcRequestMatcher

--- java/spring-security/common/src/main/java/sample/spring/security/mvc/MyMvcController.java
+++ java/spring-security/common/src/main/java/sample/spring/security/mvc/MyMvcController.java
@@ -1,9 +1,9 @@
 package sample.spring.security.mvc;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class MyMvcController {
     
     @GetMapping("/hello")
