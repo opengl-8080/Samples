595c78984a567571d0bb248534364e1cf7050dcb
opengl-8080 <tomcat.port.8080+github@gmail.com>
2017-07-31 22:35:13 +0900

    Spring Security MVC @AuthenticationPrincipal

--- java/spring-security/common/src/main/java/sample/spring/security/mvc/MyMvcController.java
+++ java/spring-security/common/src/main/java/sample/spring/security/mvc/MyMvcController.java
@@ -1,13 +1,16 @@
 package sample.spring.security.mvc;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MyMvcController {
     
-    @GetMapping("/foo")
-    public String foo() {
-        return "FOO!!";
+    @GetMapping("/user")
+    public String foo(@AuthenticationPrincipal User user) {
+        System.out.println("username=" + user.getUsername() + ", authorities=" + user.getAuthorities());
+        return "User!!";
     }
 }
--- java/spring-security/java-config/src/main/java/sample/spring/config/MySecurityConfig.java
+++ java/spring-security/java-config/src/main/java/sample/spring/config/MySecurityConfig.java
@@ -1,5 +1,6 @@
 package sample.spring.config;
 
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -10,8 +11,17 @@ public class MySecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-            .mvcMatchers("/foo").authenticated()
+            .mvcMatchers("/login").permitAll()
+            .anyRequest().authenticated()
             .and()
             .formLogin();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+            .withUser("foo")
+            .password("foo")
+            .authorities("GENERAL_USER", "ADMINISTRATOR");
     }
 }
