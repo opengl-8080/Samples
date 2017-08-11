e8c20bb4906176ab7511e368b0c2ffc9ef38cbb8
opengl-8080 <tomcat.port.8080+github@gmail.com>
2017-08-11 21:45:41 +0900

    JavaParser toString() の挙動確認

--- java/javaparser/src/main/java/sample/javaparser/Main.java
+++ java/javaparser/src/main/java/sample/javaparser/Main.java
@@ -2,9 +2,6 @@ package sample.javaparser;
 
 import com.github.javaparser.JavaParser;
 import com.github.javaparser.ast.CompilationUnit;
-import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
-import com.github.javaparser.ast.body.FieldDeclaration;
-import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
 
 import java.io.IOException;
 import java.nio.file.Path;
@@ -12,29 +9,11 @@ import java.nio.file.Paths;
 
 public class Main {
     public static void main(String[] args) {
-        Path source = Paths.get("src/main/java/sample/javaparser/ParseCommentSample.java");
+        Path source = Paths.get("src/main/java/sample/javaparser/ToStringSample.java");
         
         try {
             CompilationUnit unit = JavaParser.parse(source);
-
-            System.out.println("unit orphan comment > " + unit.getOrphanComments());
-
-            unit.accept(new VoidVisitorAdapter<Void>() {
-                @Override
-                public void visit(ClassOrInterfaceDeclaration n, Void arg) {
-                    System.out.println("<<ClassOrInterfaceDeclaration>>");
-                    System.out.println("comment > " + n.getComment());
-                    System.out.println("orphan comment > " + n.getOrphanComments());
-                    super.visit(n, arg);
-                }
-
-                @Override
-                public void visit(FieldDeclaration n, Void arg) {
-                    System.out.println("<<FieldDeclaration>>");
-                    System.out.println("comment > " + n.getComment());
-                    super.visit(n, arg);
-                }
-            }, null);
+            System.out.println(unit);
         } catch (IOException e) {
             e.printStackTrace(System.err);
         }
---
+++ java/javaparser/src/main/java/sample/javaparser/ToStringSample.java
@@ -0,0 +1,14 @@
+package sample.javaparser;
+
+public class ToStringSample {
+
+    public void method(
+       int age,
+       String name
+    ) {
+        System.out.println(
+            "age=" + age +
+            "name=" + name
+        );
+    }
+}
