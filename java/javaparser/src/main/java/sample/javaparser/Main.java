f221eb0945be5760c2909cf166956f5a6522543c
opengl-8080 <tomcat.port.8080+github@gmail.com>
2017-08-11 21:14:28 +0900

    JavaParser コメントデフォルトの挙動を検証

--- java/javaparser/src/main/java/sample/javaparser/Main.java
+++ java/javaparser/src/main/java/sample/javaparser/Main.java
@@ -2,17 +2,7 @@ package sample.javaparser;
 
 import com.github.javaparser.JavaParser;
 import com.github.javaparser.ast.CompilationUnit;
-import com.github.javaparser.ast.body.AnnotationMemberDeclaration;
-import com.github.javaparser.ast.expr.ConditionalExpr;
-import com.github.javaparser.ast.expr.EnclosedExpr;
-import com.github.javaparser.ast.expr.IntegerLiteralExpr;
-import com.github.javaparser.ast.expr.TypeExpr;
-import com.github.javaparser.ast.expr.VariableDeclarationExpr;
-import com.github.javaparser.ast.stmt.ExplicitConstructorInvocationStmt;
-import com.github.javaparser.ast.stmt.UnparsableStmt;
-import com.github.javaparser.ast.type.IntersectionType;
-import com.github.javaparser.ast.type.UnionType;
-import com.github.javaparser.ast.type.UnknownType;
+import com.github.javaparser.ast.comments.LineComment;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
 
 import java.io.IOException;
@@ -21,21 +11,18 @@ import java.nio.file.Paths;
 
 public class Main {
     public static void main(String[] args) {
-        Path source = Paths.get("src/main/java/sample/javaparser/TargetClass.java");
+        Path source = Paths.get("src/main/java/sample/javaparser/ParseCommentSample.java");
         
         try {
             CompilationUnit unit = JavaParser.parse(source);
             
             unit.accept(new VoidVisitorAdapter<Void>() {
-                
                 @Override
-                public void visit(UnknownType n, Void arg) {
-                    System.out.println("****************************************");
-                    System.out.println(n.getParentNode().get().getChildNodes());
+                public void visit(LineComment n, Void arg) {
+                    System.out.println(n);
                     super.visit(n, arg);
                 }
             }, null);
-
         } catch (IOException e) {
             e.printStackTrace(System.err);
         }
---
+++ java/javaparser/src/main/java/sample/javaparser/ParseCommentSample.java
@@ -0,0 +1,11 @@
+package sample.javaparser;
+
+// a comment
+
+// b comment
+public class ParseCommentSample {
+    
+    // c comment
+    
+    private String name; // d comment
+}
