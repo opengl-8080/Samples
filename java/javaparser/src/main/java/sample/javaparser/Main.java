0aaef08137743f56ad0ecc61896cc622e212ead4
opengl-8080 <tomcat.port.8080+github@gmail.com>
2017-08-11 22:17:44 +0900

    JavaParser toString の整形

--- java/javaparser/src/main/java/sample/javaparser/Main.java
+++ java/javaparser/src/main/java/sample/javaparser/Main.java
@@ -2,6 +2,9 @@ package sample.javaparser;
 
 import com.github.javaparser.JavaParser;
 import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.Modifier;
+import com.github.javaparser.ast.body.ConstructorDeclaration;
+import com.github.javaparser.ast.stmt.BlockStmt;
 
 import java.io.IOException;
 import java.nio.file.Path;
@@ -9,10 +12,22 @@ import java.nio.file.Paths;
 
 public class Main {
     public static void main(String[] args) {
-        Path source = Paths.get("src/main/java/sample/javaparser/ToStringSample.java");
+        Path source = Paths.get("src/main/java/sample/javaparser/Main.java");
         
         try {
             CompilationUnit unit = JavaParser.parse(source);
+
+            unit.getClassByName("Main").ifPresent(mainClass -> {
+                // private final String textValue; フィールドを追加
+                mainClass.addField(String.class, "textValue", Modifier.PRIVATE, Modifier.FINAL);
+                
+                // textValue を初期化するようにコンストラクタを追加
+                ConstructorDeclaration constructor = mainClass.addConstructor(Modifier.PUBLIC);
+                constructor.addParameter(String.class, "textValue"); // コンストラクタの引数追加
+                BlockStmt body = constructor.createBody(); // コンストラクタのボディを追加
+                body.addStatement("this.textValue = textValue;");
+            });
+
             System.out.println(unit);
         } catch (IOException e) {
             e.printStackTrace(System.err);
--- java/javaparser/src/main/java/sample/javaparser/ToStringSample.java
+++ java/javaparser/src/main/java/sample/javaparser/ToStringSample.java
@@ -1,11 +1,21 @@
 package sample.javaparser;
 
+/**
+ * Javadoc
+ */
 public class ToStringSample {
+    // line comment
 
+    /**
+     * Javadoc
+     * @param age age
+     * @param name name
+     */
     public void method(
        int age,
        String name
     ) {
+        /* block comment */
         System.out.println(
             "age=" + age +
             "name=" + name
