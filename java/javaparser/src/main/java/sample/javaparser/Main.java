496e34dc64c943df66436cbaab715f97ef976083
opengl-8080 <tomcat.port.8080+github@gmail.com>
2017-08-11 17:37:31 +0900

    JavaParser Hello World

---
+++ java/javaparser/src/main/java/sample/javaparser/Main.java
@@ -0,0 +1,25 @@
+package sample.javaparser;
+
+import com.github.javaparser.JavaParser;
+import com.github.javaparser.ast.CompilationUnit;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Main {
+    public static void main(String[] args) {
+        Path source = Paths.get("src/main/java/sample/javaparser/Main.java");
+        
+        try {
+            CompilationUnit unit = JavaParser.parse(source);
+            
+            System.out.println("***********************************************");
+            System.out.println(unit);
+            System.out.println("***********************************************");
+            
+        } catch (IOException e) {
+            e.printStackTrace(System.err);
+        }
+    }
+}
