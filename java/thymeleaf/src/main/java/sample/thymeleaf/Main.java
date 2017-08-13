0b1c2f0d6d961aa518d1037f3c57ae155dddcdc7
opengl-8080 <tomcat.port.8080+github@gmail.com>
2017-08-14 02:07:17 +0900

    Thymeleaf Message Bundle

--- java/thymeleaf/src/main/java/sample/thymeleaf/Main.java
+++ java/thymeleaf/src/main/java/sample/thymeleaf/Main.java
@@ -2,11 +2,21 @@ package sample.thymeleaf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
 
 @SpringBootApplication
 public class Main {
 
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
+    }
+    
+    @Bean
+    public ResourceBundleMessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("messages/Messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
     }
 }
