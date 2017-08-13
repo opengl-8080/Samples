a1b8330df0e48117508759b59fd87b1db2339cdb
opengl-8080 <tomcat.port.8080+github@gmail.com>
2017-08-13 14:51:19 +0900

    Thymeleaf th:text hello world

--- java/thymeleaf/src/main/java/sample/thymeleaf/web/HelloController.java
+++ java/thymeleaf/src/main/java/sample/thymeleaf/web/HelloController.java
@@ -1,15 +1,13 @@
 package sample.thymeleaf.web;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloController {
     
     @GetMapping("/hello")
-    public String hello(Model model) {
-        model.addAttribute("message", "Hello Thymeleaf!!");
+    public String hello() {
         return "hello";
     }
 }
