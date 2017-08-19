c23f48effeed66a484c4edb190ecf580a5ece071
opengl-8080 <tomcat.port.8080+github@gmail.com>
2017-08-19 16:03:16 +0900

    Reladomo CRUD create

--- java/reladomo/src/main/java/sample/reladomo/Main.java
+++ java/reladomo/src/main/java/sample/reladomo/Main.java
@@ -11,29 +11,28 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 public class Main {
-    
+
     public static void main(String[] args) throws Exception {
         prepareDatabase();
         prepareConnectionManager();
 
         try (TablePrinter tablePrinter = new TablePrinter()) {
             MithraManagerProvider.getMithraManager().executeTransactionalCommand(tx -> {
-                // DB に SampleTable のインスタンスを保存
-                SampleTable foo1 = new SampleTable();
-                foo1.setName("foo1");
-                foo1.insert();
-
-                SampleTable foo2 = new SampleTable();
-                foo2.setName("foo2");
-                foo2.insert();
+                SampleTable foo = new SampleTable();
+                foo.setName("foo");
+                foo.insert();
                 
+                SampleTable bar = new SampleTable();
+                bar.setName("bar");
+                bar.insert();
+
                 return null;
             });
             
-            tablePrinter.print("SAMPLE_TABLE");
+            tablePrinter.print("sample_table");
         }
     }
-    
+
     private static void prepareDatabase() {
         try (
             Connection con = MyConnectionManager.getInstance().getConnection();
@@ -44,7 +43,7 @@ public class Main {
             throw new RuntimeException("failed to prepare database.", e);
         }
     }
-    
+
     private static void prepareConnectionManager() {
         try (InputStream config = Main.class.getResourceAsStream("/ReladomoConfig.xml")) {
             MithraManagerProvider.getMithraManager().readConfiguration(config);
