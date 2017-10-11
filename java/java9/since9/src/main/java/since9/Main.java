35a8d8002d4dcc1f94237fbed0dde5e08375e28c
opengl-8080 <tomcat.port.8080+github@gmail.com>
2017-10-11 22:04:56 +0900

    Since9 方針転換前コミット

--- java/java9/since9/src/main/java/since9/Main.java
+++ java/java9/since9/src/main/java/since9/Main.java
@@ -1,20 +1,58 @@
 package since9;
 
 import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.UncheckedIOException;
 import java.net.URL;
+import java.net.URLConnection;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
     private static final String URL_BASE = "http://download.java.net/java/jdk9/docs/api";
+    private boolean shouldWait;
 
-    public static void main(String[] args) throws IOException {
-        URL url = new URL(URL_BASE + "/allclasses-noframe.html");
-        Document document = Jsoup.parse(url, (int) TimeUnit.SECONDS.toMillis(30L));
-        document.select("body main ul li a").forEach(link -> {
-            System.out.println(link);
+    public static void main(String[] args) {
+        new Main().execute();
+    }
+    
+    private void execute() {
+        String allClassesHtml = this.get(URL_BASE + "/allclasses-noframe.html");
+        Jsoup.parse(allClassesHtml).select("body main ul li a").forEach(link -> {
+            String href = link.attr("href");
+            String classPage = this.get(URL_BASE + "/" + href);
+            Jsoup.parse(classPage).select("")
         });
+    }
+    
+    private String get(String url) {
+        if (this.shouldWait) {
+            try {
+                TimeUnit.SECONDS.sleep(2L);
+            } catch (InterruptedException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        
+        try {
+            URLConnection con = new URL(url).openConnection();
+            try (InputStream in = con.getInputStream();
+                 ByteArrayOutputStream out = new ByteArrayOutputStream();) {
+                
+                byte[] buf = new byte[1024];
+                int length;
+                while ((length = in.read(buf)) != -1) {
+                    out.write(buf, 0, length);
+                }
+
+                return out.toString("UTF-8");
+            }
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        } finally {
+            this.shouldWait = true;
+        }
     }
 }
