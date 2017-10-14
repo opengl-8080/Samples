567dfad22c8b7feb99622b91b8e573221e659435
opengl-8080 <tomcat.port.8080+github@gmail.com>
2017-10-14 18:11:14 +0900

    正規表現 replaceAll

--- java/regexp/src/main/java/sample/regexp/Main.java
+++ java/regexp/src/main/java/sample/regexp/Main.java
@@ -3,9 +3,8 @@ package sample.regexp;
 public class Main {
 
     public static void main(String[] args) {
-        String text = "abc123";
+        String text = "<<abc123>>";
 
-        System.out.println(text.matches("[a-z0-9]+"));
-        System.out.println(text.matches("[a-z]+"));
+        System.out.println(text.replaceAll("[a-z]+", "$"));
     }
 }
