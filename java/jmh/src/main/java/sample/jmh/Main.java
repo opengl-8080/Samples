0682eee436389972e80b1504c132e53b55786298
opengl-8080 <tomcat.port.8080+github@gmail.com>
2017-10-17 22:51:14 +0900

    JMH JIT の動き確認

--- java/jmh/src/main/java/sample/jmh/Main.java
+++ java/jmh/src/main/java/sample/jmh/Main.java
@@ -1,21 +1,33 @@
 package sample.jmh;
 
 import org.openjdk.jmh.annotations.Benchmark;
-import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
-import org.openjdk.jmh.runner.options.Options;
-import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 
 public class Main {
 
-    public static void main(String[] args) throws RunnerException {
-        Options options = new OptionsBuilder().include(Main.class.getSimpleName())
-                .warmupIterations(5)
-                .measurementIterations(5)
-                .forks(2)
-                .build();
-        
-        new Runner(options).run();
+    public static void main(String[] args) throws RunnerException, IOException {
+//        Options options = new OptionsBuilder().include(Main.class.getSimpleName())
+//                .warmupIterations(5)
+//                .measurementIterations(5)
+//                .forks(2)
+//                .build();
+//        
+//        new Runner(options).run();
+
+        try (BufferedWriter bw = Files.newBufferedWriter(Paths.get("build/test.txt"), StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING)) {
+            for (int i=0; i<10000; i++) {
+                long start = System.nanoTime();
+                new Main().execute();
+                long end = System.nanoTime();
+                bw.write(String.format("[%05d] %d%n", i, (end - start)));
+            }
+        }
     }
     
     @Benchmark
