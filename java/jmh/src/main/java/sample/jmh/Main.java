fd638c97e56405bec491f4c1d34eab830c0a1c3d
opengl-8080 <tomcat.port.8080+github@gmail.com>
2017-10-18 22:26:28 +0900

    JMH JMHを使わない場合のコードを整理

--- java/jmh/src/main/java/sample/jmh/Main.java
+++ java/jmh/src/main/java/sample/jmh/Main.java
@@ -1,37 +1,19 @@
 package sample.jmh;
 
-import org.openjdk.jmh.annotations.Benchmark;
-import org.openjdk.jmh.runner.RunnerException;
-
-import java.io.BufferedWriter;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
 
 public class Main {
 
-    public static void main(String[] args) throws RunnerException, IOException {
-//        Options options = new OptionsBuilder().include(Main.class.getSimpleName())
-//                .warmupIterations(5)
-//                .measurementIterations(5)
-//                .forks(2)
-//                .build();
-//        
-//        new Runner(options).run();
-
-        try (BufferedWriter bw = Files.newBufferedWriter(Paths.get("build/test.txt"), StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING)) {
-            for (int i=0; i<10000; i++) {
-                long start = System.nanoTime();
-                new Main().execute();
-                long end = System.nanoTime();
-                bw.write(String.format("[%05d] %d%n", i, (end - start)));
-            }
+    public static void main(String[] args) throws IOException {
+        for (int i=0; i<1000; i++) {
+            long start = System.nanoTime();
+            new Main().execute();
+            long end = System.nanoTime();
+            System.out.print(String.format("[%05d] %d%n", i, (end - start)));
         }
     }
     
-    @Benchmark
-    public void execute() {
+    private void execute() {
         for (int i=0; i<10000; i++) {
             String a = "abc";
             String b = "def";
