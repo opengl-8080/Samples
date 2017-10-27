8a3ac94182dd3f423afed8bc1e3fe36a388b7df2
opengl-8080 <tomcat.port.8080+github@gmail.com>
2017-10-27 22:36:23 +0900

    JTA-study メモリ版に修正

--- java/study-jta/src/main/java/sample/jta/Main.java
+++ java/study-jta/src/main/java/sample/jta/Main.java
@@ -1,38 +1,35 @@
 package sample.jta;
 
-import org.h2.tools.Server;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 public class Main {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        Server server = Server.createTcpServer().start();
-
-        System.out.println("hoge");
-
         Class.forName("org.h2.Driver");
-        try (Connection con1 = DriverManager.getConnection("jdbc:h2:tcp://localhost/./out/test", "sa", "");
-             Connection con2 = DriverManager.getConnection("jdbc:h2:tcp://localhost/./out/test", "sa", "")) {
-            try (Statement stmt = con1.createStatement()) {
-                stmt.executeUpdate("create table if not exists foo(id number(10), code varchar(20))");
+        try (Connection con = getConnection();
+             Statement stmt = con.createStatement()) {
+            stmt.executeUpdate("create table if not exists foo(id number(10), code varchar(20))");
 
-                stmt.executeUpdate("insert into foo values (1, 'hoge')");
-                stmt.executeUpdate("insert into foo values (2, 'fuga')");
-            }
-            
-            try (ResultSet rs = con2.createStatement().executeQuery("select * from foo")) {
-                while (rs.next()) {
-                    long id = rs.getLong("id");
-                    String code = rs.getString("code");
-                    System.out.println("id=" + id + ", code=" + code);
-                }
-            }
+            stmt.executeUpdate("insert into foo values (1, 'hoge')");
+            stmt.executeUpdate("insert into foo values (2, 'fuga')");
         }
 
-        server.stop();
+        try (Connection con = getConnection();
+             PreparedStatement ps = con.prepareStatement("select * from foo");
+             ResultSet rs = ps.executeQuery()) {
+            while (rs.next()) {
+                long id = rs.getLong("id");
+                String code = rs.getString("code");
+                System.out.println("id=" + id + ", code=" + code);
+            }
+        }
+    }
+    
+    private static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1", "sa", "");
     }
 }
