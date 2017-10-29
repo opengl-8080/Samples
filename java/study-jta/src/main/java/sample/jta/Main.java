f5a7d7c25275f27068ad4e9304bba4330d391199
opengl-8080 <tomcat.port.8080+github@gmail.com>
2017-10-29 11:18:49 +0900

    JDBC Statement, Result の close 整理

--- java/study-jta/src/main/java/sample/jta/Main.java
+++ java/study-jta/src/main/java/sample/jta/Main.java
@@ -8,28 +8,37 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class Main {
-    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+    public static void main(String[] args) throws ClassNotFoundException {
         Class.forName("org.h2.Driver");
-        try (Connection con = getConnection();
-             Statement stmt = con.createStatement()) {
-            stmt.executeUpdate("create table if not exists foo(id number(10), code varchar(20))");
 
-            stmt.executeUpdate("insert into foo values (1, 'hoge')");
-            stmt.executeUpdate("insert into foo values (2, 'fuga')");
-        }
-
-        try (Connection con = getConnection();
-             PreparedStatement ps = con.prepareStatement("select * from foo");
-             ResultSet rs = ps.executeQuery()) {
-            while (rs.next()) {
-                long id = rs.getLong("id");
-                String code = rs.getString("code");
-                System.out.println("id=" + id + ", code=" + code);
+        try (Connection con = DriverManager.getConnection("jdbc:h2:mem:test", "sa", "")) {
+            try (Statement stmt = con.createStatement()) {
+                stmt.executeUpdate("create table if not exists foo(id number(10), code varchar(20))");
             }
+
+            try (PreparedStatement ps = con.prepareStatement("insert into foo values (?, ?)")) {
+                ps.setLong(1, 1L);
+                ps.setString(2, "hoge");
+                ps.executeUpdate();
+
+                ps.setLong(1, 2L);
+                ps.setString(2, "fuga");
+                ps.executeUpdate();
+            }
+
+            ResultSet rs;
+            try (PreparedStatement ps = con.prepareStatement("select * from foo")) {
+                rs = ps.executeQuery();
+                while (rs.next()) {
+                    long id = rs.getLong("id");
+                    String code = rs.getString("code");
+                    System.out.println("id=" + id + ", code=" + code);
+                }
+                System.out.println("(1) rs.isClosed()=" + rs.isClosed());
+            }
+            System.out.println("(2) rs.isClosed()=" + rs.isClosed());
+        } catch (SQLException e) {
+            e.printStackTrace(System.err);
         }
-    }
-    
-    private static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1", "sa", "");
     }
 }
