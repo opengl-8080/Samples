9644b14da94f4d74419b2b7b8b1f35e428e53c3a
opengl-8080 <tomcat.port.8080+github@gmail.com>
2017-11-10 22:33:13 +0900

    JavaFX Alert Hello World

--- java/javafx/src/main/java/sample/javafx/Main.java
+++ java/javafx/src/main/java/sample/javafx/Main.java
@@ -6,17 +6,19 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.net.MalformedURLException;
+import java.net.URL;
 
 public class Main extends Application {
 
-    public static void main(String[] args) throws InterruptedException, MalformedURLException {
-        launch(args);
+    public static void main(String[] args) {
+        Application.launch(args);
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(this.getClass().getResource("/main.fxml"));
+        URL url = this.getClass().getResource("/main.fxml");
+        System.out.println(url);
+        Parent root = FXMLLoader.load(url);
         Scene scene = new Scene(root);
         primaryStage.setScene(scene);
 
--- java/javafx/src/main/java/sample/javafx/MainController.java
+++ java/javafx/src/main/java/sample/javafx/MainController.java
@@ -1,28 +1,13 @@
 package sample.javafx;
 
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.media.Media;
-import javafx.scene.media.MediaPlayer;
-import javafx.scene.media.MediaView;
+import javafx.scene.control.Alert;
 
-import java.net.URL;
-import java.nio.file.Paths;
-import java.util.ResourceBundle;
-
-public class MainController implements Initializable {
-
-    @FXML
-    private MediaView mediaView;
+public class MainController {
     
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-        String source = Paths.get("movies/oow2010-2.flv").toUri().toString();
-        Media media = new Media(source);
-        MediaPlayer player = new MediaPlayer(media);
-        this.mediaView.setMediaPlayer(player);
-        player.play();
-
-        System.out.println("play!!");
+    @FXML
+    public void showDialog() {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.showAndWait();
     }
 }
