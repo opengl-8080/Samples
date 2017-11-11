1088806ffa2280a8341f7db0acaa7a128a9e576d
opengl-8080 <tomcat.port.8080+github@gmail.com>
2017-11-11 19:49:50 +0900

    JavaFX showAndWait() result

--- java/javafx/src/main/java/sample/javafx/MainController.java
+++ java/javafx/src/main/java/sample/javafx/MainController.java
@@ -2,12 +2,19 @@ package sample.javafx;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+
+import java.util.Optional;
 
 public class MainController {
     
     @FXML
     public void showDialog() {
         Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
-        alert.showAndWait();
+        Optional<ButtonType> buttonType = alert.showAndWait();
+        
+        buttonType.ifPresent(selected -> {
+            System.out.println("selected=" + selected);
+        });
     }
 }
