7583b643fd7e6dad18909732a5b231b4b5bd15ea
opengl-8080 <tomcat.port.8080+github@gmail.com>
2017-11-12 00:35:26 +0900

    JavaFX new ButtonType

--- java/javafx/src/main/java/sample/javafx/MainController.java
+++ java/javafx/src/main/java/sample/javafx/MainController.java
@@ -2,17 +2,15 @@ package sample.javafx;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonBar;
 import javafx.scene.control.ButtonType;
 
 public class MainController {
     
     @FXML
     public void showDialog() {
-        Alert alert = new Alert(
-                Alert.AlertType.INFORMATION, 
-                "めっせーじ",
-                ButtonType.OK,
-                ButtonType.NO);
-        alert.showAndWait();
+        ButtonType myButton = new ButtonType("ぼたん", ButtonBar.ButtonData.APPLY);
+        Alert alert = new Alert(Alert.AlertType.INFORMATION, "めっせーじ", myButton);
+        alert.showAndWait().ifPresent(System.out::println);
     }
 }
