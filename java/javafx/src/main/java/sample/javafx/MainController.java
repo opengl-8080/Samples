153b5a081a6954b51eefaa798b7dbec1564ecc0f
opengl-8080 <tomcat.port.8080+github@gmail.com>
2017-11-11 20:03:09 +0900

    JavaFX AlertType

--- java/javafx/src/main/java/sample/javafx/MainController.java
+++ java/javafx/src/main/java/sample/javafx/MainController.java
@@ -2,19 +2,14 @@ package sample.javafx;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
-import javafx.scene.control.ButtonType;
-
-import java.util.Optional;
 
 public class MainController {
     
     @FXML
     public void showDialog() {
-        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
-        Optional<ButtonType> buttonType = alert.showAndWait();
-        
-        buttonType.ifPresent(selected -> {
-            System.out.println("selected=" + selected);
-        });
+        new Alert(Alert.AlertType.INFORMATION).showAndWait();
+        new Alert(Alert.AlertType.CONFIRMATION).showAndWait();
+        new Alert(Alert.AlertType.WARNING).showAndWait();
+        new Alert(Alert.AlertType.ERROR).showAndWait();
     }
 }
