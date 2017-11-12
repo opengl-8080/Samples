140766a9ddc4d9e9f395af8fb14c10f5a30558cf
opengl-8080 <tomcat.port.8080+github@gmail.com>
2017-11-12 09:28:22 +0900

    JavaFX showAndWait() の戻り値の検証1

--- java/javafx/src/main/java/sample/javafx/MainController.java
+++ java/javafx/src/main/java/sample/javafx/MainController.java
@@ -2,15 +2,17 @@ package sample.javafx;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
-import javafx.scene.control.ButtonBar;
 import javafx.scene.control.ButtonType;
+
+import java.util.Optional;
 
 public class MainController {
     
     @FXML
     public void showDialog() {
-        ButtonType myButton = new ButtonType("ぼたん", ButtonBar.ButtonData.APPLY);
-        Alert alert = new Alert(Alert.AlertType.INFORMATION, "めっせーじ", myButton);
-        alert.showAndWait().ifPresent(System.out::println);
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "めっせーじ", ButtonType.YES);
+        Optional<ButtonType> buttonType = alert.showAndWait();
+        String result = buttonType.map(ButtonType::getText).orElse("選択無し");
+        System.out.println(result);
     }
 }
