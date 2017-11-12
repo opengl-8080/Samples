570a9ea784911a74250bf814e2b249ac19a71798
opengl-8080 <tomcat.port.8080+github@gmail.com>
2017-11-12 09:52:43 +0900

    JavaFX titleText, contetText, headerText

--- java/javafx/src/main/java/sample/javafx/MainController.java
+++ java/javafx/src/main/java/sample/javafx/MainController.java
@@ -2,17 +2,15 @@ package sample.javafx;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
-import javafx.scene.control.ButtonType;
-
-import java.util.Optional;
 
 public class MainController {
     
     @FXML
     public void showDialog() {
-        Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "めっせーじ", ButtonType.YES, ButtonType.NO, ButtonType.CANCEL);
-        Optional<ButtonType> buttonType = alert.showAndWait();
-        String result = buttonType.map(ButtonType::getText).orElse("選択無し");
-        System.out.println(result);
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("たいとる");
+        alert.setHeaderText("へっだーてきすと");
+        alert.setContentText("こんてんつてきすと");
+        alert.showAndWait();
     }
 }
