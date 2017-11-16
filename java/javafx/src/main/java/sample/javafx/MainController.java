73538dd93fcf52c00ee9654486eeb25c14e566cc
opengl-8080 <tomcat.port.8080+github@gmail.com>
2017-11-16 23:46:50 +0900

    JavaFX 所有者の設定

--- java/javafx/src/main/java/sample/javafx/MainController.java
+++ java/javafx/src/main/java/sample/javafx/MainController.java
@@ -1,5 +1,38 @@
 package sample.javafx;
 
-public class MainController {
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
+import java.io.IOException;
+import java.net.URL;
+
+public class MainController {
+    
+    private Stage stage;
+
+    public void setStage(Stage stage) {
+        this.stage = stage;
+    }
+
+    @FXML
+    public void openOtherWindow() throws IOException {
+        // fxml をロードして、
+        URL fxml = this.getClass().getResource("/other.fxml");
+        FXMLLoader loader = new FXMLLoader(fxml);
+        Pane pane = loader.load();
+
+        // シーンを作成
+        Scene scene = new Scene(pane);
+
+        // ステージにシーンを登録して
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.initOwner(this.stage);
+
+        // 表示
+        stage.showAndWait();
+    }
 }
