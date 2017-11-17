512077a8ee4bb46ab4bbce869492186e1492b489
opengl-8080 <tomcat.port.8080+github@gmail.com>
2017-11-17 14:17:33 +0900

    JavaFX Modality

--- java/javafx/src/main/java/sample/javafx/MainController.java
+++ java/javafx/src/main/java/sample/javafx/MainController.java
@@ -1,26 +1,36 @@
 package sample.javafx;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+
 public class MainController {
-    
-    public void hello() {
-        System.out.println("Hello MainController!!");
+
+    private Stage stage;
+
+    public void setStage(Stage stage) {
+        this.stage = stage;
     }
-//
-//    @FXML
-//    public void openOtherWindow() throws IOException {
-//        // fxml をロードして、
-//        URL fxml = this.getClass().getResource("/other.fxml");
-//        FXMLLoader loader = new FXMLLoader(fxml);
-//        Pane pane = loader.load();
-//        
-//        // シーンを作成
-//        Scene scene = new Scene(pane);
-//        
-//        // ステージにシーンを登録して
-//        Stage stage = new Stage();
-//        stage.setScene(scene);
-//        
-//        // 表示
-//        stage.showAndWait();
-//    }
+
+    @FXML
+    public void openOtherWindow() throws IOException {
+        URL fxml = this.getClass().getResource("/other.fxml");
+        FXMLLoader loader = new FXMLLoader(fxml);
+        Pane pane = loader.load();
+
+        Scene scene = new Scene(pane);
+
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.initOwner(this.stage);
+        stage.initModality(Modality.WINDOW_MODAL);
+
+        stage.showAndWait();
+    }
 }
