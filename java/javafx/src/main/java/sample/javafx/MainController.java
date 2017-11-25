94418a325cce0e1605da617a2f291bfd1cca70b4
opengl-8080 <tomcat.port.8080+github@gmail.com>
2017-11-25 21:27:23 +0900

    JavaFX Event Bubbling

--- java/javafx/src/main/java/sample/javafx/MainController.java
+++ java/javafx/src/main/java/sample/javafx/MainController.java
@@ -1,35 +1,52 @@
 package sample.javafx;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.input.DragEvent;
-import javafx.scene.input.Dragboard;
-import javafx.scene.input.TransferMode;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
 
-public class MainController {
+import java.net.URL;
+import java.util.ResourceBundle;
 
+public class MainController implements Initializable {
+    @FXML
+    private Pane bluePane;
+    @FXML
+    private Pane yellowPane;
+    @FXML
+    private Button button;
+    @FXML
+    private TextField textField;
     @FXML
     private Label label;
-    
-    @FXML
-    public void onDragOver(DragEvent event) {
-        Dragboard dragboard = event.getDragboard();
 
-        if (dragboard.hasFiles()) {
-            dragboard.getFiles().forEach(System.out::println);
-            event.acceptTransferModes(TransferMode.COPY);
-        }
-
-        event.consume();
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        this.registerHandlers(this.bluePane, "bluePane");
+        this.registerHandlers(this.yellowPane, "yellowPane");
+//        this.registerConsumeHandlers(this.yellowPane, "yellowPane");
+        this.registerHandlers(this.button, "button");
+        this.registerHandlers(this.textField, "textField");
+        this.registerHandlers(this.label, "label");
     }
     
-    @FXML
-    public void onDragDropped(DragEvent event) {
-        Dragboard dragboard = event.getDragboard();
+    private void registerHandlers(Node node, String name) {
+        node.addEventFilter(MouseEvent.MOUSE_CLICKED, e -> System.out.println("filter " + name));
+        node.addEventHandler(MouseEvent.MOUSE_CLICKED, e -> System.out.println("handler " + name));
+    }
 
-        dragboard.getFiles().forEach(System.out::println);
-
-        event.setDropCompleted(true);
-        event.consume();
+    private void registerConsumeHandlers(Node node, String name) {
+        node.addEventFilter(MouseEvent.MOUSE_CLICKED, e -> {
+            System.out.println("filter " + name);
+            e.consume();
+        });
+        node.addEventHandler(MouseEvent.MOUSE_CLICKED, e -> {
+            System.out.println("handler " + name);
+            e.consume();
+        });
     }
 }
