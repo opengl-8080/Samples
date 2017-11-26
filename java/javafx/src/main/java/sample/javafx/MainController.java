813f206b074cd7c3cb0a46ba1cee5d0053a63bf6
opengl-8080 <tomcat.port.8080+github@gmail.com>
2017-11-26 19:10:01 +0900

    JavaFX drop

--- java/javafx/src/main/java/sample/javafx/MainController.java
+++ java/javafx/src/main/java/sample/javafx/MainController.java
@@ -1,33 +1,15 @@
 package sample.javafx;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-import javafx.scene.input.ClipboardContent;
 import javafx.scene.input.DragEvent;
 import javafx.scene.input.Dragboard;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.input.TransferMode;
 
 public class MainController {
-    @FXML
-    private Label dragLabel;
-    @FXML
-    private Label dropLabel;
-    
-    @FXML
-    public void onDragDetected(MouseEvent e) {
-        Dragboard dragboard = this.dragLabel.startDragAndDrop(TransferMode.COPY);
-
-        ClipboardContent content = new ClipboardContent();
-        content.putString("Drag Test");
-        dragboard.setContent(content);
-
-        e.consume();
-    }
     
     @FXML
     public void onDragOver(DragEvent e) {
-        e.acceptTransferModes(TransferMode.COPY_OR_MOVE);
+        e.acceptTransferModes(TransferMode.ANY);
         e.consume();
     }
     
@@ -36,5 +18,6 @@ public class MainController {
         Dragboard dragboard = e.getDragboard();
         String string = dragboard.getString();
         System.out.println("dropped string = " + string);
+        e.consume();
     }
 }
