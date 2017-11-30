b7509140163822228877c88354ff893e7b3952e3
opengl-8080 <tomcat.port.8080+github@gmail.com>
2017-11-30 22:28:20 +0900

    JavaFX Hello Observable

--- java/javafx/src/main/java/sample/javafx/property/Main.java
+++ java/javafx/src/main/java/sample/javafx/property/Main.java
@@ -1,19 +1,14 @@
 package sample.javafx.property;
 
-import javafx.beans.binding.DoubleBinding;
 import javafx.beans.property.SimpleDoubleProperty;
 
 public class Main {
     public static void main(String[] args) {
         SimpleDoubleProperty a = new SimpleDoubleProperty();
-        SimpleDoubleProperty b = new SimpleDoubleProperty();
+        
+        a.addListener(System.out::println);
+        
         a.set(1.0);
-        b.set(2.0);
-
-        DoubleBinding sum = a.add(b);
-        System.out.println(sum.get());
-
-        a.set(5.0);
-        System.out.println(sum.get());
+        a.set(2.0);
     }
 }
