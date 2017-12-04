3b6617410416e8270319d93cd3ad7876f43063f3
opengl-8080 <tomcat.port.8080+github@gmail.com>
2017-12-04 21:21:47 +0900

    JavaFX Property bind()

--- java/javafx/src/main/java/sample/javafx/property/Main.java
+++ java/javafx/src/main/java/sample/javafx/property/Main.java
@@ -1,19 +1,22 @@
 package sample.javafx.property;
 
-import javafx.beans.binding.DoubleBinding;
 import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.SimpleDoubleProperty;
 
 public class Main {
     public static void main(String[] args) {
-        DoubleProperty a = new SimpleDoubleProperty(2.0);
+        DoubleProperty a = new SimpleDoubleProperty();
+        DoubleProperty b = new SimpleDoubleProperty();
         
-        DoubleBinding binding = new MyBinding(a);
+        System.out.println("a=" + a + ", b=" + b);
 
-        System.out.println("((2.0 + 3) * 2 - 4.0) / 3.0 = " + binding.get());
+        a.bind(b);
+
+        System.out.println("a=" + a + ", b=" + b);
         
-        a.set(5.0);
+        b.set(3.0);
 
-        System.out.println("((5.0 + 3) * 2 - 4.0) / 3.0 = " + binding.get());
+        System.out.println("a=" + a + ", b=" + b);
+        System.out.println("a=" + a.get() + ", b=" + b);
     }
 }
