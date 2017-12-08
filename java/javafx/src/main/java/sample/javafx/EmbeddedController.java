be112fc0ed65aabbcdf8bdb4fbc08680592642ae
opengl-8080 <tomcat.port.8080+github@gmail.com>
2017-12-08 17:08:18 +0900

    JavaFX fx:include で別ウィンドウ

--- java/javafx/src/main/java/sample/javafx/EmbeddedController.java
+++ java/javafx/src/main/java/sample/javafx/EmbeddedController.java
@@ -1,13 +1,31 @@
 package sample.javafx;
 
+import javafx.beans.binding.StringBinding;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 
-public class EmbeddedController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class EmbeddedController implements Initializable {
+    
+    private IntegerProperty count = new SimpleIntegerProperty(0);
+    
     @FXML
     private Label label;
     
-    public void setMessage(String message) {
-        this.label.setText(message);
+    @FXML
+    public void countUp() {
+        int now = this.count.get();
+        this.count.set(now + 1);
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        StringBinding output = this.count.asString("count = %d");
+        this.label.textProperty().bind(output);
     }
 }
--- java/javafx/src/main/java/sample/javafx/MainController.java
+++ java/javafx/src/main/java/sample/javafx/MainController.java
@@ -2,16 +2,30 @@ package sample.javafx;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MainController implements Initializable {
     @FXML
-    private EmbeddedController embeddedController;
+    private Pane embeddedPane;
+    
+    private Stage embeddedWindow;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        this.embeddedController.setMessage("Hello!!");
+        Scene scene = new Scene(this.embeddedPane);
+        this.embeddedWindow = new Stage();
+        this.embeddedWindow.setScene(scene);
+        this.embeddedWindow.initModality(Modality.APPLICATION_MODAL);
+    }
+    
+    @FXML
+    public void openWindow() {
+        this.embeddedWindow.showAndWait();
     }
 }
