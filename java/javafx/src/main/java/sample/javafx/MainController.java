5130ee067feb6315d7f967b99cfc89cb5d862709
opengl-8080 <tomcat.port.8080+github@gmail.com>
2017-12-13 23:16:34 +0900

    JavaFX Thread Service から Task を実行

--- java/javafx/src/main/java/sample/javafx/MainController.java
+++ java/javafx/src/main/java/sample/javafx/MainController.java
@@ -1,5 +1,6 @@
 package sample.javafx;
 
+import javafx.concurrent.Service;
 import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 
@@ -8,7 +9,7 @@ import java.util.concurrent.ExecutionException;
 public class MainController {
     
     @FXML
-    public void start() throws ExecutionException, InterruptedException {
+    public void startTask() throws ExecutionException, InterruptedException {
         System.out.println("Main Thread = " + Thread.currentThread().getName());
         
         Task<String> task = new Task<String>() {
@@ -26,5 +27,29 @@ public class MainController {
 
         String result = task.get();
         System.out.println("end thread. result=" + result);
+    }
+    
+    @FXML
+    public void startService() {
+        Service<String> service = new Service<String>() {
+            @Override
+            protected Task<String> createTask() {
+                return new Task<String>() {
+
+                    @Override
+                    protected String call() throws Exception {
+                        System.out.println(Thread.currentThread().getName());
+                        return "fuga";
+                    }
+                };
+            }
+        };
+
+        System.out.println("start service");
+        service.start();
+        service.setOnSucceeded(e -> {
+            System.out.println("value=" + service.getValue());
+            System.out.println("thread = " + Thread.currentThread().getName());
+        });
     }
 }
