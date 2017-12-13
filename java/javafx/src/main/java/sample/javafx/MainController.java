2fc961835db86e0adef0f7d37526a8ccd60a5af1
opengl-8080 <tomcat.port.8080+github@gmail.com>
2017-12-13 23:04:10 +0900

    JavaFX Thread 普通にTaskを起動してみる

--- java/javafx/src/main/java/sample/javafx/MainController.java
+++ java/javafx/src/main/java/sample/javafx/MainController.java
@@ -1,31 +1,30 @@
 package sample.javafx;
 
+import javafx.concurrent.Task;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
-import javafx.stage.Modality;
-import javafx.stage.Stage;
 
-import java.net.URL;
-import java.util.ResourceBundle;
+import java.util.concurrent.ExecutionException;
 
-public class MainController implements Initializable {
-    @FXML
-    private Pane embeddedPane;
-    
-    private Stage embeddedWindow;
-
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-        Scene scene = new Scene(this.embeddedPane);
-        this.embeddedWindow = new Stage();
-        this.embeddedWindow.setScene(scene);
-        this.embeddedWindow.initModality(Modality.APPLICATION_MODAL);
-    }
+public class MainController {
     
     @FXML
-    public void openWindow() {
-        this.embeddedWindow.showAndWait();
+    public void start() throws ExecutionException, InterruptedException {
+        System.out.println("Main Thread = " + Thread.currentThread().getName());
+        
+        Task<String> task = new Task<String>() {
+
+            @Override
+            protected String call() throws Exception {
+                System.out.println(Thread.currentThread().getName());
+                return "hoge";
+            }
+        };
+
+        Thread thread = new Thread(task);
+        System.out.println("begin thread");
+        thread.start();
+
+        String result = task.get();
+        System.out.println("end thread. result=" + result);
     }
 }
