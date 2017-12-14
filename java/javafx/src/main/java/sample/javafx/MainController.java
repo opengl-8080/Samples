dcd40ea1a30f2a66802b5ec7d61b644ffc44dad5
opengl-8080 <tomcat.port.8080+github@gmail.com>
2017-12-14 23:03:43 +0900

    JavaFX Thread UIスレッド名を確認

--- java/javafx/src/main/java/sample/javafx/MainController.java
+++ java/javafx/src/main/java/sample/javafx/MainController.java
@@ -1,60 +1,64 @@
 package sample.javafx;
 
-import javafx.concurrent.Service;
-import javafx.concurrent.Task;
-import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 
-import java.util.concurrent.ExecutionException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class MainController {
-    
-    @FXML
-    public void startTask() throws ExecutionException, InterruptedException {
-        System.out.println("Main Thread = " + Thread.currentThread().getName());
-        
-        Task<String> task = new Task<String>() {
+public class MainController implements Initializable {
 
-            @Override
-            protected String call() throws Exception {
-                this.updateMessage("foo");
-                System.out.println(Thread.currentThread().getName());
-                return "hoge";
-            }
-        };
-
-        task.setOnSucceeded(e -> {
-            System.out.println("succeeded thread = " + Thread.currentThread().getName());
-        });
-
-        Thread thread = new Thread(task);
-        System.out.println("begin thread");
-        thread.start();
-
-        String result = task.get();
-        System.out.println("end thread. result=" + result);
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        System.out.println("thread = " + Thread.currentThread().getName());
     }
-    
-    @FXML
-    public void startService() {
-        Service<String> service = new Service<String>() {
-            @Override
-            protected Task<String> createTask() {
-                return new Task<String>() {
-
-                    @Override
-                    protected String call() throws Exception {
-                        System.out.println(Thread.currentThread().getName());
-                        return "fuga";
-                    }
-                };
-            }
-        };
-
-        System.out.println("start service");
-        service.start();
-        service.setOnSucceeded(e -> {
-            System.out.println("value=" + service.getValue());
-            System.out.println("thread = " + Thread.currentThread().getName());
-        });
-    }
+//    
+//    @FXML
+//    public void startTask() throws ExecutionException, InterruptedException {
+//        System.out.println("Main Thread = " + Thread.currentThread().getName());
+//        
+//        Task<String> task = new Task<String>() {
+//
+//            @Override
+//            protected String call() throws Exception {
+//                this.updateMessage("foo");
+//                System.out.println(Thread.currentThread().getName());
+//                return "hoge";
+//            }
+//        };
+//
+//        task.setOnSucceeded(e -> {
+//            System.out.println("succeeded thread = " + Thread.currentThread().getName());
+//        });
+//
+//        Thread thread = new Thread(task);
+//        System.out.println("begin thread");
+//        thread.start();
+//
+//        String result = task.get();
+//        System.out.println("end thread. result=" + result);
+//    }
+//    
+//    @FXML
+//    public void startService() {
+//        Service<String> service = new Service<String>() {
+//            @Override
+//            protected Task<String> createTask() {
+//                return new Task<String>() {
+//
+//                    @Override
+//                    protected String call() throws Exception {
+//                        System.out.println(Thread.currentThread().getName());
+//                        return "fuga";
+//                    }
+//                };
+//            }
+//        };
+//
+//        System.out.println("start service");
+//        service.start();
+//        service.setOnSucceeded(e -> {
+//            System.out.println("value=" + service.getValue());
+//            System.out.println("thread = " + Thread.currentThread().getName());
+//        });
+//    }
 }
