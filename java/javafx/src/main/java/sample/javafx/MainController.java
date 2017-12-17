e976c15d553712c6c2604f0f292ee75dbc86d903
opengl-8080 <tomcat.port.8080+github@gmail.com>
2017-12-17 20:35:22 +0900

    JavaFX Task cancel

--- java/javafx/src/main/java/sample/javafx/MainController.java
+++ java/javafx/src/main/java/sample/javafx/MainController.java
@@ -1,25 +1,44 @@
 package sample.javafx;
 
 import javafx.concurrent.Task;
+import javafx.concurrent.Worker;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MainController implements Initializable {
+    
+    private Worker worker;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        Runnable task = new Task<Void>() {
+        Task<Void> task = new Task<Void>() {
 
             @Override
             protected Void call() throws Exception {
-                System.out.println("task.call()");
-                System.out.println("thread = " + Thread.currentThread().getName());
+                System.out.println("start...");
+                final long max = 100000000L;
+                
+                for (long i=0; i<max; i++) {
+                    if (this.isCancelled()) {
+                        System.out.println("cancelled!!");
+                        return null;
+                    }
+                }
+
+                System.out.println("finished");
                 return null;
             }
         };
         
+        this.worker = task;
         new Thread(task).start();
+    }
+    
+    @FXML
+    public void stop() {
+        this.worker.cancel();
     }
 }
