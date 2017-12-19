b92f1a6238dd555543a641a6f259a50a35119c9b
opengl-8080 <tomcat.port.8080+github@gmail.com>
2017-12-20 00:18:38 +0900

    JavaFX Task exception property

--- java/javafx/src/main/java/sample/javafx/MainController.java
+++ java/javafx/src/main/java/sample/javafx/MainController.java
@@ -1,36 +1,28 @@
 package sample.javafx;
 
-import javafx.application.Platform;
-import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.property.StringProperty;
 import javafx.concurrent.Task;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
 
 import java.io.IOException;
 
 public class MainController {
 
     @FXML
-    private Label label;
-
-    @FXML
-    public void start() throws IOException {
+    public void throwException() throws IOException {
         MyTask task = new MyTask();
-        this.label.textProperty().bind(task.valueProperty());
+        task.setOnFailed(event -> {
+            Throwable exception = task.getException();
+            System.out.println("exception=" + exception);
+        });
         
         new Thread(task).start();
     }
     
-    private static class MyTask extends Task<String> {
+    private static class MyTask extends Task<Void> {
 
         @Override
-        protected String call() throws Exception {
-            for (int i=0; i<100000000; i++) {
-                this.updateValue("i=" + i);
-            }
-            
-            return "finished!!";
+        protected Void call() throws Exception {
+            throw new RuntimeException("test");
         }
     }
 }
