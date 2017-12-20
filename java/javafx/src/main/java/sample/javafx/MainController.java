a69e8cca3fd2febedcb2659291d6934e241c2356
opengl-8080 <tomcat.port.8080+github@gmail.com>
2017-12-21 00:09:30 +0900

    JavaFx Service helloworld

--- java/javafx/src/main/java/sample/javafx/MainController.java
+++ java/javafx/src/main/java/sample/javafx/MainController.java
@@ -1,27 +1,36 @@
 package sample.javafx;
 
+import javafx.concurrent.Service;
 import javafx.concurrent.Task;
+import javafx.concurrent.Worker;
 import javafx.fxml.FXML;
 
 import java.io.IOException;
 
 public class MainController {
 
+    private Service<Void> service = new Service<Void>() {
+        @Override
+        protected Task<Void> createTask() {
+            System.out.println("Service.createTask()");
+            
+            return new Task<Void>() {
+                @Override
+                protected Void call() throws Exception {
+                    System.out.println("Task.call()");
+                    return null;
+                }
+            };
+        }
+    };
+    
     @FXML
     public void start() throws IOException {
-        Task<Void> task = new Task<Void>() {
-
-            @Override
-            protected Void call() throws Exception {
-                throw new RuntimeException("test");
-            }
-        };
-        
-        task.setOnFailed(event -> {
-            Throwable exception = task.exceptionProperty().get();
-            System.out.println("exception=" + exception);
-        });
-        
-        new Thread(task).start();
+        if (!this.service.getState().equals(Worker.State.READY)) {
+            System.out.println("reset");
+            this.service.reset();
+        }
+        System.out.println("start");
+        this.service.start();
     }
 }
