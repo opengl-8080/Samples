18fbfbd45bc722be3d4997fa1fb716a00ae84334
opengl-8080 <tomcat.port.8080+github@gmail.com>
2017-12-23 22:00:29 +0900

    JavaFX ReadOnlyPropertyWapper

--- java/javafx/src/main/java/sample/javafx/property/Main.java
+++ java/javafx/src/main/java/sample/javafx/property/Main.java
@@ -1,31 +1,12 @@
 package sample.javafx.property;
 
-import javafx.collections.FXCollections;
-import javafx.collections.ListChangeListener.Change;
-import javafx.collections.ObservableList;
-
 public class Main {
     public static void main(String[] args) {
-        ObservableList<String> list = FXCollections.observableArrayList("one", "two", "three");
+        MyClass myClass = new MyClass();
+        System.out.println(myClass.valueProperty());
+        
+        myClass.updateValue();
 
-        list.addListener((Change<? extends String> change) -> {
-            System.out.println("==========================================");
-            while (change.next()) {
-                System.out.println("list=" + change.getList());
-                
-                System.out.println("replaced=" + change.wasReplaced());
-                System.out.println("added=" + change.wasAdded());
-                System.out.println("removed=" + change.wasRemoved());
-                
-                System.out.println("from=" + change.getFrom());
-                System.out.println("to=" + change.getTo());
-                System.out.println("addedSubList=" + change.getAddedSubList());
-                System.out.println("addedSize=" + change.getAddedSize());
-                System.out.println("removed=" + change.getRemoved());
-                System.out.println("removedSize=" + change.getRemovedSize());
-            }
-        });
-
-        list.set(1, "FOO");
+        System.out.println(myClass.valueProperty());
     }
 }
--- java/javafx/src/main/java/sample/javafx/property/MyClass.java
+++ java/javafx/src/main/java/sample/javafx/property/MyClass.java
@@ -1,17 +1,20 @@
 package sample.javafx.property;
 
-import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.ReadOnlyDoubleProperty;
-import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.ReadOnlyDoubleWrapper;
 
 public class MyClass {
-    private DoubleProperty value = new SimpleDoubleProperty();
+    private ReadOnlyDoubleWrapper value = new ReadOnlyDoubleWrapper();
     
     public final double getValue() {
         return this.value.getValue();
     }
     
     public final ReadOnlyDoubleProperty valueProperty() {
-        return this.value;
+        return this.value.getReadOnlyProperty();
+    }
+    
+    public void updateValue() {
+        this.value.set(99.9);
     }
 }
