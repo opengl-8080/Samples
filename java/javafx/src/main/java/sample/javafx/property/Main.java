27c7321ba21f4196b09a149598c7d1cd06ca0e42
opengl-8080 <tomcat.port.8080+github@gmail.com>
2017-12-23 22:30:19 +0900

    JavaFX FilteredList

--- java/javafx/src/main/java/sample/javafx/property/Main.java
+++ java/javafx/src/main/java/sample/javafx/property/Main.java
@@ -2,19 +2,24 @@ package sample.javafx.property;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.collections.transformation.SortedList;
+import javafx.collections.transformation.FilteredList;
 
 public class Main {
     public static void main(String[] args) {
         ObservableList<String> list = FXCollections.observableArrayList("one", "two", "three");
-        SortedList<String> sortedList = list.sorted();
+        FilteredList<String> filteredList = list.filtered(e -> e.contains("e"));
 
         System.out.println("list=" + list);
-        System.out.println("sortedList=" + sortedList);
+        System.out.println("filteredList=" + filteredList);
         
-        list.addAll("four", "five", "six");
+        list.addAll("four", "five");
 
         System.out.println("list=" + list);
-        System.out.println("sortedList=" + sortedList);
+        System.out.println("filteredList=" + filteredList);
+
+        filteredList.setPredicate(e -> e.contains("f"));
+
+        System.out.println("list=" + list);
+        System.out.println("filteredList=" + filteredList);
     }
 }
