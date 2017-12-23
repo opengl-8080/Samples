6e7b71647b4dc8e7ff45e01f924c6dfb3b0c0d06
opengl-8080 <tomcat.port.8080+github@gmail.com>
2017-12-23 20:57:29 +0900

    JavaFx Collection Change add

--- java/javafx/src/main/java/sample/javafx/property/Main.java
+++ java/javafx/src/main/java/sample/javafx/property/Main.java
@@ -4,6 +4,8 @@ import javafx.collections.FXCollections;
 import javafx.collections.ListChangeListener.Change;
 import javafx.collections.ObservableList;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         ObservableList<String> list = FXCollections.observableArrayList("one", "two", "three");
@@ -12,24 +14,14 @@ public class Main {
             System.out.println("==========================================");
             while (change.next()) {
                 System.out.println("list=" + change.getList());
-                if (change.wasAdded()) {
-                    System.out.println("追加 : " + change);
-                }
-                if (change.wasRemoved()) {
-                    System.out.println("削除 : " + change);
-                }
-                if (change.wasPermutated()) {
-                    System.out.println("順序変更 : " + change);
-                }
-                if (change.wasReplaced()) {
-                    System.out.println("置換 : " + change);
-                }
+                System.out.println("from=" + change.getFrom());
+                System.out.println("to=" + change.getTo());
+                System.out.println("addedSubList=" + change.getAddedSubList());
+                System.out.println("addedSize=" + change.getAddedSize());
             }
         });
 
         list.add("FOO");
-        list.remove("one");
-        FXCollections.sort(list);
-        list.set(1, "hoge");
+        list.addAll(Arrays.asList("hoge", "fuga"));
     }
 }
