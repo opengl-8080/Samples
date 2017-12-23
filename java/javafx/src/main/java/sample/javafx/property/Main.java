174b23d6752fddc6bae9c4e7de96f5703546d6c5
opengl-8080 <tomcat.port.8080+github@gmail.com>
2017-12-23 20:46:54 +0900

    JavaFX Collection

--- java/javafx/src/main/java/sample/javafx/property/Main.java
+++ java/javafx/src/main/java/sample/javafx/property/Main.java
@@ -1,15 +1,35 @@
 package sample.javafx.property;
 
-import javafx.beans.property.DoubleProperty;
-import javafx.beans.property.ReadOnlyDoubleProperty;
-import javafx.beans.property.SimpleDoubleProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener.Change;
+import javafx.collections.ObservableList;
 
 public class Main {
     public static void main(String[] args) {
-        MyClass myClass = new MyClass();
-        DoubleProperty readOnlyDoubleProperty = (DoubleProperty)myClass.valueProperty();
-        readOnlyDoubleProperty.set(20.0);
+        ObservableList<String> list = FXCollections.observableArrayList("one", "two", "three");
+        
+        list.addListener((Change<? extends String> change) -> {
+            System.out.println("==========================================");
+            while (change.next()) {
+                System.out.println("list=" + change.getList());
+                if (change.wasAdded()) {
+                    System.out.println("追加 : " + change);
+                }
+                if (change.wasRemoved()) {
+                    System.out.println("削除 : " + change);
+                }
+                if (change.wasPermutated()) {
+                    System.out.println("順序変更 : " + change);
+                }
+                if (change.wasReplaced()) {
+                    System.out.println("置換 : " + change);
+                }
+            }
+        });
 
-        System.out.println(myClass.getValue());
+        list.add("FOO");
+        list.remove("one");
+        FXCollections.sort(list);
+        list.set(1, "hoge");
     }
 }
