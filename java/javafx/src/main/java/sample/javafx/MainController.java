319da5516c37679cac804a59cd72378ffde1e7c7
opengl-8080 <tomcat.port.8080+github@gmail.com>
2017-12-23 10:00:05 +0900

    Java FX StringProperty StringConverter

--- java/javafx/src/main/java/sample/javafx/MainController.java
+++ java/javafx/src/main/java/sample/javafx/MainController.java
@@ -1,26 +1,52 @@
 package sample.javafx;
 
-import javafx.beans.property.DoubleProperty;
-import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TextField;
+import javafx.util.StringConverter;
 
 import java.net.URL;
-import java.text.DecimalFormat;
-import java.text.Format;
+import java.time.DateTimeException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.ResourceBundle;
 
 public class MainController implements Initializable {
     @FXML
     private TextField textField;
     
-    private DoubleProperty value = new SimpleDoubleProperty();
+    private ObjectProperty<LocalDate> value = new SimpleObjectProperty<>(LocalDate.of(2017, 1, 1));
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        Format format = new DecimalFormat("#,##0.00");
-        this.textField.textProperty().bindBidirectional(this.value, format);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("uuuu/MM/dd");
+        
+        this.textField.textProperty().bindBidirectional(this.value, new StringConverter<LocalDate>() {
+            @Override
+            public String toString(LocalDate date) {
+                if (date == null) {
+                    return "";
+                }
+                
+                try {
+                    return formatter.format(date);
+                } catch (DateTimeException e) {
+                    return "";
+                }
+            }
+
+            @Override
+            public LocalDate fromString(String text) {
+                try {
+                    return LocalDate.parse(text, formatter);
+                } catch (DateTimeParseException e) {
+                    return null;
+                }
+            }
+        });
     }
     
     @FXML
@@ -30,6 +56,6 @@ public class MainController implements Initializable {
     
     @FXML
     public void resetValue() {
-        this.value.set(9876.54);
+        this.value.set(LocalDate.of(2017, 1, 1));
     }
 }
