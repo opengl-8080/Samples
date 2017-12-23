f9b34e8caf1e1ebd07c02c5fa4013d9c37ab3b8d
opengl-8080 <tomcat.port.8080+github@gmail.com>
2017-12-23 21:48:08 +0900

    JavaFX Collection Change replace

--- java/javafx/src/main/java/sample/javafx/property/Main.java
+++ java/javafx/src/main/java/sample/javafx/property/Main.java
@@ -6,22 +6,26 @@ import javafx.collections.ObservableList;
 
 public class Main {
     public static void main(String[] args) {
-        ObservableList<String> list
-            = FXCollections.observableArrayList("aaa", "ccc", "bbb", "eee", "ddd", "fff");
-        
+        ObservableList<String> list = FXCollections.observableArrayList("one", "two", "three");
+
         list.addListener((Change<? extends String> change) -> {
             System.out.println("==========================================");
-            System.out.println("new list=" + change.getList());
             while (change.next()) {
-                System.out.println("permutated=" + change.wasPermutated());
-                for (int oldIndex=change.getFrom(); oldIndex<change.getTo(); oldIndex++) {
-                    int newIndex = change.getPermutation(oldIndex);
-                    System.out.println("old(" + oldIndex + ") -> new(" + newIndex + ")");
-                }
+                System.out.println("list=" + change.getList());
+                
+                System.out.println("replaced=" + change.wasReplaced());
+                System.out.println("added=" + change.wasAdded());
+                System.out.println("removed=" + change.wasRemoved());
+                
+                System.out.println("from=" + change.getFrom());
+                System.out.println("to=" + change.getTo());
+                System.out.println("addedSubList=" + change.getAddedSubList());
+                System.out.println("addedSize=" + change.getAddedSize());
+                System.out.println("removed=" + change.getRemoved());
+                System.out.println("removedSize=" + change.getRemovedSize());
             }
         });
 
-        System.out.println("old list=" + list);
-        list.sort(String::compareTo);
+        list.set(1, "FOO");
     }
 }
