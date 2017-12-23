f83e89cd0050f7ae531a74c6f7382bb7cad09303
opengl-8080 <tomcat.port.8080+github@gmail.com>
2017-12-23 09:27:16 +0900

    JavaFX 双方向バインディング

--- java/javafx/src/main/java/sample/javafx/MainController.java
+++ java/javafx/src/main/java/sample/javafx/MainController.java
@@ -1,37 +1,35 @@
 package sample.javafx;
 
-import javafx.concurrent.Service;
-import javafx.concurrent.Task;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
 import javafx.fxml.FXML;
-import javafx.scene.control.ProgressBar;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
 
-import java.io.IOException;
+import java.net.URL;
+import java.text.DecimalFormat;
+import java.text.Format;
+import java.util.ResourceBundle;
 
-public class MainController {
+public class MainController implements Initializable {
     @FXML
-    private ProgressBar progressBar;
+    private TextField textField;
+    
+    private DoubleProperty value = new SimpleDoubleProperty();
 
-    private Service<Void> service = new Service<Void>() {
-        
-        @Override
-        protected Task<Void> createTask() {
-            return new Task<Void>() {
-                @Override
-                protected Void call() throws Exception {
-                    int max = 10000000;
-                    for (int i=0; i<max; i++) {
-                        this.updateProgress(i, max);
-                    }
-                    
-                    return null;
-                }
-            };
-        }
-    };
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        Format format = new DecimalFormat("#,##0.00");
+        this.textField.textProperty().bindBidirectional(this.value, format);
+    }
     
     @FXML
-    public void start() throws IOException {
-        this.service.restart();
-        this.progressBar.progressProperty().bind(this.service.progressProperty());
+    public void checkValue() {
+        System.out.println("value=" + value.getValue());
+    }
+    
+    @FXML
+    public void resetValue() {
+        this.value.set(9876.54);
     }
 }
