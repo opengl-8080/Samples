245a1321976bcc02baac2c6f6d1ba99fb1c42ebc
opengl-8080 <tomcat.port.8080+github@gmail.com>
2017-12-24 11:27:25 +0900

    JavaFX Reflection

--- java/javafx/src/main/java/sample/javafx/MainController.java
+++ java/javafx/src/main/java/sample/javafx/MainController.java
@@ -2,9 +2,8 @@ package sample.javafx;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.effect.InnerShadow;
-import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
+import javafx.scene.control.Label;
+import javafx.scene.effect.Reflection;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -12,16 +11,15 @@ import java.util.ResourceBundle;
 public class MainController implements Initializable {
     
     @FXML
-    private Pane pane;
+    private Label label;
     
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        InnerShadow innerShadow = new InnerShadow();
-        innerShadow.setOffsetX(7.0);
-        innerShadow.setOffsetY(7.0);
-        innerShadow.setColor(Color.valueOf("500"));
-        innerShadow.setRadius(10.0);
-        
-        this.pane.setEffect(innerShadow);
+        Reflection reflection = new Reflection();
+        reflection.setTopOffset(-25.0);
+        reflection.setTopOpacity(0.8);
+        reflection.setFraction(0.8);
+
+        this.label.setEffect(reflection);
     }
 }
