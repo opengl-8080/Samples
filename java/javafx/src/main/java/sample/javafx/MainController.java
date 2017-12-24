20f6bc62e25de5a141eb5c8b5e3882af903b8802
opengl-8080 <tomcat.port.8080+github@gmail.com>
2017-12-24 10:23:17 +0900

    JavaFx Bloom

--- java/javafx/src/main/java/sample/javafx/MainController.java
+++ java/javafx/src/main/java/sample/javafx/MainController.java
@@ -1,51 +1,21 @@
 package sample.javafx;
 
-import javafx.application.Platform;
-import javafx.beans.property.ReadOnlyListProperty;
-import javafx.beans.property.ReadOnlyListWrapper;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.concurrent.Task;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.Initializable;
+import javafx.scene.effect.Bloom;
+import javafx.scene.layout.Pane;
 
-import java.util.concurrent.TimeUnit;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class MainController {
+public class MainController implements Initializable {
     
     @FXML
-    private Label label;
-    
-    @FXML
-    public void start() {
-        MyTask myTask = new MyTask();
-        this.label.textProperty().bind(myTask.listProperty().asString());
+    private Pane pane;
 
-        Thread thread = new Thread(myTask);
-        thread.setDaemon(true);
-        thread.start();
-    }
-    
-    private static class MyTask extends Task<Void> {
-        private ReadOnlyListWrapper<String> list = new ReadOnlyListWrapper<>(FXCollections.observableArrayList());
-
-        public final ObservableList<String> getList() {
-            return list.get();
-        }
-
-        public ReadOnlyListProperty<String> listProperty() {
-            return list.getReadOnlyProperty();
-        }
-
-        @Override
-        protected Void call() throws Exception {
-            for (int i=0; i<10; i++) {
-                String value = String.valueOf(i);
-                Platform.runLater(() -> this.list.add(value));
-                
-                TimeUnit.SECONDS.sleep(1);
-            }
-            return null;
-        }
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        Bloom bloom = new Bloom();
+        this.pane.setEffect(bloom);
     }
 }
