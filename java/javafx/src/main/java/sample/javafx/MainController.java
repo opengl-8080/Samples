c3be653d58e39ea4cdbabe1425113e4e99946dfa
opengl-8080 <tomcat.port.8080+github@gmail.com>
2017-12-27 00:26:33 +0900

    JavaFX Media movie

--- java/javafx/src/main/java/sample/javafx/MainController.java
+++ java/javafx/src/main/java/sample/javafx/MainController.java
@@ -1,22 +1,32 @@
 package sample.javafx;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
 
+import java.net.URL;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ResourceBundle;
 
-public class MainController {
-    
+public class MainController implements Initializable {
+    @FXML
+    private MediaView mediaView;
     private MediaPlayer player;
     
     @FXML
     public void start() {
-        Path music = Paths.get("./media/music.m4a");
+        this.player.play();
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        Path music = Paths.get("./media/movie.mp4");
         Media media = new Media(music.toUri().toString());
         this.player = new MediaPlayer(media);
-        this.player.setVolume(0.05);
-        this.player.play();
+        this.player.setVolume(0.3);
+        this.mediaView.setMediaPlayer(this.player);
     }
 }
