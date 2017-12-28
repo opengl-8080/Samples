b92e7c397d5537a25ad65e6e3804829dc8dae529
opengl-8080 <tomcat.port.8080+github@gmail.com>
2017-12-28 23:47:11 +0900

    JavaFX Media seek

--- java/javafx/src/main/java/sample/javafx/MainController.java
+++ java/javafx/src/main/java/sample/javafx/MainController.java
@@ -1,10 +1,10 @@
 package sample.javafx;
 
-import javafx.collections.ObservableMap;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
+import javafx.util.Duration;
 
 import java.net.URL;
 import java.nio.file.Path;
@@ -13,23 +13,19 @@ import java.util.ResourceBundle;
 
 public class MainController implements Initializable {
     
+    private MediaPlayer player;
+    
     @FXML
-    public void start() {
-        
+    public void seek() {
+        this.player.seek(Duration.minutes(1));
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         Path music = Paths.get("./media/music2.mp3");
         Media media = new Media(music.toUri().toString());
-        MediaPlayer player = new MediaPlayer(media);
-        
-        player.setOnReady(() -> {
-            ObservableMap<String, Object> metadata = media.getMetadata();
-            
-            metadata.forEach((key, value) -> {
-                System.out.println("key=" + key + ", value=" + value);
-            });
-        });
+        this.player = new MediaPlayer(media);
+        this.player.setStartTime(Duration.minutes(2));
+        this.player.play();
     }
 }
