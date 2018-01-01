55d5597409c3ddf47bcfc9b83fdfb05e412b66d6
opengl-8080 <tomcat.port.8080+github@gmail.com>
2018-01-01 10:26:27 +0900

    JavaFX MenuBar hello

--- java/javafx/src/main/java/sample/javafx/MainController.java
+++ java/javafx/src/main/java/sample/javafx/MainController.java
@@ -1,23 +1,11 @@
 package sample.javafx;
 
 import javafx.fxml.FXML;
-import javafx.stage.DirectoryChooser;
-import javafx.stage.Stage;
-
-import java.io.File;
 
 public class MainController {
     
-    private Stage stage;
-    
     @FXML
-    public void openFileDialog() {
-        DirectoryChooser chooser = new DirectoryChooser();
-        File directory = chooser.showDialog(this.stage);
-        System.out.println("directory=" + directory);
-    }
-
-    public void setStage(Stage stage) {
-        this.stage = stage;
+    public void open() {
+        System.out.println("「開く」が選択された");
     }
 }
