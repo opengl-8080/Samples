0cc475fcf16a178ec2d9d59d2a4c015370cbcb13
opengl-8080 <tomcat.port.8080+github@gmail.com>
2018-01-02 08:50:35 +0900

    JavaFX MenuBar Mnemonic

--- java/javafx/src/main/java/sample/javafx/MainController.java
+++ java/javafx/src/main/java/sample/javafx/MainController.java
@@ -1,48 +1,21 @@
 package sample.javafx;
 
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.RadioMenuItem;
-import javafx.scene.control.ToggleGroup;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-
-public class MainController implements Initializable {
-    @FXML
-    private ToggleGroup group1;
-    @FXML
-    private RadioMenuItem hogeRadioMenuItem;
-    @FXML
-    private RadioMenuItem fugaRadioMenuItem;
-    @FXML
-    private RadioMenuItem piyoRadioMenuItem;
+public class MainController {
     
     @FXML
-    private ToggleGroup group2;
-    @FXML
-    private RadioMenuItem fizzRadioMenuItem;
-    @FXML
-    private RadioMenuItem buzzRadioMenuItem;
+    public void open() {
+        System.out.println("open");
+    }
     
-
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-        this.hogeRadioMenuItem.setUserData("hoge");
-        this.fugaRadioMenuItem.setUserData("fuga");
-        this.piyoRadioMenuItem.setUserData("piyo");
-        this.group1.selectedToggleProperty().addListener((toggle, oldValue, newValue) -> {
-            if (newValue != null) {
-                System.out.println("[group1] " + newValue.getUserData());
-            }
-        });
-        
-        this.fizzRadioMenuItem.setUserData("fizz");
-        this.buzzRadioMenuItem.setUserData("buzz");
-        this.group2.selectedToggleProperty().addListener((toggle, oldValue, newValue) -> {
-            if (newValue != null) {
-                System.out.println("[group2] " + newValue.getUserData());
-            }
-        });
+    @FXML
+    public void close() {
+        System.out.println("close");
+    }
+    
+    @FXML
+    public void fullScreen() {
+        System.out.println("full screen");
     }
 }
