c8dae6ce4752ec0d5a286fd234b2e66116205ba9
opengl-8080 <tomcat.port.8080+github@gmail.com>
2018-01-01 19:11:43 +0900

    JavaFX MenuBar CheckMenuItem

--- java/javafx/src/main/java/sample/javafx/MainController.java
+++ java/javafx/src/main/java/sample/javafx/MainController.java
@@ -2,19 +2,19 @@ package sample.javafx;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.MenuItem;
-import javafx.scene.image.ImageView;
+import javafx.scene.control.CheckMenuItem;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MainController implements Initializable {
     @FXML
-    private MenuItem openMenuItem;
+    private CheckMenuItem checkMenuItem;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        ImageView image = new ImageView("/img/open.png");
-        this.openMenuItem.setGraphic(image);
+        this.checkMenuItem.selectedProperty().addListener((value, oldValue, newValue) -> {
+            System.out.println(newValue ? "チェックされた" : "チェックが外れた");
+        });
     }
 }
