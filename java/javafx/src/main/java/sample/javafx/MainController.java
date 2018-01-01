7dec23795c6dddf83be6ec408824326bb5bb8377
opengl-8080 <tomcat.port.8080+github@gmail.com>
2018-01-01 09:43:51 +0900

    JavaFX FileChooser extensionFilters

--- java/javafx/src/main/java/sample/javafx/MainController.java
+++ java/javafx/src/main/java/sample/javafx/MainController.java
@@ -1,7 +1,9 @@
 package sample.javafx;
 
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
 import javafx.stage.Stage;
 
 import java.io.File;
@@ -13,7 +15,12 @@ public class MainController {
     @FXML
     public void openFileDialog() {
         FileChooser chooser = new FileChooser();
-        chooser.setInitialDirectory(new File("C:/Program Files/java/jdk-9.0.1"));
+        
+        ObservableList<ExtensionFilter> extensionFilters = chooser.getExtensionFilters();
+        extensionFilters.add(new ExtensionFilter("何でもあり", "*.*"));
+        extensionFilters.add(new ExtensionFilter("画像だけやで", "*.jpg", "*.jpeg", "*.png", "*.gif"));
+        extensionFilters.add(new ExtensionFilter("まさかの動画", "*.mp4"));
+        
         File file = chooser.showOpenDialog(this.stage);
         System.out.println("file=" + file);
     }
