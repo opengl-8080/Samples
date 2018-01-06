b38be5776f76193bc8646dc33928c27dcabaef09
opengl-8080 <tomcat.port.8080+github@gmail.com>
2018-01-06 13:12:53 +0900

    KDoc outputFormat html

--- kotlin/kdoc/src/main/kotlin/sample/kdoc/Hello.kt
+++ kotlin/kdoc/src/main/kotlin/sample/kdoc/Hello.kt
@@ -1,6 +1,40 @@
 package sample.kdoc
 
 /**
- * Hello KDoc!!
+ * クラスの説明.
  */
-class Hello
+class TopLevelClass {
+    /**
+     * コンパニオンオブジェクトの説明.
+     */
+    companion object {
+        /**
+         * コンパニオンオブジェクトのプロパティの説明.
+         */
+        val value = "test"
+    }
+
+    /**
+     * プロパティの説明.
+     */
+    var value: String? = null
+
+    /**
+     * メソッドの説明.
+     * 
+     * @args arg 引数の説明
+     * @return return 戻り値の説明
+     */
+    fun method(arg: String = "default") = "method"
+}
+
+/**
+ * トップレベルの関数の説明.
+ */
+fun topLevelFunction() {
+}
+
+/**
+ * トップレベルのプロパティの説明.
+ */
+val topLevelProperty = "test"
