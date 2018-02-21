55693d47540dd1faac93014209e36ca3baffa9ac
opengl-8080 <tomcat.port.8080+github@gmail.com>
2018-02-21 22:50:35 +0900

    JavaFX ウィンドウなしを調整

--- java/javafx/src/main/java/sample/javafx/Main.java
+++ java/javafx/src/main/java/sample/javafx/Main.java
@@ -2,9 +2,10 @@ package sample.javafx;
 
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
@@ -22,26 +23,23 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        Platform.setImplicitExit(false);
+        
         primaryStage.initStyle(StageStyle.UTILITY);
         primaryStage.setOpacity(0.0);
         primaryStage.setX(-1000);
         primaryStage.setY(-1000);
-        
-        Stage main = new Stage();
-        main.initOwner(primaryStage);
-        main.initStyle(StageStyle.TRANSPARENT);
-        
-        BorderPane borderPane = new BorderPane();
-        borderPane.setPrefSize(300, 200);
-        Button button = new Button("close");
-        button.setOnAction(e -> Platform.exit());
-        borderPane.setCenter(button);
-        Scene scene = new Scene(borderPane);
-        main.setScene(scene);
-
-        Platform.setImplicitExit(false);
         primaryStage.show();
-        main.show();
+        
+        Stage mainStage = new Stage();
+        mainStage.initOwner(primaryStage);
+        mainStage.initStyle(StageStyle.TRANSPARENT);
+        Parent root = FXMLLoader.load(this.getClass().getResource("/main.fxml"));
+        Scene scene = new Scene(root);
+        scene.setFill(Color.TRANSPARENT);
+        mainStage.setScene(scene);
+        
+        mainStage.show();
         
         if (SystemTray.isSupported()) {
             BufferedImage img = ImageIO.read(this.getClass().getResource("/img/open.png"));
@@ -49,10 +47,10 @@ public class Main extends Application {
             this.trayIcon.setImageAutoSize(true);
             this.trayIcon.addActionListener(e -> {
                 Platform.runLater(() -> {
-                    if (main.isShowing()) {
-                        main.hide();
+                    if (mainStage.isShowing()) {
+                        mainStage.hide();
                     } else {
-                        main.show();
+                        mainStage.show();
                     }
                 });
             });
--- java/javafx/src/main/java/sample/javafx/MainController.java
+++ java/javafx/src/main/java/sample/javafx/MainController.java
@@ -1,17 +1,12 @@
 package sample.javafx;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-
-public class MainController implements Initializable {
+public class MainController {
+    
     @FXML
-    private EmbeddedController embeddedController;
-
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-        this.embeddedController.setMessage("Hello!!");
+    public void close() {
+        Platform.exit();
     }
 }
