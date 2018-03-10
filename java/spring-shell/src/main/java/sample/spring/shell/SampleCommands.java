68053825d7492623b7dcf529d61fb2f0a1dee3ad
opengl-8080 <tomcat.port.8080+github@gmail.com>
2018-03-10 22:32:27 +0900

    SpringShell ShellMethod.key

--- java/spring-shell/src/main/java/sample/spring/shell/SampleCommands.java
+++ java/spring-shell/src/main/java/sample/spring/shell/SampleCommands.java
@@ -6,7 +6,7 @@ import org.springframework.shell.standard.ShellMethod;
 @ShellComponent
 public class SampleCommands {
     
-    @ShellMethod("hello")
+    @ShellMethod(value="hello", key = "test")
     public void hello() {
         System.out.println("Hello Spring Shell!!");
     }
