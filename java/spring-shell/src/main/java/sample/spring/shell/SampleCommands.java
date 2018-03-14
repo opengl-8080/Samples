9163fc20509c3bcf44d13797821a5bb6c2626f37
opengl-8080 <tomcat.port.8080+github@gmail.com>
2018-03-14 21:14:05 +0900

    SpringShell コマンドに複数の名前を割り当てる

--- java/spring-shell/src/main/java/sample/spring/shell/SampleCommands.java
+++ java/spring-shell/src/main/java/sample/spring/shell/SampleCommands.java
@@ -6,7 +6,7 @@ import org.springframework.shell.standard.ShellMethod;
 @ShellComponent
 public class SampleCommands {
     
-    @ShellMethod(value="Hello World", key="hoge")
+    @ShellMethod(value="Hello World", key={"hoge", "fuga"})
     public void hello() {
         System.out.println("Hello Spring Shell!!");
     }
