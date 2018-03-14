ea8475044285845ec3e06d6f939c46230907c28c
opengl-8080 <tomcat.port.8080+github@gmail.com>
2018-03-14 21:12:00 +0900

    SpringShell コマンド名の指定

--- java/spring-shell/src/main/java/sample/spring/shell/SampleCommands.java
+++ java/spring-shell/src/main/java/sample/spring/shell/SampleCommands.java
@@ -6,13 +6,8 @@ import org.springframework.shell.standard.ShellMethod;
 @ShellComponent
 public class SampleCommands {
     
-    @ShellMethod
+    @ShellMethod(value="Hello World", key="hoge")
     public void hello() {
         System.out.println("Hello Spring Shell!!");
-    }
-    
-    @ShellMethod(value="arguments", prefix="*")
-    public void hoge(int a, int b, int c) {
-        System.out.println("a=" + a + ", b=" + b + ", c=" + c);
     }
 }
