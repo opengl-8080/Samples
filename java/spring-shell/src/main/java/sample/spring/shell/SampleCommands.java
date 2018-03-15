6f6b9db1a8ad14f418b715c8309e9c06cb2236dc
opengl-8080 <tomcat.port.8080+github@gmail.com>
2018-03-15 20:56:14 +0900

    SpringShell 引数のデフォルト値

--- java/spring-shell/src/main/java/sample/spring/shell/SampleCommands.java
+++ java/spring-shell/src/main/java/sample/spring/shell/SampleCommands.java
@@ -6,9 +6,9 @@ import org.springframework.shell.standard.ShellOption;
 
 @ShellComponent
 public class SampleCommands {
-    
+
     @ShellMethod("Hello World")
-    public void hello(int a, @ShellOption("--foo") int b, @ShellOption({"-h", "--hoge"}) int c) {
-        System.out.println("a=" + a + ", b=" + b + ", c=" + c);
+    public void hello(@ShellOption(defaultValue="9") int a) {
+        System.out.println("a=" + a);
     }
 }
