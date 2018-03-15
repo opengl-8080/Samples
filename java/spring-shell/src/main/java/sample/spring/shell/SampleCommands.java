de4f3ea3bdcaf5c344d472a6c467944b876ef55f
opengl-8080 <tomcat.port.8080+github@gmail.com>
2018-03-15 20:51:37 +0900

    SpringShell 引数名を変更する

--- java/spring-shell/src/main/java/sample/spring/shell/SampleCommands.java
+++ java/spring-shell/src/main/java/sample/spring/shell/SampleCommands.java
@@ -2,12 +2,13 @@ package sample.spring.shell;
 
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
+import org.springframework.shell.standard.ShellOption;
 
 @ShellComponent
 public class SampleCommands {
     
-    @ShellMethod(value="Hello World", prefix="-")
-    public void hello(int a) {
-        System.out.println("a=" + a);
+    @ShellMethod("Hello World")
+    public void hello(int a, @ShellOption("--foo") int b, @ShellOption({"-h", "--hoge"}) int c) {
+        System.out.println("a=" + a + ", b=" + b + ", c=" + c);
     }
 }
