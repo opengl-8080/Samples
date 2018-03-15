d503a5a4b360048fa0b4a760b143ecf242eb3781
opengl-8080 <tomcat.port.8080+github@gmail.com>
2018-03-15 20:43:52 +0900

    SpringShell 引数のプレフィックス

--- java/spring-shell/src/main/java/sample/spring/shell/SampleCommands.java
+++ java/spring-shell/src/main/java/sample/spring/shell/SampleCommands.java
@@ -6,8 +6,8 @@ import org.springframework.shell.standard.ShellMethod;
 @ShellComponent
 public class SampleCommands {
     
-    @ShellMethod("Hello World")
-    public void hello(int a, int b, int fooBar) {
-        System.out.println("a=" + a + ", b=" + b + ", fooBar=" + fooBar);
+    @ShellMethod(value="Hello World", prefix="-")
+    public void hello(int a) {
+        System.out.println("a=" + a);
     }
 }
