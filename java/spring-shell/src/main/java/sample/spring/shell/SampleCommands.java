27c7f6f817414b813ad437fcdf57861f0fe484e1
opengl-8080 <tomcat.port.8080+github@gmail.com>
2018-03-16 21:42:08 +0900

    SpringShell boolean 引数のデフォルト値指定

--- java/spring-shell/src/main/java/sample/spring/shell/SampleCommands.java
+++ java/spring-shell/src/main/java/sample/spring/shell/SampleCommands.java
@@ -2,12 +2,13 @@ package sample.spring.shell;
 
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
+import org.springframework.shell.standard.ShellOption;
 
 @ShellComponent
 public class SampleCommands {
 
     @ShellMethod("Hello World")
-    public void hello(boolean a) {
-        System.out.println("a=" + a);
+    public void hello(@ShellOption(defaultValue="true") boolean a, @ShellOption(defaultValue="false") boolean b) {
+        System.out.println("a=" + a + ", b=" + b);
     }
 }
