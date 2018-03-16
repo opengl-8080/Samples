3555bfd6a1f8ffea7071f9c7f712610e39f4ec67
opengl-8080 <tomcat.port.8080+github@gmail.com>
2018-03-16 21:21:44 +0900

    SpringShell boolean引数

--- java/spring-shell/src/main/java/sample/spring/shell/SampleCommands.java
+++ java/spring-shell/src/main/java/sample/spring/shell/SampleCommands.java
@@ -2,15 +2,12 @@ package sample.spring.shell;
 
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
-import org.springframework.shell.standard.ShellOption;
-
-import java.util.Arrays;
 
 @ShellComponent
 public class SampleCommands {
 
     @ShellMethod("Hello World")
-    public void hello(@ShellOption(arity=3) int[] a, int b) {
-        System.out.println("a=" + Arrays.toString(a) + ", b=" + b);
+    public void hello(boolean a) {
+        System.out.println("a=" + a);
     }
 }
