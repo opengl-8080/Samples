97253e15914fb6313e61e42e6c7f4f414073c35d
opengl-8080 <tomcat.port.8080+github@gmail.com>
2018-03-18 10:28:31 +0900

    SpringShell 組み込みコマンドの除去　特定のものだけ

--- java/spring-shell/src/main/java/sample/spring/shell/SampleApplication.java
+++ java/spring-shell/src/main/java/sample/spring/shell/SampleApplication.java
@@ -2,11 +2,14 @@ package sample.spring.shell;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.util.StringUtils;
 
 @SpringBootApplication
 public class SampleApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SampleApplication.class, args);
+		String[] disabledCommands = {"--spring.shell.command.help.enabled=false"};
+		String[] fullArgs = StringUtils.concatenateStringArrays(args, disabledCommands);
+		SpringApplication.run(SampleApplication.class, fullArgs);
 	}
 }
