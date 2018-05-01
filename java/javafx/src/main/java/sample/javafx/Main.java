3c2f028b7e1fcf05428475639463488840ed5f48
opengl-8080 <tomcat.port.8080+github@gmail.com>
2018-05-01 16:29:03 +0900

    JavaFX CSSの相対パス指定

--- java/javafx/src/main/java/sample/javafx/Main.java
+++ java/javafx/src/main/java/sample/javafx/Main.java
@@ -13,9 +13,7 @@ public class Main extends Application {
     
     @Override
     public void start(Stage primaryStage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(this.getClass().getResource("/common.fxml"));
-        
-        loader.setController(new FugaController());
+        FXMLLoader loader = new FXMLLoader(this.getClass().getResource("/main.fxml"));
         
         Parent root = loader.load();
         Scene scene = new Scene(root);
