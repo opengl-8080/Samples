abb21021596ad175408bcb15221e5dc64e7df882
opengl-8080 <tomcat.port.8080+github@gmail.com>
2018-05-30 21:57:02 +0900

    Gradle with Jigsaw: 依存あり非モジュール構成

--- java/gradle/jigsaw/src/main/java/sample/jigsaw/Main.java
+++ java/gradle/jigsaw/src/main/java/sample/jigsaw/Main.java
@@ -1,8 +1,10 @@
 package sample.jigsaw;
 
+import org.apache.commons.lang3.RandomStringUtils;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello Jigsaw!!");
+        System.out.println("Hello Jigsaw!! : " + RandomStringUtils.random(10, "0123456789"));
     }
 }
