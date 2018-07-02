4800e664193cbea52228cf96aea8df2e6ba0a331
opengl-8080 <tomcat.port.8080+github@gmail.com>
2018-07-02 22:29:12 +0900

    Jigsaw : add jep261

--- java/java9/jigsaw/src/sample/Main.java
+++ java/java9/jigsaw/src/sample/Main.java
@@ -1,9 +1,12 @@
 package sample;
 
 import hoge.Hoge;
+import hoge.Hoge2;
 
 public class Main {
     public static void main(String[] args) {
         new Hoge().method();
+        Hoge2 hoge2 = new Hoge2();
+        System.out.println("hoge2=" + hoge2);
     }
 }
---
+++ java/java9/jigsaw/unmodular-hoge/src/hoge/Hoge2.java
@@ -0,0 +1,5 @@
+package hoge;
+
+public class Hoge2 {
+    
+}
