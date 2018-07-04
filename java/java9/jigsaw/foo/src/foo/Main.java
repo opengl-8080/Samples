2eaef8e7dd2ac54c30d57becfb8deb26e0ac1da4
opengl-8080 <tomcat.port.8080+github@gmail.com>
2018-07-04 21:40:11 +0900

    Jigsaw : --add-modules で指定できる特殊なオプションについて

--- java/java9/jigsaw/foo/src/foo/Main.java
+++ java/java9/jigsaw/foo/src/foo/Main.java
@@ -1,16 +1,7 @@
 package foo;
 
-import bar.Bar;
-import bar.internal.InternalBar;
-
 public class Main {
     public static void main(String[] args) {
-        Bar bar = new Bar();
-        InternalBar ib = new InternalBar();
-        Module unnamedModule = Thread.currentThread().getContextClassLoader().getUnnamedModule();
-        unnamedModule.getPackages().forEach(packageName -> {
-            boolean result = unnamedModule.isOpen(packageName);
-            System.out.println(packageName + " is " + (result ? "opened." : "closed."));
-        });
+        System.out.println("Hello World");
     }
 }
