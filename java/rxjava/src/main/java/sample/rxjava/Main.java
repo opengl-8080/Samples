01945d4bc590a1c2769e90fe5e5337af2853befb
opengl-8080 <tomcat.port.8080+github@gmail.com>
2018-07-23 22:18:19 +0900

    RxJava : Hello World

--- java/rxjava/src/main/java/sample/rxjava/Main.java
+++ java/rxjava/src/main/java/sample/rxjava/Main.java
@@ -1,10 +1,57 @@
 package sample.rxjava;
 
+import io.reactivex.BackpressureStrategy;
 import io.reactivex.Flowable;
+import io.reactivex.schedulers.Schedulers;
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Flowable.just(1, 2, 3, 4, 5).subscribe(System.out::println);
+    public static void main(String[] args) throws InterruptedException {
+        Flowable<String> flowable = Flowable.create(emitter -> {
+            String[] messages = {"Hello, World!", "こんにちは、世界！"};
+            
+            for (String message : messages) {
+                if (emitter.isCancelled()) {
+                    return;
+                }
+                emitter.onNext(message);
+            }
+
+            emitter.onComplete();
+        }, BackpressureStrategy.BUFFER);
+        
+        flowable.observeOn(Schedulers.computation())
+                .subscribe(new Subscriber<>() {
+                    
+                    private Subscription subscription;
+                    
+                    @Override
+                    public void onSubscribe(Subscription subscription) {
+                        this.subscription = subscription;
+                        this.subscription.request(1L);
+                    }
+
+                    @Override
+                    public void onNext(String message) {
+                        String threadName = Thread.currentThread().getName();
+                        System.out.println(threadName + ": " + message);
+                        this.subscription.request(1L);
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        String threadName = Thread.currentThread().getName();
+                        System.out.println(threadName + ": 完了しました");
+                    }
+
+                    @Override
+                    public void onError(Throwable error) {
+                        error.printStackTrace(System.err);
+                    }
+                });
+        
+        Thread.sleep(500L);
     }
 }
