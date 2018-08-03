df9972046c42468403ed0fb68e92861e93221b08
opengl-8080 <tomcat.port.8080+github@gmail.com>
2018-08-03 22:37:59 +0900

    RxJava : マルチスレッドの動作を検証

--- java/rxjava/src/main/java/sample/rxjava/Main.java
+++ java/rxjava/src/main/java/sample/rxjava/Main.java
@@ -3,34 +3,29 @@ package sample.rxjava;
 import io.reactivex.Flowable;
 import io.reactivex.schedulers.Schedulers;
 
-import java.util.Set;
-import java.util.concurrent.ConcurrentHashMap;
-
 public class Main {
-
-    private static volatile int counter;
     
     public static void main(String[] args) throws Exception {
-        Flowable<Integer> f1 = Flowable.range(1, 10000)
-                .subscribeOn(Schedulers.computation())
-                .observeOn(Schedulers.computation());
-        Flowable<Integer> f2 = Flowable.range(1, 10000)
-                .subscribeOn(Schedulers.computation())
-                .observeOn(Schedulers.computation());
-
-        Set<String> names = ConcurrentHashMap.newKeySet();
-
-        Flowable.merge(f1, f2)
+        Flowable.range(1, 100)
+                .observeOn(Schedulers.computation())
+                .filter(it -> {
+                    System.out.println("[" + Thread.currentThread().getName() + "][filter] " + it);
+                    return it%2 == 0;
+                })
+                .observeOn(Schedulers.computation())
+                .map(it -> {
+                    System.out.println("[" + Thread.currentThread().getName() + "][map] " + it);
+                    return it*10;
+                })
+                .observeOn(Schedulers.computation())
                 .subscribe(
                     data -> {
-                        names.add(Thread.currentThread().getName());
-                        counter++;
+                        System.out.println("[" + Thread.currentThread().getName() + "][onNext] " + data);
                     },
                     error -> error.printStackTrace(System.err),
-                    () -> System.out.println("counter=" + counter)
+                    () -> System.out.println("complete")
                 );
         
         Thread.sleep(1000);
-        System.out.println(names);
     }
 }
