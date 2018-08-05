2947c6fbe8a0fdce4ff4d9c1b5318aeec40e84c3
opengl-8080 <tomcat.port.8080+github@gmail.com>
2018-08-05 21:05:46 +0900

    RxJava : リソース管理

--- java/rxjava/src/main/java/sample/rxjava/Main.java
+++ java/rxjava/src/main/java/sample/rxjava/Main.java
@@ -1,31 +1,50 @@
 package sample.rxjava;
 
+import io.reactivex.BackpressureStrategy;
 import io.reactivex.Flowable;
-import io.reactivex.schedulers.Schedulers;
+import io.reactivex.functions.Consumer;
+import io.reactivex.functions.Function;
+import org.reactivestreams.Publisher;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.concurrent.Callable;
 
 public class Main {
     
     public static void main(String[] args) throws Exception {
-        Flowable.range(1, 100)
-                .observeOn(Schedulers.computation())
-                .filter(it -> {
-                    System.out.println("[" + Thread.currentThread().getName() + "][filter] " + it);
-                    return it%2 == 0;
-                })
-                .observeOn(Schedulers.computation())
-                .map(it -> {
-                    System.out.println("[" + Thread.currentThread().getName() + "][map] " + it);
-                    return it*10;
-                })
-                .observeOn(Schedulers.computation())
-                .subscribe(
-                    data -> {
-                        System.out.println("[" + Thread.currentThread().getName() + "][onNext] " + data);
-                    },
-                    error -> error.printStackTrace(System.err),
-                    () -> System.out.println("complete")
-                );
+        Callable<BufferedReader> resourceSupplier = () -> Files.newBufferedReader(Paths.get("./build.gradle"));
+        Function<BufferedReader, Publisher<String>> sourceSupplier = reader -> {
+            return Flowable.create(emitter -> {
+                try {
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        emitter.onNext(line);
+                    }
+                    emitter.onComplete();
+                } catch (IOException e) {
+                    throw new UncheckedIOException(e);
+                }
+            }, BackpressureStrategy.BUFFER);
+        };
+        Consumer<BufferedReader> resourceDisposer = reader -> {
+            try {
+                System.out.println("close");
+                reader.close();
+            } catch (IOException e) {
+                e.printStackTrace(System.err);
+            }
+        };
         
-        Thread.sleep(1000);
+        Flowable<String> flowable = Flowable.using(resourceSupplier, sourceSupplier, resourceDisposer);
+
+        flowable.subscribe(
+            (line) -> System.out.println("[subscribe]=" + line),
+            Throwable::printStackTrace,
+            () -> System.out.println("complete")
+        );
     }
 }
