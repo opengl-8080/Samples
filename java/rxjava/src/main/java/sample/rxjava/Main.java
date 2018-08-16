68e097d144c4c0497cf3c8575e76f0d41bd6aad5
opengl-8080 <tomcat.port.8080+github@gmail.com>
2018-08-16 22:10:43 +0900

    RxJava : 変換オペレータを一通り

--- java/rxjava/src/main/java/sample/rxjava/Main.java
+++ java/rxjava/src/main/java/sample/rxjava/Main.java
@@ -1,60 +1,16 @@
 package sample.rxjava;
 
-import io.reactivex.BackpressureStrategy;
 import io.reactivex.Flowable;
-import io.reactivex.FlowableSubscriber;
-import io.reactivex.schedulers.Schedulers;
-import org.reactivestreams.Subscription;
+
+import java.util.concurrent.TimeUnit;
 
 public class Main {
     
-    static int i = 0;
-    
     public static void main(String[] args) throws Exception {
-        
-        Flowable.<Integer>generate(emitter -> {
-            if (i < 100) {
-                Thread.sleep(50);
-                emitter.onNext(i);
-                i++;
-            } else {
-                emitter.onComplete();
-            }
-        })
-        .doOnNext(value -> System.out.println("observe : " + value))
-        .observeOn(Schedulers.computation(), false, 2)
-        .subscribeOn(Schedulers.computation())
-        .subscribe(new FlowableSubscriber<>() {
-            Subscription subscription;
-            int n = 0;
-            
-            @Override
-            public void onSubscribe(Subscription subscription) {
-                this.subscription = subscription;
-                this.subscription.request(9);
-            }
-
-            @Override
-            public void onNext(Integer integer) {
-                System.out.println("subscribe :: " + integer);
-                try {
-                    Thread.sleep(500);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                this.subscription.request(1);
-            }
-
-            @Override
-            public void onError(Throwable t) {
-                t.printStackTrace(System.err);
-            }
-
-            @Override
-            public void onComplete() {
-                System.out.println("complete!");
-            }
-        });
+        Flowable.interval(500, TimeUnit.MILLISECONDS)
+                .take(4, TimeUnit.SECONDS)
+                .doOnComplete(() -> System.out.println("complete!"))
+                .subscribe(System.out::println);
         
         Thread.sleep(10000);
     }
