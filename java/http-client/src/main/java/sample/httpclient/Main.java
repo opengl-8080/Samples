208ac2aa460cb9254844c4143bd629c621bb6d06
opengl-8080 <tomcat.port.8080+github@gmail.com>
2018-09-26 21:42:30 +0900

    HttpClient : Hello World

---
+++ java/http-client/src/main/java/sample/httpclient/Main.java
@@ -0,0 +1,26 @@
+package sample.httpclient;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+        HttpClient httpClient = HttpClient.newHttpClient();
+        HttpRequest httpRequest = HttpRequest.newBuilder().uri(URI.create("https://www.google.co.jp")).GET().build();
+        CompletableFuture<HttpResponse<String>> future = httpClient.sendAsync(httpRequest, HttpResponse.BodyHandlers.ofString());
+        future.thenAcceptAsync(stringHttpResponse -> {
+            System.out.println("body=" + stringHttpResponse.body());
+        });
+
+        System.out.println("waiting...");
+
+        TimeUnit.SECONDS.sleep(3);
+
+        System.out.println("terminate.");
+    }
+}
