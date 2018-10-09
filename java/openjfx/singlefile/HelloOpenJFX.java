43202063ce15096d28a7d837e39d58e70b8cb6e1
opengl-8080 <tomcat.port.8080+github@gmail.com>
2018-10-09 22:22:02 +0900

    OpenJFX : Hello World 2018 fall

---
+++ java/openjfx/singlefile/HelloOpenJFX.java
@@ -0,0 +1,27 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class HelloOpenJFX extends Application {
+
+    public static void main(String[] args) {
+        Application.launch(HelloOpenJFX.class, args);
+    }
+
+    @Override
+    public void start(Stage stage) {
+        Label label = new Label("Hello OpenJFX!!");
+        label.setStyle(
+            "-fx-font-size: 50px;"
+          + "-fx-font-weight: bold;"
+          + "-fx-alignment: center;"
+          + "-fx-text-fill: linear-gradient(to bottom right, #3f95ea 0%, #52d3aa 100%);"
+        );
+        Scene scene = new Scene(label);
+        stage.setScene(scene);
+        stage.setWidth(450);
+        stage.setHeight(200);
+        stage.show();
+    }
+}
