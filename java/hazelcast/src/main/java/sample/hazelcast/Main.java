faa97d1d4c4c5cb8cc543e02e6ccebf93836a676
opengl-8080 <tomcat.port.8080+github@gmail.com>
2018-10-22 22:03:29 +0900

    Hazelcast : システムプロパティを設定ファイルから指定

--- java/hazelcast/src/main/java/sample/hazelcast/Main.java
+++ java/hazelcast/src/main/java/sample/hazelcast/Main.java
@@ -1,15 +1,16 @@
 package sample.hazelcast;
 
-import com.hazelcast.config.Config;
+import com.hazelcast.config.ClasspathXmlConfig;
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
 
 public class Main {
     
     public static void main(String[] args) {
-        Config config = new Config();
-        config.setInstanceName("ProgrammaticallyInstance");
+        ClasspathXmlConfig config = new ClasspathXmlConfig("my-hazelcast.xml");
         HazelcastInstance instance = Hazelcast.newHazelcastInstance(config);
-        System.out.println("instance.name = " + instance.getName());
+        
+        int size = instance.getPartitionService().getPartitions().size();
+        System.out.println("partition.size=" + size);
     }
 }
