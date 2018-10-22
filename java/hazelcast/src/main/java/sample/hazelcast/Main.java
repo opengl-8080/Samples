44fead38c72d4a720d968d04cc4147f9ede72ec7
opengl-8080 <tomcat.port.8080+github@gmail.com>
2018-10-22 21:42:01 +0900

    Hazelcast : Config を直接 new

--- java/hazelcast/src/main/java/sample/hazelcast/Main.java
+++ java/hazelcast/src/main/java/sample/hazelcast/Main.java
@@ -1,13 +1,14 @@
 package sample.hazelcast;
 
-import com.hazelcast.config.ClasspathXmlConfig;
+import com.hazelcast.config.Config;
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
 
 public class Main {
     
     public static void main(String[] args) {
-        ClasspathXmlConfig config = new ClasspathXmlConfig("my-hazelcast.xml");
+        Config config = new Config();
+        config.setInstanceName("ProgrammaticallyInstance");
         HazelcastInstance instance = Hazelcast.newHazelcastInstance(config);
         System.out.println("instance.name = " + instance.getName());
     }
