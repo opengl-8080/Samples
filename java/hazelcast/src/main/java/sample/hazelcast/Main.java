14df156425d5c7359382abe1eeb5178679f9b3b9
opengl-8080 <tomcat.port.8080+github@gmail.com>
2018-10-24 21:25:29 +0900

    Hazelcast : hazelcast.config でクラスパス内のファイルを指定できるか検証

--- java/hazelcast/src/main/java/sample/hazelcast/Main.java
+++ java/hazelcast/src/main/java/sample/hazelcast/Main.java
@@ -1,17 +1,12 @@
 package sample.hazelcast;
 
-import com.hazelcast.config.Config;
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
 
 public class Main {
     
     public static void main(String[] args) {
-        Config config = new Config();
-        config.setProperty("hazelcast.partition.count", "231");
-        HazelcastInstance instance = Hazelcast.newHazelcastInstance(config);
-        
-        int size = instance.getPartitionService().getPartitions().size();
-        System.out.println("partition.size=" + size);
+        HazelcastInstance instance = Hazelcast.newHazelcastInstance();
+        System.out.println(instance.getName());
     }
 }
