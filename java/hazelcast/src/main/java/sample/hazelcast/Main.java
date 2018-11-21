07bd88d481cc12c82015f4e0e7f89532cb73d3b4
opengl-8080 <tomcat.port.8080+github@gmail.com>
2018-11-21 23:00:34 +0900

    Hazelcast : Map に保存した値がコピーであることの確認

--- java/hazelcast/src/main/java/sample/hazelcast/Main.java
+++ java/hazelcast/src/main/java/sample/hazelcast/Main.java
@@ -1,26 +1,33 @@
 package sample.hazelcast;
 
 import com.hazelcast.config.ClasspathXmlConfig;
-import com.hazelcast.config.MapConfig;
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.core.IMap;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         ClasspathXmlConfig config = new ClasspathXmlConfig("my-hazelcast.xml");
         HazelcastInstance instance = Hazelcast.newHazelcastInstance(config);
-        MapConfig mapConfig = instance.getConfig().getMapConfig("sample");
-        System.out.println(mapConfig.getEvictionPolicy());
-        System.out.println(mapConfig.getMaxSizeConfig().getSize());
 
-        IMap<Object, Object> map = instance.getMap("sample");
-        map.put("one", "ONE");
-        map.put("two@foo", "TWO");
-        map.put("three@foo", "THREE");
+        IMap<String, List<String>> map = instance.getMap("sample");
+        List<String> list = new ArrayList<>();
+        list.add("foo");
+        list.add("bar");
+        map.put("one", list);
         
+        print(map);
+
+        List<String> saved = map.get("one");
+        saved.add("fizz");
+        saved.add("buzz");
+
+        System.out.println("original list=" + list);
+        System.out.println("saved list=" + saved);
         print(map);
     }
 
